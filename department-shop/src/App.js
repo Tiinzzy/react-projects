4b8a8aa7c2077f971331350524a56b4cc257bd25
Tina <tinzzy.cracked@yahoo.com>
2022-11-07 17:21:41 -0500

    Created router to make links to other pages, have to fix header

--- department-shop/src/App.js
+++ department-shop/src/App.js
@@ -1,13 +1,46 @@
 import './App.css';
 
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+
+
+import OrdersList from './components/OrdersList';
 import DropDownMenu from './components/DropDownMenu';
 
-function App() {
-  return (
-    <>
-      <DropDownMenu />
-    </>
-  );
+// import Box from '@mui/material/Box';
+
+
+
+
+
+function Header() {
+  return (<Box display='flex' mb={5}>
+    <Link href="/">Home</Link>
+    <Divider orientation="vertical" variant="middle" flexItem sx={{ ml: 2, mr: 2 }} />
+    <Link href="/add-order" >Add Order</Link>
+    <Divider orientation="vertical" variant="middle" flexItem sx={{ ml: 2, mr: 2 }} />
+    <Link href="/list-orders" >List Orders</Link>
+  </Box>);
 }
 
-export default App;
+export default function App() {
+
+  return (
+    <>
+      <Header />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h3>HOME</h3>} />
+          <Route path="/add-order" element={<DropDownMenu />} />
+          <Route path="/list-orders" element={<OrdersList />} />
+          <Route path='*' element={<h3>Error</h3>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
