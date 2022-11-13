83a262a7e09ebf0e574c55b99d2d0c022ec8b6e4
Tina <tinzzy.cracked@yahoo.com>
2022-11-13 17:30:36 -0500

    styled heather

--- file-diff/src/components/Header.js
+++ file-diff/src/components/Header.js
@@ -1,8 +1,14 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
 export default function Header(props) {
     return (
-        <div style={{ padding: 5, background: '#eaeaea', marginBottom: 10 }}>
-            This is header
-        </div>
+        <Box style={{ padding: 5, background: '#34344A', marginBottom: 10, color: '#CC5A71' }}>
+            <Typography variant="body2" component="body2" >Compare Texts</Typography>
+        </Box>
     );
 }
 
