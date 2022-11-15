2666fbcea5939d5f192986f4be5c1f6ea7ff0640
Tina <tinzzy.cracked@yahoo.com>
2022-11-15 18:32:35 -0500

    changed the style of displaying differences, differences are shown by colors.

--- file-diff/src/components/ShowResults.js
+++ file-diff/src/components/ShowResults.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 
 const keyValue = {
@@ -8,17 +9,60 @@ const keyValue = {
 }
 
 export default function ShowResults(props) {
+
+    let negatives = {};
+    for (let l in props.diff) {
+        let line = props.diff[l];
+        for (let w in line) {
+            if (line[w].sign === '-') {
+                negatives[line[w].position] = line[w].word;
+            }
+        }
+    }
+
+    let positives = [];
+    for (let l in props.diff) {
+        let line = props.diff[l];
+        for (let w in line) {
+            if (line[w].sign === '+') {
+                positives.push(line[w].word);
+            }
+        }
+    }
+
+    let allWords = [];
+    let splitText = props.text.split(' ');
+    for (let w in splitText) {
+        allWords.push(splitText[w].replace('\n', ''))
+    }
+
     return (
-        <Box style={{ display: 'inline-block', marginRight: 10, width: 400, marginTop: 15, marginBottom: 15, border: 'solid 1px #eaeaea', height: '100%' }}>
-            <Box style={{ width: 400, textAlign: 'left', height: '100%', display: 'flex', flexDirection: 'column', paddingTop: 10, paddingLeft: 10, paddingBottom: 10 }}>
-                <Typography variant="body2" component="div">
-                    {props.diff.map((line, l) => (<div key={l}>
-                        {line.map((diff, d) => (<div key={d}>
-                            {diff.sign} {diff.word} @ index {diff.position}
-                        </div>))}
+        <Box style={{ width: '100%', textAlign: 'left', height: '100%', paddingTop: 10, paddingLeft: 10, paddingBottom: 10 }}>
+            {allWords.map((w, i) => (<span key={i} style={{ color: positives.includes(w) ? 'green' : ' #353535' }}>
+                {negatives.hasOwnProperty(i + '') ? <span style={{ color: 'red' }}>{negatives[i + ''] + ' '}</span> : null}
+                {w + ' '}
+            </span>))}
+
+            <Divider />
+            <Divider />
+
+            <Typography variant="body2" component="div">
+                {props.diff.map((line, l) => (<div key={l}>
+                    {line.map((diff, d) => (<div key={d}>
+                        {diff.sign} {diff.word} @line {diff.lineNo} @position {diff.position}
                     </div>))}
-                </Typography>
-            </Box>
+                </div>))}
+            </Typography>
+
+
+            {/* 
+            <Divider />
+            {JSON.stringify(negatives)}
+            <Divider />
+            {JSON.stringify(positives)}
+            <Divider />
+            {JSON.stringify(allWords)} 
+            */}
         </Box>
     );
 }
