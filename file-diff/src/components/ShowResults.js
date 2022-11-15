6e031553d7653bd12881b6a46e94aa280cf72c70
Tina <tinzzy.cracked@yahoo.com>
2022-11-15 17:15:38 -0500

    mapping on the results to show each lines differences separately

--- file-diff/src/components/ShowResults.js
+++ file-diff/src/components/ShowResults.js
@@ -3,12 +3,20 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+const keyValue = {
+    marginRight: 10,
+}
+
 export default function ShowResults(props) {
     return (
         <Box style={{ display: 'inline-block', marginRight: 10, width: 400, marginTop: 15, marginBottom: 15 }}>
             <Box style={{ width: 400, textAlign: 'left' }}>
                 <Typography variant="body2" component="div" >
-                    {JSON.stringify(props.diff)}
+                    {props.diff.map((line, l) => (<div key={l}>
+                        {line.map((diff, d) => (<div key={d}>
+                            {diff.sign} {diff.word} @ index {diff.position}
+                        </div>))}
+                    </div>))}
                 </Typography>
             </Box>
         </Box>
