b6058bda04235f0b2280090b9f69be4cace39e1a
Tina <tinzzy.cracked@yahoo.com>
2022-11-15 17:33:01 -0500

    fixed some style issues

--- file-diff/src/components/ShowResults.js
+++ file-diff/src/components/ShowResults.js
@@ -9,9 +9,9 @@ const keyValue = {
 
 export default function ShowResults(props) {
     return (
-        <Box style={{ display: 'inline-block', marginRight: 10, width: 400, marginTop: 15, marginBottom: 15 }}>
-            <Box style={{ width: 400, textAlign: 'left' }}>
-                <Typography variant="body2" component="div" >
+        <Box style={{ display: 'inline-block', marginRight: 10, width: 400, marginTop: 15, marginBottom: 15, border: 'solid 1px #eaeaea', height: '100%' }}>
+            <Box style={{ width: 400, textAlign: 'left', height: '100%', display: 'flex', flexDirection: 'column', paddingTop: 10, paddingLeft: 10, paddingBottom: 10 }}>
+                <Typography variant="body2" component="div">
                     {props.diff.map((line, l) => (<div key={l}>
                         {line.map((diff, d) => (<div key={d}>
                             {diff.sign} {diff.word} @ index {diff.position}
