355643b17ed6c973a131410944bfc2f1750280ec
Tina <tinzzy.cracked@yahoo.com>
2022-11-15 17:06:42 -0500

    imported differences, created theme for a primary coior for mui button

--- file-diff/src/App.js
+++ file-diff/src/App.js
@@ -4,10 +4,21 @@ import './App.css';
 import FileViewer from './components/FileViewer';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ShowResults from './components/ShowResults';
+
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { compareTexts } from './components/text-compare';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#c2185b'
+    }
+  },
+});
 
 let allfiles = {
 }
@@ -20,40 +31,37 @@ function fileViewer2CallBack(file) {
   allfiles.f2 = file;
 }
 
-function ShowResult(props) {
-  return (<>
-    <div style={{ padding: 10, border: 'solid 1px gray', margin: 10}}>
-      {JSON.stringify(props.diff)}
-    </div>
-  </>);
-}
-
 function App() {
   const [diff1, setDiff1] = useState([]);
   const [diff2, setDiff2] = useState([]);
 
   function compare() {
     let result = compareTexts(allfiles.f1, allfiles.f2);
+
     setDiff1(result.diff1);
     setDiff2(result.diff2);
   }
 
   return (
-    <div className="App">
-      <Header />
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
 
-      <Box>
-        <Button onClick={() => compare()} variant="outlined" size="small">Compare</Button>
-      </Box>
+        <Box>
+          <Button onClick={() => compare()} variant="contained" size="small" style={{ backgroundColor: '#CC5A71', marginBottom: 10 }}>Compare</Button>
+        </Box>
 
-      <FileViewer id='file #1' callback={fileViewer1CallBack} />
-      <FileViewer id='file #2' callback={fileViewer2CallBack} />
+        <FileViewer id='file #1' callback={fileViewer1CallBack} />
+        <FileViewer id='file #2' callback={fileViewer2CallBack} />
 
-      <ShowResult diff={diff1} />
-      <ShowResult diff={diff2} />
+        <Box>
+          <ShowResults diff={diff1} />
+          <ShowResults diff={diff2} />
+        </Box>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div >
+    </ThemeProvider>
   );
 }
 
