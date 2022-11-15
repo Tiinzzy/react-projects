31ea69296b136b53e6fd6c548397a49cb95e9635
Tina <tinzzy.cracked@yahoo.com>
2022-11-15 18:29:49 -0500

    changed style of displaying content, with table to avoid complexity

--- file-diff/src/App.js
+++ file-diff/src/App.js
@@ -47,11 +47,15 @@ function cleanDiff(diff) {
 function App() {
   const [diff1, setDiff1] = useState([]);
   const [diff2, setDiff2] = useState([]);
+  const [text1, setText1] = useState(null);
+  const [text2, setText2] = useState(null);
 
   function compare() {
     let result = compareTexts(allfiles.f1, allfiles.f2);
     setDiff1(cleanDiff(result.diff1));
     setDiff2(cleanDiff(result.diff2));
+    setText1(allfiles.f1);
+    setText2(allfiles.f2);
   }
 
   return (
@@ -63,13 +67,21 @@ function App() {
           <Button onClick={() => compare()} variant="contained" size="small" style={{ backgroundColor: '#CC5A71', marginBottom: 10 }}>Compare</Button>
         </Box>
 
-        <FileViewer id='file #1' callback={fileViewer1CallBack} />
-        <FileViewer id='file #2' callback={fileViewer2CallBack} />
+        <table width='100%'>
+          <tbody>
+            <tr valign='top'>
+              <td width='50%'>
+                <FileViewer id='file #1' callback={fileViewer1CallBack} />
+                <ShowResults diff={diff1} text={text1} />
+              </td>
 
-        <Box>
-          <ShowResults diff={diff1} />
-          <ShowResults diff={diff2} />
-        </Box>
+              <td width='50%'>
+                <FileViewer id='file #2' callback={fileViewer2CallBack} />
+                <ShowResults diff={diff2} text={text2} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
         <Footer />
       </div >
