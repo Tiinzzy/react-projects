27a9a0fd913affae01e4f82941bf0c2445d19afb
Tina <tinzzy.cracked@yahoo.com>
2022-11-17 16:18:49 -0500

    fixed displaying extra words that are only in one of the texts, showing in blue

--- file-diff/src/components/ShowResults.js
+++ file-diff/src/components/ShowResults.js
@@ -23,7 +23,7 @@ export default function ShowResults(props) {
         let result = '#000000';
         for (let l in props.diff) {
             for (let [p] in props.diff[l]) {
-                if (props.diff[l][p].lineNo * 1 === line && props.diff[l][p].position * 1 === position && props.diff[l][p].word === word) {
+                if (props.diff[l][p].lineNo * 1 === line && props.diff[l][p].word === word && props.diff[l][p].position * 1 === position) {
                     if (props.diff[l][p].sign === '+') {
                         return '#08B500';
                     }
@@ -63,10 +63,13 @@ export default function ShowResults(props) {
         <Box style={{ width: '100%', textAlign: 'left', height: '100%', paddingTop: 10, paddingLeft: 10, paddingBottom: 10 }}>
             {allWords.length > 0 && <>
                 <Box style={{ marginBottom: 10 }}>
-                    {allWords.map((line, i) => (<div key={i}>
-                        {line.map((word, w) => (<span key={w} style={{ color: getPositiveColor(i, w, word), backgroundColor: getBackColor(i, w, word) }}>
-                            <span style={{ color: '#FF0000', backgroundColor: '#FFD9D9' }}> {getNegativeWord(i, w)} </span>
+                    {allWords.map((line, l) => (<div key={l}>
+                        {line.map((word, w) => (<span key={w} style={{ color: getPositiveColor(l, w, word), backgroundColor: getBackColor(l, w, word) }}>
+                            <span style={{ color: '#FF0000', backgroundColor: '#FFD9D9' }}> {getNegativeWord(l, w)} </span>
                             {word + ' '}
+                        </span>))}
+                        {props.diff[l].filter((e) => e.extra).map((diff, d) => (<span key={d}>
+                            <span style={{ color: '#0041E2', backgroundColor: "#C6DCFF" }}>{diff.word + ' '}</span>
                         </span>))}
                     </div>))}
                 </Box>
@@ -82,7 +85,7 @@ export default function ShowResults(props) {
                     </div>))}
                 </Typography>
             </>}
-            {/* {JSON.stringify(props.diff)} */}
         </Box>
     );
 }
+
