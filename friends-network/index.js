738440a0c7ad5b6e41b3cfe9189a6889bd80dfb6
Tina <tinzzy.cracked@yahoo.com>
2022-11-18 15:14:48 -0500

    fixed function that saves users friends by giving it a file name

--- friends-network/index.js
+++ friends-network/index.js
@@ -1,31 +1,38 @@
 const fs = require("fs");
 var args = process.argv.slice(2);
 
-main(args[0]);
+main(args[0], args[1]);
 
 //----------------------------------------------
-function main(user) {
+function main(user, file) {
   user = user || null;
+  file = file || null;
+
   let data = readData("./users.txt")
 
-  let friends = getFriends(data, user, 0);
-  showFriends(friends);
-  saveAsJsonFriends('friends.json', friends);
+  if (user !== null) {
+    let friends = getFriends(data, user, 0);
+    showFriends(friends);
+    if (file !== null) {
+      saveAsJsonFriends(file + '-friends.json', friends);
+    }
+  }
 }
 
 //----------------------------------------------
 function showFriends(friends) {
-  friends.forEach((e,i) => {
-    console.log(1+i, e);
+  friends.forEach((e, i) => {
+    console.log(1 + i, e);
   });
 }
 
-
 //----------------------------------------------
 function saveAsJsonFriends(file, friends) {
-
+  fs.writeFile(file, JSON.stringify(friends), function (err) {
+    if (err) throw err;
+    console.log(file + ' created successfully.');
+  });
 }
-
 
 //----------------------------------------------
 function getFriends(data, user, level) {
@@ -33,10 +40,10 @@ function getFriends(data, user, level) {
   if (level === 0) {
     data.forEach(p => {
       if (p.user === user) {
-        result = result.concat(p.friends);                
+        result = result.concat(p.friends);
       }
     });
-  } else {
+  } else if (level > 0) {
 
   }
 
