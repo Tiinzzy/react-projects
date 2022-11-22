1a302c8232c5002ca56e150662457ffd040877dd
Tina <tinzzy.cracked@yahoo.com>
2022-11-22 18:02:12 -0500

    linked pages to do separate tasks

--- simple-cost-analysis/src/App.js
+++ simple-cost-analysis/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import DisplayGrid from './components/DisplayGrid';
+import Header from './components/Header';
+import FilePicker from './components/FilePicker';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <DisplayGrid/>
+      <Header />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route paath="/upload-file" element={<FilePicker />} />
+          <Route path="/display-grid" element={<DisplayGrid />} />
+          <Route path='*' element={<h3>Error</h3>} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
