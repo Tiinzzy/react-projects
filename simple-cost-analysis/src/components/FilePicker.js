7c649262f6220cea32e62cc5fab1233593a4ce4b
Tina <tinzzy.cracked@yahoo.com>
2022-11-23 18:33:09 -0500

    code cleanup

--- simple-cost-analysis/src/components/FilePicker.js
+++ simple-cost-analysis/src/components/FilePicker.js
@@ -10,31 +10,26 @@ import { saveCsv } from './functions';
 const csv = require('csvtojson')
 
 export default function FilePicker() {
-    const [csvD, setCsvD] = useState(null);
+    const [jCsv, setJCsv] = useState(null);
     const [openFileSelector, { filesContent }] = useFilePicker({
         accept: '.CSV',
     });
 
     async function convertToCsv(content) {
         let csvData = await csv().fromString(content);
-        let id = 1;
+        let id = 1000;
         let data = {};
 
-        csvData.forEach(row => {
+        let saveData = {};
+        csvData.forEach(row => {            
             row.id = id++;
-            row.category = 'None';
+            row.CATEGORY = 'None';            
             data[row.id] = row;
-        })
+            row.AMOUNT = (row.AMOUNT*1).toFixed(2);
+            saveData[row.id] = row;
+        });
 
-        //console.log(data);
-        // console.log(Object.values(data));
-        // console.log(Object.keys(data));
-
-        // Todo tomorrow
-        // 1) write a function that returns number of rows stored in the backend!?
-        // 2) if the number of the rows are not zero ask user would you like to replace the alreadly stored data 
-        // 3) if user says yes to 2) or the backend data is nothing save the data to the backend as it IS WORKING NOW
-        setCsvD(csvData);
+        setJCsv(saveData);
     }
 
     function processContent(content) {
@@ -43,8 +38,8 @@ export default function FilePicker() {
     }
 
     async function handleSave() {
-        console.log(csvD);
-        return await saveCsv(csvD);
+        console.log(jCsv);
+        return await saveCsv(jCsv);
     }
 
     return (<>
