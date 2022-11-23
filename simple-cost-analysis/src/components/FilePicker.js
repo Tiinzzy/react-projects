c4c58c77486b57e00b847f9cdf1a82f47c70d033
Tina <tinzzy.cracked@yahoo.com>
2022-11-23 15:23:04 -0500

    fixed issue with save by assigning useSet

--- simple-cost-analysis/src/components/FilePicker.js
+++ simple-cost-analysis/src/components/FilePicker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -10,6 +10,7 @@ import { saveCsv } from './functions';
 const csv = require('csvtojson')
 
 export default function FilePicker() {
+    const [csvD, setCsvD] = useState(null);
     const [openFileSelector, { filesContent }] = useFilePicker({
         accept: '.CSV',
     });
@@ -25,8 +26,6 @@ export default function FilePicker() {
             data[row.id] = row;
         })
 
-        saveCsv(data);
-
         //console.log(data);
         // console.log(Object.values(data));
         // console.log(Object.keys(data));
@@ -35,7 +34,7 @@ export default function FilePicker() {
         // 1) write a function that returns number of rows stored in the backend!?
         // 2) if the number of the rows are not zero ask user would you like to replace the alreadly stored data 
         // 3) if user says yes to 2) or the backend data is nothing save the data to the backend as it IS WORKING NOW
-
+        setCsvD(csvData);
     }
 
     function processContent(content) {
@@ -43,11 +42,27 @@ export default function FilePicker() {
         return null;
     }
 
-    return (
-        <Box>
-            <Button onClick={() => openFileSelector()} variant="contained">Select CSV File </Button>
-            <br />
-            {filesContent.map((file, i) => (<span key={i}>{processContent(file.content)}</span>))}
+    async function handleSave() {
+        console.log(csvD);
+        return await saveCsv(csvD);
+    }
+
+    return (<>
+        <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', marginBottom: 20, marginTop: 30, marginLeft: 20 }}>
+            Would you like to save the files?
+            <Box style={{ marginLeft: 20, display: 'inline-block' }}>
+                <Button onClick={() => handleSave()} variant="contained">Save File</Button>
+            </Box>
         </Box>
+        <br />
+        <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', marginBottom: 20, marginLeft: 20 }}>
+            Click to open CSV file
+            <Box style={{ marginLeft: 20, display: 'inline-block' }}>
+                <Button onClick={() => openFileSelector()} variant="contained">Select CSV File </Button>
+                <br />
+                {filesContent.map((file, i) => (<span key={i}>{processContent(file.content)}</span>))}
+            </Box>
+        </Box>
+    </>
     );
 }
