5e039c6381a1a1b6fd2d68507ab7fdac2a230597
Tina <tinzzy.cracked@yahoo.com>
2022-11-24 10:07:40 -0500

    shows uploaded file and confirms saving

--- simple-cost-analysis/src/components/FilePicker.js
+++ simple-cost-analysis/src/components/FilePicker.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import SnackbarContent from '@mui/material/SnackbarContent';
 
 import { useFilePicker } from 'use-file-picker';
 
@@ -11,6 +13,8 @@ const csv = require('csvtojson')
 
 export default function FilePicker() {
     const [jCsv, setJCsv] = useState(null);
+    const [openSnack, setOpenSnack] = useState(false);
+    const [message, setMessage] = useState(null);
     const [openFileSelector, { filesContent }] = useFilePicker({
         accept: '.CSV',
     });
@@ -21,11 +25,11 @@ export default function FilePicker() {
         let data = {};
 
         let saveData = {};
-        csvData.forEach(row => {            
+        csvData.forEach(row => {
             row.id = id++;
-            row.CATEGORY = 'None';            
+            row.CATEGORY = 'None';
             data[row.id] = row;
-            row.AMOUNT = (row.AMOUNT*1).toFixed(2);
+            row.AMOUNT = (row.AMOUNT * 1).toFixed(2);
             saveData[row.id] = row;
         });
 
@@ -39,25 +43,46 @@ export default function FilePicker() {
 
     async function handleSave() {
         console.log(jCsv);
+        setOpenSnack(true);
         return await saveCsv(jCsv);
     }
 
+    function handleCloseSnack() {
+        setOpenSnack(false);
+        setMessage(null);
+    }
+
     return (<>
-        <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', marginBottom: 20, marginTop: 30, marginLeft: 20 }}>
-            Would you like to save the files?
-            <Box style={{ marginLeft: 20, display: 'inline-block' }}>
-                <Button onClick={() => handleSave()} variant="contained">Save File</Button>
+        <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', marginBottom: 20, marginTop: 30, marginLeft: 20, alignItems: 'center'}}>
+            Click to open CSV file
+            <Box style={{ marginLeft: 20, display: 'flex', flexDirection: 'row' }}>
+                <Button onClick={() => openFileSelector()} variant="outlined">Select CSV File </Button>
+                <br />
+                <span style={{ marginLeft: 30, paddingTop: 10, fontWeight: 'bold' }}>File Name:</span>
+                {filesContent.map((file, i) => (
+                    <Box key={i} style={{ marginLeft: 20, paddingTop: 10 }}>{processContent(file.content)}
+                        {file.name}
+                    </Box>))}
             </Box>
         </Box>
         <br />
-        <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', marginBottom: 20, marginLeft: 20 }}>
-            Click to open CSV file
+
+        <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', marginBottom: 20, marginLeft: 20, alignItems: 'center'}}>
+            Would you like to save the files?
             <Box style={{ marginLeft: 20, display: 'inline-block' }}>
-                <Button onClick={() => openFileSelector()} variant="contained">Select CSV File </Button>
-                <br />
-                {filesContent.map((file, i) => (<span key={i}>{processContent(file.content)}</span>))}
+                <Button onClick={() => handleSave()} variant="outlined">Save File</Button>
             </Box>
         </Box>
+
+        <Snackbar
+            anchorOrigin={{ vertical: 'top', horizontal: 'center', }}
+            open={openSnack}
+            autoHideDuration={2000}
+            onClose={() => handleCloseSnack()}>
+
+            <SnackbarContent style={{ backgroundColor: '#63A355', color: 'white', textAlign: 'center', fontWeight: 'bold' }}
+                message='File Saved Sucessfully' />
+        </Snackbar>
     </>
     );
 }
