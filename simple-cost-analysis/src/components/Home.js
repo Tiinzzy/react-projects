978d9c46460e54c77973561ebf4cba3782a70f05
Tina <tinzzy.cracked@yahoo.com>
2022-11-25 16:35:44 -0500

    wrote instructions

--- simple-cost-analysis/src/components/Home.js
+++ simple-cost-analysis/src/components/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import Box from '@mui/material/Box';
 
+import './style.css';
+
 class Home extends React.Component {
 
     constructor(props) {
@@ -14,8 +16,15 @@ class Home extends React.Component {
     render() {
         return (
             <>
-                <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', marginLeft: 10, marginTop: 10, marginBottom: 10 }}>
-                   This will be home
+                <Box className="HomeBox">
+                    <p>1.First go to Upload File</p>
+                    <p>2.Select a file to uploead using the button</p>
+                    <p>2.a. THe file must be CSV to be read</p>
+                    <p>3.When your file is uploaded, you will see a display of it</p>
+                    <p>4.There is also a display of any pervious data if it existed from before</p>
+                    <p>5.If youre satisfied with your file click on save button</p>
+                    <p>6.Once the pop-up comes up, you will be notified that saving new file will remove any previous data</p>
+                    <p>7.AFter saving the file new data will be available and you can see them in Show Data</p>
                 </Box>
 
             </>
