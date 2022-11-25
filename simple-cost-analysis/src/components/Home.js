d615f378fc93cef10999986ab57d230b10470a57
Tina <tinzzy.cracked@yahoo.com>
2022-11-25 17:05:53 -0500

    added content to an array to map and read and assign to list

--- simple-cost-analysis/src/components/Home.js
+++ simple-cost-analysis/src/components/Home.js
@@ -4,6 +4,17 @@ import Box from '@mui/material/Box';
 
 import './style.css';
 
+const HELP = [
+    "First go to Upload File",
+    "Select a file to uploead using the button",
+    "The file must be CSV to be read",
+    "When your file is uploaded, you will see a display of it",
+    "There is also a display of any pervious data if it existed from before",
+    "If youre satisfied with your file click on save button",
+    "Once the pop-up comes up, you will be notified that saving new file will remove any previous data",
+    "AFter saving the file new data will be available and you can see them in Show Data"
+];
+
 class Home extends React.Component {
 
     constructor(props) {
@@ -12,22 +23,15 @@ class Home extends React.Component {
         }
     }
 
-
     render() {
         return (
-            <>
-                <Box className="HomeBox">
-                    <p>1.First go to Upload File</p>
-                    <p>2.Select a file to uploead using the button</p>
-                    <p>2.a. THe file must be CSV to be read</p>
-                    <p>3.When your file is uploaded, you will see a display of it</p>
-                    <p>4.There is also a display of any pervious data if it existed from before</p>
-                    <p>5.If youre satisfied with your file click on save button</p>
-                    <p>6.Once the pop-up comes up, you will be notified that saving new file will remove any previous data</p>
-                    <p>7.AFter saving the file new data will be available and you can see them in Show Data</p>
-                </Box>
-
-            </>
+            <Box className="HomeBox">
+                <ol>
+                    {HELP.map((e, i) => (
+                        <li key={i}>{e}</li>
+                    ))}
+                </ol>
+            </Box>
         );
     }
 }
