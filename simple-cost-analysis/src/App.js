aed9897ce2bd558ad1d0eb626215bcc52f1cd496
Tina <tinzzy.cracked@yahoo.com>
2022-11-28 10:19:46 -0500

    going to start from begining

--- simple-cost-analysis/src/App.js
+++ simple-cost-analysis/src/App.js
@@ -1,24 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import DisplayGrid from './components/DisplayGrid';
-import Header from './components/Header';
-import SaveFile from './components/SaveFile';
-import Home from './components/Home';
 
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header />
 
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/upload-save-file" element={<SaveFile />} />
-          <Route path="/display-grid" element={<DisplayGrid />} />
-          {/* <Route path='*' element={<h3>Error</h3>} /> */}
-        </Routes>
-      </BrowserRouter>
+          <Route path="/" element={ } />
+          <Route path="/" element={ } />
+          <Route path="/" element={<>} />
+          <Route path='*' element={<h3>Error</h3>} />
+          </Routes>
+      </BrowserRouter> */}
 
     </div>
   );
