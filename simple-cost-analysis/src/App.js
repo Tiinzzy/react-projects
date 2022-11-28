aa569ec015a093697d48d012e8993b9a6cd255af
Tina <tinzzy.cracked@yahoo.com>
2022-11-28 17:45:54 -0500

    added footer and components for path and route

--- simple-cost-analysis/src/App.js
+++ simple-cost-analysis/src/App.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
+import FileDisplay from "./components/FileDisplay";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+
+
+export function appCallBack(file) {
+  console.log('>>>> APP.js >> appCallBack');
+  console.log(file);
+}
+
+function callback(file) {
+  console.log('>>>> APP.js');
+  console.log(file);
+}
 
 function App() {
   return (
-    <div>
-      {/* <Header />
-
+    <>
+      <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ } />
-          <Route path="/" element={ } />
-          <Route path="/" element={<>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/upload-save-display-file" element={<FileDisplay callback={callback} />} />
           <Route path='*' element={<h3>Error</h3>} />
-          </Routes>
-      </BrowserRouter> */}
-
-    </div>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
