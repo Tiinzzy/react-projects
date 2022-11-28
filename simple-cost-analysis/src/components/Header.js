4783b33a67d3e845621464e31a9f1e459027bc3c
Tina <tinzzy.cracked@yahoo.com>
2022-11-28 10:45:00 -0500

    component to have header and change page content

---
+++ simple-cost-analysis/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Divider from '@mui/material/Divider';
+
+import './design.css';
+
+const headerStyle = {
+    cursor: 'pointer',
+    color: '#1c4966',
+    TextDecoder: 'none'
+}
+
+class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <Box className="HeaderBox">
+                    <Link href="/" style={headerStyle}>Home</Link>
+                    <Divider orientation="vertical" variant="middle" flexItem sx={{ ml: 2, mr: 2 }} />
+                    <Link href="/upload-save-file" style={headerStyle}> Files</Link>
+                </Box>
+                <Divider flexItem sx={{ borderRightWidth: 5 }} color="#1c4966" />
+            </>
+        );
+    }
+}
+export default Header;
