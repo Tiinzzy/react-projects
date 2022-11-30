9a32669c4537ab3498ab67afc5e0f89f6c725704
Tina <tinzzy.cracked@yahoo.com>
2022-11-29 19:25:37 -0500

    select file, dialog opens to display the content, after saving snackbar confirms it

--- simple-cost-analysis/src/components/FilePicker.js
+++ simple-cost-analysis/src/components/FilePicker.js
@@ -1,35 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Dialog from "@mui/material/Dialog";
+import Snackbar from '@mui/material/Snackbar';
+import SnackbarContent from '@mui/material/SnackbarContent';
+
+import Save from './Save';
 
 import { useFilePicker } from 'use-file-picker';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 
-import { appCallBack } from '../App';
+const csv = require('csvtojson');
 
-function passContentToParent(callback, file) {
-    // callback(file);
-    appCallBack(file);
-    return file.name;
+const headerStyle = {
+    cursor: 'pointer',
+    color: '#1c4966',
+    TextDecoder: 'none'
 }
 
 export default function FilePicker(props) {
-    const [openFileSelector, { filesContent, loading }] = useFilePicker({
+    const [jCsv, setJCsv] = useState(null);
+    const [openSnack, setOpenSnack] = useState(false);
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [gridRef,] = useState(React.createRef());
+    const [showSaveMessage, setShowSaveMessage] = useState(false);
+
+    const [openFileSelector, { filesContent, clear }] = useFilePicker({
         accept: '.CSV',
     });
 
+    async function convertToCsv(content) {
+        let csvData = await csv().fromString(content);
+        let id = 1000;
+        let data = {};
+
+        let saveData = {};
+        csvData.forEach(row => {
+            row.id = id++;
+            row.CATEGORY = 'None';
+            data[row.id] = row;
+            row.AMOUNT = (row.AMOUNT * 1).toFixed(2);
+            saveData[row.id] = row;
+        });
+
+        setJCsv(saveData);
+
+        if (gridRef.current !== null) {
+            gridRef.current.refreshGrid(saveData);
+        }
+
+        clear();
+        setDialogOpen(true)
+    }
+
+    function processContent(content) {
+        convertToCsv(content);
+        return null;
+    }
+
+    function handleCloseDialog() {
+        setOpenSnack(true);
+        setDialogOpen(false);
+    }
+
+    function handleCloseSnack() {
+        setOpenSnack(false);
+    }
+
+    function saveCallBack(saved) {
+        setShowSaveMessage(saved);
+    }
+
     return (
         <>
-            <Box className='FilePickerBox'>
-                <Box className='FilePickerButton'>
-                    <Button variant="outlined" onClick={() => openFileSelector()}>Select file </Button>
-                </Box>
-                <Box className='FilePickerName'>
-                    File Name:
-                    {filesContent.map((file, index) => (
-                        <Box key={index} pl={1}> {passContentToParent(props.callFileDisplay, file)}</Box>
-                    ))}
-                </Box>
+            <Box>
+                <Link variant="text" onClick={() => openFileSelector()} style={headerStyle}>Select File </Link>
+                {filesContent.map((file, i) => (
+                    <Box key={i} className='FilePickerFileName2 '>{processContent(file.content, i)}</Box>))}
             </Box>
+
+            {dialogOpen && <Dialog onClose={() => handleCloseDialog()} open={dialogOpen} maxWidth="lg" fullWidth={true}>
+                <Save jCsv={jCsv} handleCloseDialog={handleCloseDialog} callback={saveCallBack} />
+            </Dialog>}
+
+            {showSaveMessage && <Snackbar
+                anchorOrigin={{ vertical: 'top', horizontal: 'center', }}
+                open={openSnack}
+                autoHideDuration={2000}
+                onClose={handleCloseSnack}>
+                <SnackbarContent style={{ backgroundColor: '#63A355', color: 'white', fontWeight: 'bold' }}
+                    message={<div style={{ textAlign: 'center', width: 400 }}>File Saved Sucessfully</div>} />
+            </Snackbar>}
         </>
 
     );
