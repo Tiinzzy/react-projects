d8fe5e29d0d9ab9a02ba78a0cbf3a8d2ac472f32
Tina <tinzzy.cracked@yahoo.com>
2022-12-09 17:31:01 -0500

    added new stacked graph

--- simple-cost-analysis/src/components/GraphDisplay.js
+++ simple-cost-analysis/src/components/GraphDisplay.js
@@ -1,27 +1,38 @@
 import React from "react";
 
 import { Chart } from "react-google-charts";
-
 import Box from '@mui/material/Box';
 
 import './design.css';
 
 export default function GraphDisplay(props) {
+    var options = {
+        isStacked: true,
+        bars: "horizontal",
+        bar: { groupWidth: '75%' },
+        legend: { position: 'top', maxLines: 3 },
+        chartArea: {'width': '85%', 'height': '75%'}
+    };
 
     return (
         <Box style={{ margin: 20 }}>
             {props.graphIndex === 1 && <Chart
                 chartType="Bar"
-                width="100%"
-                height="500px"
+                height={window.innerHeight / 2}
                 data={props.dailyData}
             />}
 
             {props.graphIndex === 2 && <Chart
                 chartType="Bar"
-                width="100%"
-                height="500px"
+                height={window.innerHeight / 2}
                 data={props.weekDaysData}
+            />}
+
+            {props.graphIndex === 3 && <Chart
+                chartType="ColumnChart"
+                height={window.innerHeight / 2}
+                data={props.detailedSummary}
+                options={options}
             />}
         </Box>
     );
