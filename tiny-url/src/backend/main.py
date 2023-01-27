5793930abfb48d31ca5242cf770abbd895af93fa
Tina <tinzzy.cracked@yahoo.com>
2023-01-27 16:34:25 -0500

    created rest api to connect to frontend and get and send data

---
+++ tiny-url/src/backend/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from tiny_url import Tiny_Url
+
+app = Flask(__name__)
+
+@app.route("/getting_url_and_convert", methods=['GET'])
+def getting_url_and_convert():
+    args = request.args
+    data = Tiny_Url.submit_and_get_url(
+        args.get('mainUrl'), args.get('tinyUrl'))
+    return jsonify(data)
+
+
+@app.route("/get_url", methods=['GET'])
+def get_url():
+    args = request.args
+    data = Tiny_Url.get_url(args.get('tinyUrl'))
+    print(data)
+    return jsonify(data)
+
+
