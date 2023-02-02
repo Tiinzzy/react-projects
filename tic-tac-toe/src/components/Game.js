7fbd01b8bd3248f6158456f3707b9be5458515e2
Tina <tinzzy.cracked@yahoo.com>
2023-02-02 18:55:36 -0500

    added button for play

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box'
 import './style.css';
 
 import { GAME_BODY, insertIntoCurrentHouses, CURRENT_FULL_HOUSES, ALL_HOUSES, findNextMove, WINNING_POSSIBILITIES } from './constants';
+import { Button } from "@mui/material";
 
 const DELAY_TIME = 3 * 1000;
 
@@ -14,18 +15,25 @@ class Game extends React.Component {
         super(props);
         this.state = {
             message: 'User\'s turn',
-            firstTime: true
+            firstTime: true,
+            play: false,
+            display: true
 
         }
         this.houseClicked = this.houseClicked.bind(this);
+        this.startGame = this.startGame.bind(this);
     }
-
+    startGame() {
+        this.setState({ play: true, display: false })
+    }
     houseClicked(e) {
         insertIntoCurrentHouses(e, 'X')
         document.getElementById(e);
         document.getElementById(e).textContent = "X";
         this.computerMove();
         this.setState({ message: 'Computer\'s turn' });
+
+        const found = WINNING_POSSIBILITIES.some(r => CURRENT_FULL_HOUSES.indexOf(r) > 0);
 
     }
 
@@ -42,11 +50,12 @@ class Game extends React.Component {
         } else {
             setTimeout(() => {
                 let nextMoveIndex = findNextMove();
-                console.log(nextMoveIndex)
                 let nextMove = ALL_HOUSES[nextMoveIndex];
-                insertIntoCurrentHouses(nextMove, 'O')
-                document.getElementById(nextMove).textContent = "O";
-                insertIntoCurrentHouses(nextMove, 'O');
+                console.log('error -->', nextMove)
+                if (nextMove !== undefined) {
+                    insertIntoCurrentHouses(nextMove, 'O');
+                    document.getElementById(nextMove).textContent = "O";
+                }
             }, DELAY_TIME);
         }
     }
@@ -54,28 +63,35 @@ class Game extends React.Component {
     render() {
         return (
             <Box id="main-div" className="GameBody">
-                <Box className="PlayerTurn">{this.state.message}</Box>
-                <Box className="EachMapping">
-                    {GAME_BODY[0].map((rows, index) =>
-                        < Box id={rows} key={index}
-                            onClick={() => this.houseClicked(rows)}
-                            className="EachRow">
-                        </Box>)}
-                </Box>
-                <Box className="EachMapping">
-                    {GAME_BODY[1].map((rows, index) =>
-                        < Box id={rows} key={index}
-                            onClick={() => this.houseClicked(rows)}
-                            className="EachRow">
-                        </Box>)}
-                </Box>
-                <Box className="EachMapping">
-                    {GAME_BODY[2].map((rows, index) =>
-                        < Box id={rows} key={index}
-                            onClick={() => this.houseClicked(rows)}
-                            className="EachRow">
-                        </Box>)}
-                </Box>
+                {this.state.display === true &&
+                    <Button variant="contained" className="StartBtn" onClick={() => this.startGame()}>Start</Button>}
+                {this.state.play === true &&
+                    <Box className="PlayerTurn">
+                        {this.state.message}</Box>}
+                {this.state.play === true &&
+                    <Box className="EachMapping">
+                        {GAME_BODY[0].map((rows, index) =>
+                            < Box id={rows} key={index}
+                                onClick={() => this.houseClicked(rows)}
+                                className="EachRow">
+                            </Box>)}
+                    </Box>}
+                {this.state.play === true &&
+                    <Box className="EachMapping">
+                        {GAME_BODY[1].map((rows, index) =>
+                            < Box id={rows} key={index}
+                                onClick={() => this.houseClicked(rows)}
+                                className="EachRow">
+                            </Box>)}
+                    </Box>}
+                {this.state.play === true &&
+                    <Box className="EachMapping">
+                        {GAME_BODY[2].map((rows, index) =>
+                            < Box id={rows} key={index}
+                                onClick={() => this.houseClicked(rows)}
+                                className="EachRow">
+                            </Box>)}
+                    </Box>}
                 <Box id="final-result" className="PlayerTurn" marginTop={2}></Box>
             </Box>
         );
