04fbff15440b1b53b3f5fd58427d4afa91465f70
Tina <tinzzy.cracked@yahoo.com>
2023-02-04 17:13:41 -0500

    different icon and message if there is a draw result

--- tic-tac-toe/src/components/OutcomeDialog.js
+++ tic-tac-toe/src/components/OutcomeDialog.js
@@ -3,6 +3,7 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import CelebrationIcon from '@mui/icons-material/Celebration';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 
 import './style.css';
 
@@ -12,18 +13,27 @@ class OutcomeDialog extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            outcomeMessage: props.outcomeMessage
+            outcomeMessage: props.outcomeMessage,
+            showSad: false,
+            showCongratulations: false
 
         }
     }
-
+    componentDidMount() {
+        if (this.state.outcomeMessage === 'Draw Result!') {
+            this.setState({ showSad: true });
+        } else if (this.state.outcomeMessage === 'Computer Wins!') {
+            this.setState({ showCongratulations: true });
+        }
+    }
 
     render() {
         return (
             <Box className="DialogContent">
-                <CelebrationIcon fontSize="large" style={{ marginRight: 30 }} />
+                {this.state.showCongratulations === true && <CelebrationIcon fontSize="large" style={{ marginRight: 30 }} />}
                 <Typography variant="h3" fontSize={25}>{this.state.outcomeMessage}</Typography>
-                <CelebrationIcon className="CelbrateImg" fontSize="large" style={{ marginLeft: 30 }} />
+                {this.state.showSad === true && <SentimentVeryDissatisfiedIcon fontSize="large" style={{ marginLeft: 30 }} />}
+                {this.state.showCongratulations === true && <CelebrationIcon className="CelbrateImg" fontSize="large" style={{ marginLeft: 30 }} />}
             </Box>
         );
     }
