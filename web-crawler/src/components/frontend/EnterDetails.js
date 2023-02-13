4274c0c18ad05191eeafa4378cfd66de00b6662d
Tina <tinzzy.cracked@yahoo.com>
2023-02-13 14:01:43 -0500

    component with texfields to get data from user for crawling

---
+++ web-crawler/src/components/frontend/EnterDetails.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button";
+
+
+class EnterDetails extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    setUrl(e) {
+        this.setState({ url: e.target.value });
+    }
+
+    setDepth(e) {
+        this.setState({ depth: e.target.value });
+    }
+
+    setSearch(e) {
+        this.setState({ searchNum: e.target.value });
+    }
+
+    sendDataToBackend() {
+        console.log(this.state.url, this.state.depth, this.state.searchNum)
+    }
+
+    render() {
+        return (
+            <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', width: 600, marginTop: 30 }}>
+                <Typography>Enter URL</Typography>
+                <TextField size="small" variant="outlined" style={{ marginBottom: 20 }} onChange={(e) => this.setUrl(e)} />
+
+                <Typography>Depth of Search</Typography>
+                <TextField size="small" variant="outlined" style={{ marginBottom: 20 }} onChange={(e) => this.setDepth(e)} />
+
+                <Typography>Number of Search</Typography>
+                <TextField size="small" variant="outlined" style={{ marginBottom: 20 }} onChange={(e) => this.setSearch(e)} />
+
+                <Box style={{ display: 'flex', justifyContent: 'right' }}>
+                    <Button variant="contained" onClick={() => this.sendDataToBackend()}>Submit</Button>
+                </Box>
+            </Box>
+        );
+    }
+}
+export default EnterDetails;
