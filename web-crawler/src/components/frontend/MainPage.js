3cff508ec0a4b4a725683b2ffab5ef91bbcb173b
Tina <tinzzy.cracked@yahoo.com>
2023-02-13 14:01:23 -0500

    placed details components in the mainpage

--- web-crawler/src/components/frontend/MainPage.js
+++ web-crawler/src/components/frontend/MainPage.js
@@ -3,6 +3,7 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+import EnterDetails from './EnterDetails';
 
 class MainPage extends React.Component {
 
@@ -15,8 +16,9 @@ class MainPage extends React.Component {
 
     render() {
         return (
-            <Box >
-                this is my main page
+            <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: 50 }}>
+                <Typography variant="h4" fontSize={20} fontWeight="bold">Web Crawler</Typography>
+                <EnterDetails />
             </Box>
         );
     }
