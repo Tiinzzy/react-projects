c50289108fc52f492fa3df9a425326281fd5a809
Tina <tinzzy.cracked@yahoo.com>
2023-02-13 17:58:13 -0500

    encoded url to send to backend

--- web-crawler/src/components/frontend/EnterDetails.js
+++ web-crawler/src/components/frontend/EnterDetails.js
@@ -5,6 +5,8 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from "@mui/material/Button";
 
+import { Base64 } from 'js-base64';
+
 import BackEndConnection from './BackEndConnection';
 const backend = BackEndConnection.INSTANCE();
 
@@ -13,7 +15,9 @@ class EnterDetails extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            url: 'https://www.bbc.com',
+            depth: 3,
+            searchNum: 3
         }
     }
 
@@ -30,20 +34,25 @@ class EnterDetails extends React.Component {
     }
 
     sendDataToBackend() {
-        backend.get_crawling_result(this.state.url, this.state.depth, this.state.searchNum);
+        let url = Base64.encode(this.state.url);
+        // console.log(this.state.url, this.state.depth, this.state.searchNum);
+
+        backend.trigger_crawling(url, this.state.depth, this.state.searchNum, (data) => {
+            console.log(data);
+        });
     }
 
     render() {
         return (
             <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', width: 600, marginTop: 30 }}>
                 <Typography>Enter URL</Typography>
-                <TextField size="small" variant="outlined" style={{ marginBottom: 20 }} onChange={(e) => this.setUrl(e)} />
+                <TextField size="small" variant="outlined" value={this.state.url} style={{ marginBottom: 20 }} onChange={(e) => this.setUrl(e)} />
 
                 <Typography>Depth of Search</Typography>
-                <TextField size="small" variant="outlined" style={{ marginBottom: 20 }} onChange={(e) => this.setDepth(e)} />
+                <TextField size="small" variant="outlined" value={this.state.depth} style={{ marginBottom: 20 }} onChange={(e) => this.setDepth(e)} />
 
                 <Typography>Number of Search</Typography>
-                <TextField size="small" variant="outlined" style={{ marginBottom: 20 }} onChange={(e) => this.setSearch(e)} />
+                <TextField size="small" variant="outlined" value={this.state.searchNum} style={{ marginBottom: 20 }} onChange={(e) => this.setSearch(e)} />
 
                 <Box style={{ display: 'flex', justifyContent: 'right' }}>
                     <Button variant="contained" onClick={() => this.sendDataToBackend()}>Submit</Button>
