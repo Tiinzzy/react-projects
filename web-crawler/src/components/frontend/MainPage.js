88d5f384decd475aba35c6972dde2fab6a303726
Tina <tinzzy.cracked@yahoo.com>
2023-02-13 13:41:31 -0500

    created component to display data and add the details

---
+++ web-crawler/src/components/frontend/MainPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+class MainPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <Box >
+                this is my main page
+            </Box>
+        );
+    }
+}
+export default MainPage;
