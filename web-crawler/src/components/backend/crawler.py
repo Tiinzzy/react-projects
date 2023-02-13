468017704c6a0a6730a24d9ac7ed38169a234f68
Tina <tinzzy.cracked@yahoo.com>
2023-02-13 17:58:43 -0500

    added functions to initial crawler, add data to json and end the crawler

--- web-crawler/src/components/backend/crawler.py
+++ web-crawler/src/components/backend/crawler.py
@@ -1,4 +1,5 @@
 import ssl
+import sys
 import random
 from bs4 import BeautifulSoup
 import urllib.request
@@ -12,16 +13,16 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-already_seen_urls = []
+crawl_result = {'urls': [], 'finished': False, 'proccess_is_running': False}
 
 
 def open_and_read_url(url):
     try:
         html = urllib.request.urlopen(url, context=ctx).read()
         soup = BeautifulSoup(html, 'lxml')
-        return html, soup
+        return soup
     except:
-        return None, None
+        return None
 
 
 def look_for_a_tag(aTag):
@@ -34,41 +35,63 @@ def look_for_a_tag(aTag):
 def look_for_href(start_url, count):
     all_urls = []
 
-    html, soup = open_and_read_url(start_url)
+    soup = open_and_read_url(start_url)
+    if soup is None:
+        return all_urls
+
     for aTag in soup.recursiveChildGenerator():
         if look_for_a_tag(aTag):
             raw_url = start_url[:-1] if start_url.endswith('/') else start_url
             if 'href' in aTag.attrs.keys():
                 href = aTag.attrs['href']
-                if href.startswith('http'):
+                if href.startswith('https'):
                     href = href
                 elif href.startswith('//'):
                     href = 'https://' + href[2:]
                 else:
                     href = raw_url + href
-                if href not in already_seen_urls and href is not None:
-                    already_seen_urls.append(href)
-                    all_urls.append(href)
-    return random.choices(all_urls, k=count)
+            all_urls.append(href)
+    if len(all_urls) < count:
+        return all_urls
+    else:
+        return random.choices(all_urls, k=count)
 
 
-def crawl_the_web(start_url, depth, count, level):
+def crawl_url(start_url, depth, count, level=0):
+    depth = int(depth)
+    count = int(count)
     if level > depth:
         return
     i = 0
     for url in start_url:
-        print(url)
-        if url is not None:
-            random_urls = look_for_href(url, count)
-        # print(i, random_urls)
-        # print()
+        print(i, '-'*level, url)
+        crawl_result['urls'].append({'index': i, 'level': level, 'url': url})
+        random_urls = look_for_href(url, count)
+        crawl_url(random_urls, depth, count, level+1)
         i += 1
-        crawl_the_web(random_urls, depth, count, level+1)
+
+
+def init_crawl():
+    crawl_result['urls'] = []
+    crawl_result['finished'] = False
+    crawl_result['proccess_is_running'] = True
+
+
+def mark_crawl_ended():
+    crawl_result['finished'] = True
+    crawl_result['proccess_is_running'] = False
+
+
+def get_crawl_result():
+    return crawl_result
 
 
 if __name__ == "__main__":
-    # start_url = input('please enter a url: ')
+    if len(sys.argv) > 1:
+        start_url = sys.argv[1]
+    else:
+        start_url = input('please enter a url: ')
 
-    start_url = 'https://en.wikipedia.org/wiki/cat'
+    # start_url = "https://www.cnn.com/"
 
-    crawl_the_web([start_url], depth=3, count=2, level=0)
+    crawl_url([start_url], depth=3, count=5, level=0)
