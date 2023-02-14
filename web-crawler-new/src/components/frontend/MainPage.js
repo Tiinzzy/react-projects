2048ddf60c64df92592c1e09837e15f0e74b2d8d
Tina <tinzzy.cracked@yahoo.com>
2023-02-14 18:12:43 -0500

    trying to fix merge conflict

---
+++ web-crawler-new/src/components/frontend/MainPage.js
@@ -0,0 +1,28 @@
+
+
+import React from "react";
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+import EnterDetails from './EnterDetails';
+
+class MainPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: 50 }}>
+                <Typography variant="h4" fontSize={20} fontWeight="bold">Web Crawler</Typography>
+                <EnterDetails />
+            </Box>
+        );
+    }
+}
+export default MainPage;
