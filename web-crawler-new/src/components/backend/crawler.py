1a7efd41d47baa89878bfb1b7b74cb1ad4351f63
Tina <tinzzy.cracked@yahoo.com>
2023-02-15 18:01:16 -0500

    sending the path, root, main url id to frontend for making a tree

--- web-crawler-new/src/components/backend/crawler.py
+++ web-crawler-new/src/components/backend/crawler.py
@@ -14,16 +14,28 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-crawl_result = {'urls': [], 'finished': False, 'proccess_is_running': False}
+crawl_result = {'urls': [], 'finished': False,
+                'proccess_is_running': False, 'already_seen_urls': []}
 
-# already_seen_urls = []
 
+urls_id = {}
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s|%(levelname)s|%(message)s',
                     datefmt="%Y-%m-%d %H:%M:%S",
                     filename="/home/tina/Downloads/logs/web-crawling.log",
                     filemode="a")
+
+
+def get_url_id(url, parent_path):
+    if url in urls_id.keys():
+        return urls_id[url]
+    else:
+        id = str(len(urls_id) + 1)
+        parent_id = parent_path[parent_path.rfind(".")+1:]
+        path = parent_path + '.' + id
+        urls_id[url] = {'id': id, 'parent_id': parent_id, 'path': path}
+        return id, parent_id, path
 
 
 def open_and_read_url(url):
@@ -61,16 +73,16 @@ def look_for_href(start_url, count):
                     href = 'https://' + href[2:]
                 else:
                     href = raw_url + href
-            # if href not in already_seen_urls:
-            #     already_seen_urls.append(href)
-            all_urls.append(href)
+            if href not in crawl_result['already_seen_urls']:
+                crawl_result['already_seen_urls'].append(href)
+                all_urls.append(href)
     if len(all_urls) < count:
         return all_urls
     else:
         return random.choices(all_urls, k=count)
 
 
-def crawl_url(start_url, depth, count, level=0):
+def crawl_url(path, start_url, depth, count, level=0):
     logging.info("Starting to crawl the web, based on give urls")
     depth = int(depth)
     count = int(count)
@@ -78,10 +90,12 @@ def crawl_url(start_url, depth, count, level=0):
         return
     i = 0
     for url in start_url:
-        print(i, '-'*level, url)
-        crawl_result['urls'].append({'index': i, 'level': level, 'url': url})
+        url_id, parent_id, url_path = get_url_id(url, path)
+        print(i, '-'*level, url_id+'=>'+url)
+        crawl_result['urls'].append(
+            {'index': i, 'level': level, 'url_id': url_id, 'parent_id': parent_id, 'url': url, 'url_path': url_path})
         random_urls = look_for_href(url, count)
-        crawl_url(random_urls, depth, count, level+1)
+        crawl_url(url_path, random_urls, depth, count, level+1)
         i += 1
 
 
@@ -90,6 +104,7 @@ def init_crawl():
     crawl_result['urls'] = []
     crawl_result['finished'] = False
     crawl_result['proccess_is_running'] = True
+    crawl_result['already_seen_urls'] = []
 
 
 def mark_crawl_ended():
@@ -111,4 +126,4 @@ if __name__ == "__main__":
 
     # start_url = "https://www.cnn.com/"
 
-    crawl_url([start_url], depth=3, count=5, level=0)
+    crawl_url('ROOT', [start_url], depth=3, count=5, level=0)
