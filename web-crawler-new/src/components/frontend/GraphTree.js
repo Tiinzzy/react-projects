879441bd99f0b54d6a8f5287b806f1b0a251efea
Tina <tinzzy.cracked@yahoo.com>
2023-02-16 08:21:31 -0500

    component to create graph

---
+++ web-crawler-new/src/components/frontend/GraphTree.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Graphviz } from "graphviz-react";
+
+export default class GraphTree extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+}
