e15268f502688b961677ea1a17af862032621149
Tina <tinzzy.cracked@yahoo.com>
2023-02-17 08:42:48 -0500

    fixed issue with displaying urls in the graphiz

--- web-crawler-new/src/components/frontend/GraphTree.js
+++ web-crawler-new/src/components/frontend/GraphTree.js
@@ -19,13 +19,17 @@ export default class GraphTree extends React.Component {
     }
 
     componentDidMount() {
-        let tree = this.state.treeData.map(e => (`"${e.parent_id}"->"${e.url_id}"`));
+        let urlRelations = {}
+        this.state.treeData.map(e => {
+            urlRelations[e.url_id] = e.url;
+            urlRelations["ROOT"] = "ROOT";
+            // urlRelations["ROOT"] = this.state.treeData[0].url;
+
+        });
+        let tree = this.state.treeData.map(e => (`"${urlRelations[e.parent_id]}"->"${urlRelations[e.url_id]}"`));
         let finalizedTree = [...new Set(tree)];
 
-        finalizedTree.forEach(e => console.log(e));
-
         let data = 'digraph G { ' + finalizedTree.join(';') + '}';
-        console.log(data);
         this.setState({ graph: data })
     }
 
