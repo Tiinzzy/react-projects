c43647c75a5eeb13e513f15eaa60d64803ccfb2e
Tina <tinzzy.cracked@yahoo.com>
2023-02-17 17:51:19 -0500

    code cleanup

--- web-crawler-new/src/components/frontend/helper.js
+++ web-crawler-new/src/components/frontend/helper.js
@@ -3,28 +3,6 @@ export const LISTENERS = {
 
 }
 
-const data = {
-    id: 'root',
-    name: 'Parent',
-    children: [
-        {
-            id: '1',
-            name: 'Child - 1',
-        },
-        {
-            id: '3',
-            name: 'Child - 3',
-            children: [
-                {
-                    id: '4',
-                    name: 'Child - 4',
-                },
-            ],
-        },
-    ],
-};
-
-
 function createNode(id, name) {
     return {
         id,
