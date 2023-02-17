d30604aa10e3262ce6cb5dceaaea8e77ed286205
Tina <tinzzy.cracked@yahoo.com>
2023-02-17 17:51:31 -0500

    code cleanup

--- web-crawler-new/src/components/frontend/Tree.js
+++ web-crawler-new/src/components/frontend/Tree.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
@@ -8,27 +8,6 @@ import Box from '@mui/material/Box';
 import BackEndConnection from './BackEndConnection';
 
 const backend = BackEndConnection.INSTANCE();
-
-const data = {
-    id: 'root',
-    name: 'Parent',
-    children: [
-        {
-            id: '1',
-            name: 'Child - 1',
-        },
-        {
-            id: '3',
-            name: 'Child - 3',
-            children: [
-                {
-                    id: '4',
-                    name: 'Child - 4',
-                },
-            ],
-        },
-    ],
-};
 
 export default function Tree(props) {
 
@@ -41,7 +20,7 @@ export default function Tree(props) {
     );
 
     return (
-        <Box style={{ width: window.innerWidth * 0.75, height: window.innerHeight * 0.75}}>
+        <Box style={{ width: window.innerWidth * 0.75, height: window.innerHeight * 0.75 }}>
             <TreeView
                 aria-label="rich object"
                 defaultCollapseIcon={<ExpandMoreIcon />}
