283b007cf786c5c3a4c08b090e16d8204e404998
Tina <tinzzy.cracked@yahoo.com>
2023-02-27 11:07:48 -0500

    sending username and password to backend to login the user and if logged in display data

--- basic-authentication/src/frontend/components/LoginForm.js
+++ basic-authentication/src/frontend/components/LoginForm.js
@@ -5,7 +5,11 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
+import BackEndConnection from './BackEndConnection';
+
 import './style.css'
+
+const backend = BackEndConnection.INSTANCE();
 
 export default class LoginForm extends React.Component {
 
@@ -13,7 +17,8 @@ export default class LoginForm extends React.Component {
         super(props);
         this.state = {
             password: null,
-            username: null
+            username: null,
+            login: false
         }
     }
 
@@ -26,19 +31,25 @@ export default class LoginForm extends React.Component {
     }
 
     loginUser() {
-        console.log('user:', this.state.username, ' password: ', this.state.password)
+        let that = this;
+        backend.login_user(this.state.username, this.state.password, (data) => {
+            if (data.authorized === true) {
+                that.setState({ login: true })
+            }
+        });
     }
 
     render() {
         return (
             <Box className="WholePageBox">
-                <Box className="LoginBox">
-                    <Typography className="LoginHeader" variant="body1">User Login Form</Typography>
-                    <TextField style={{ marginTop: 15 }} label="Username" variant="outlined" onChange={(e) => this.getUsernmae(e)} />
-                    <TextField style={{ marginTop: 20, marginBottom: 30 }} label="Password" variant="outlined" type="password" onChange={(e) => this.getPassword(e)} />
-                    <Button className="LoginBtn" variant="contained" color="primary" onClick={() => this.loginUser()}>Login</Button>
-
-                </Box>
+                {this.state.login === false &&
+                    <Box className="LoginBox">
+                        <Typography className="LoginHeader" variant="body1">User Login Form</Typography>
+                        <TextField style={{ marginTop: 15 }} label="Username" variant="outlined" onChange={(e) => this.getUsernmae(e)} />
+                        <TextField style={{ marginTop: 20, marginBottom: 30 }} label="Password" variant="outlined" type="password" onChange={(e) => this.getPassword(e)} />
+                        <Button className="LoginBtn" variant="contained" color="primary" onClick={() => this.loginUser()}>Login</Button>
+                    </Box>}
+                {this.state.login === true && <Box>this is just a test</Box>}
             </Box>
         );
     }
