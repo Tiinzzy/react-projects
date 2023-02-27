7faa5601c233b05b4102096d7f2252e52c409846
Tina <tinzzy.cracked@yahoo.com>
2023-02-27 16:30:57 -0500

    now everything is based on authentication and pages change if user is logged in or not

--- basic-authentication/src/frontend/App.js
+++ basic-authentication/src/frontend/App.js
@@ -1,11 +1,30 @@
-import LoginForm from "./components/LoginForm";
+import { useState } from "react";
+
+import LoginForm from "./components/login/LoginForm";
+import BackEndConnection from './components/tools/BackEndConnection';
+import UserPage from "./components/home_page/UserPage";
+
+const backend = BackEndConnection.INSTANCE();
+
+function callLoginStatus(setIsLogin, setUser) {
+  backend.checkLoginStatus((result) => {
+    setUser(result.user);
+    setIsLogin(result.authorized);
+  })
+}
+
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState(null);
 
+  callLoginStatus(setIsLogin, setUser);
 
   return (
-
-    <LoginForm />
+    <div>
+      {(isLogin && user !== null) && <UserPage user={user} />}
+      {!isLogin && <LoginForm />}
+    </div>
   );
 }
 
