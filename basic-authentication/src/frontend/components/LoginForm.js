acbd4f7c77b7366a2a62c0c78d9f712ff3ddf332
Tina <tinzzy.cracked@yahoo.com>
2023-02-27 15:19:52 -0500

    if there is no username or password set it will show error and won't allow login

--- basic-authentication/src/frontend/components/LoginForm.js
+++ basic-authentication/src/frontend/components/LoginForm.js
@@ -23,18 +23,20 @@ export default class LoginForm extends React.Component {
             password: null,
             username: null,
             login: false,
-            openDialog: false
+            openDialog: false,
+            userError: false,
+            passError: false
         }
         this.callLoginForm = this.callLoginForm.bind(this);
         shared.callLoginForm = this.callLoginForm;
     }
 
     getUsernmae(e) {
-        this.setState({ username: e.target.value });
+        this.setState({ username: e.target.value, userError: false });
     }
 
     getPassword(e) {
-        this.setState({ password: e.target.value });
+        this.setState({ password: e.target.value, passError: false });
     }
 
     getSecret() {
@@ -45,12 +47,16 @@ export default class LoginForm extends React.Component {
     }
 
     loginUser() {
-        let that = this;
-        backend.login_user(this.state.username, this.state.password, (data) => {
-            if (data.authorized === true) {
-                that.setState({ login: true });
-            }
-        });
+        if (this.state.password === null && this.state.username === null || this.state.password === null || his.state.username === null) {
+            this.setState({ userError: true, passError: true });
+        } else {
+            let that = this;
+            backend.login_user(this.state.username, this.state.password, (data) => {
+                if (data.authorized === true) {
+                    that.setState({ login: true });
+                }
+            });
+        }
     }
 
     closeDialog() {
@@ -59,7 +65,7 @@ export default class LoginForm extends React.Component {
 
     callLoginForm(e) {
         if (e.action === 'user-logged-out') {
-            this.setState({ login: false })
+            this.setState({ login: false, username: null, password: null })
         }
 
     }
@@ -71,8 +77,11 @@ export default class LoginForm extends React.Component {
                     <Box className="WholePageBox">
                         <Box className="LoginBox">
                             <Typography className="LoginHeader" variant="body1">User Login Form</Typography>
-                            <TextField style={{ marginTop: 15 }} label="Username" variant="outlined" onChange={(e) => this.getUsernmae(e)} />
-                            <TextField style={{ marginTop: 20, marginBottom: 30 }} label="Password" variant="outlined" type="password" onChange={(e) => this.getPassword(e)} />
+                            {this.state.userError === false ? <TextField style={{ marginTop: 15 }} label="Username" variant="outlined" onChange={(e) => this.getUsernmae(e)} /> :
+                                <TextField error helperText="Incorrect entry" style={{ marginTop: 15 }} label="Username" variant="outlined" onChange={(e) => this.getUsernmae(e)} />}
+                            {this.state.passError === false ?
+                                <TextField style={{ marginTop: 20, marginBottom: 30 }} label="Password" variant="outlined" type="password" onChange={(e) => this.getPassword(e)} /> :
+                                <TextField error helperText="Incorrect entry" style={{ marginTop: 20, marginBottom: 30 }} label="Password" variant="outlined" type="password" onChange={(e) => this.getPassword(e)} />}
                             <Button className="LoginBtn" variant="contained" color="primary" onClick={() => this.loginUser()}>Login</Button>
 
                             <Button style={{ marginTop: 10 }} className="LoginBtn" variant="contained" onClick={() => this.getSecret()}>SECRET!</Button>
@@ -82,7 +91,6 @@ export default class LoginForm extends React.Component {
                     <Box className="WholePageBox">
                         <UserPage username={this.state.username} />
                     </Box>}
-
                 <Dialog size="md" open={this.state.openDialog} onClose={() => this.closeDialog()}> <DialogPopUp status="logged-out" data={this.state.data} /> </Dialog>
             </>
         );
