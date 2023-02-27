737a965d59655eeae88cd15d38b60253df7b4e83
Tina <tinzzy.cracked@yahoo.com>
2023-02-27 11:07:21 -0500

    created class to connect to backend through axios

---
+++ basic-authentication/src/frontend/components/BackEndConnection.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const md5 = require('md5');
+
+class BackEndConnectionImpl {
+    async login_user(user, password, callback) {
+        return axios.post('/login', { user, password: md5(password) }, {})
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                return false;
+            })
+    }
+}
+
+export default class BackEndConnection {
+    static #object = null;
+
+    static INSTANCE() {
+        if (BackEndConnection.#object === null) {
+            BackEndConnection.#object = new BackEndConnectionImpl();
+        }
+        return BackEndConnection.#object;
+    }
+
+}
