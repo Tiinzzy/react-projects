a736bbbdb84499fea4b92a989463a6ec11111239
Tina <tina.vatanabadi@gmail.com>
2023-02-28 10:57:15 -0500

    getting the username and passwords and confirmation values after entering

--- basic-authentication/src/frontend/components/login/SignUp.js
+++ basic-authentication/src/frontend/components/login/SignUp.js
@@ -16,11 +16,28 @@ export default class SignUp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            password: null,
+            confPass: null,
+            username: null
         }
     }
 
-    createNewUser(){
-        console.log('clicked')
+    getUsername(e) {
+        this.setState({ username: e.target.value });
+    }
+
+    getPassword(e) {
+        this.setState({ password: e.target.value });
+    }
+
+    confirmPassword(e) {
+        this.setState({ confPass: e.target.value });
+    }
+
+    createNewUser() {
+        console.log('username: ', this.state.username);
+        console.log('password: ', this.state.password);
+        console.log('confirm password:', this.state.confPass);
     }
 
     render() {
@@ -29,9 +46,9 @@ export default class SignUp extends React.Component {
                 <Box className="WholePageBox">
                     <Box className="LoginBox">
                         <Typography className="LoginHeader" variant="body1">Creat Account</Typography>
-                        <TextField style={{ marginTop: 15 }} label="Email" variant="outlined" />
-                        <TextField style={{ marginTop: 20, marginBottom: 20 }} label="Password" variant="outlined" />
-                        <TextField style={{ marginBottom: 30 }} label="Confirm Password" variant="outlined" />
+                        <TextField style={{ marginTop: 15 }} label="Username" variant="outlined" onChange={(e) => this.getUsername(e)} />
+                        <TextField style={{ marginTop: 20, marginBottom: 20 }} type="password" label="Password" variant="outlined" onChange={(e) => this.getPassword(e)} />
+                        <TextField style={{ marginBottom: 30 }} type="password" label="Confirm Password" variant="outlined" onChange={(e) => this.confirmPassword(e)} />
 
                         <Button className="LoginBtn" variant="contained" color="primary" onClick={() => this.createNewUser()}>SIGNUP</Button>
 
