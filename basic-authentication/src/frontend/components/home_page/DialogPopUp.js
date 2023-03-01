9d6d92118e1b8d756a66a95bf415d3abccbe186c
Tina <tinzzy.cracked@yahoo.com>
2023-03-01 14:36:42 -0500

    added key down to work submit on enter as well

--- basic-authentication/src/frontend/components/home_page/DialogPopUp.js
+++ basic-authentication/src/frontend/components/home_page/DialogPopUp.js
@@ -24,21 +24,27 @@ export default class DialogPopUp extends React.Component {
             currPass: null,
             newPass: null,
             confPass: null,
-            wrongCUrrPass: false,
-            changeType: false
+            wrongCurrPass: false,
+            changeType: false,
+            error: false
         }
     }
 
     getCurrentPassword(e) {
-        this.setState({ currPass: e.target.value });
+        this.setState({ currPass: e.target.value, error: false });
     }
 
     getNewPassword(e) {
-        this.setState({ newPass: e.target.value });
+        this.setState({ newPass: e.target.value, error: false });
     }
 
     getPasswordConfirm(e) {
-        this.setState({ confPass: e.target.value });
+        this.setState({ confPass: e.target.value, error: false });
+        let key = e.code || "";
+        let isEnter = key.toLowerCase().indexOf('enter') >= 0;
+        if (isEnter) {
+            this.submitPassChanges();
+        }
     }
 
     cancelChangePass() {
@@ -50,13 +56,14 @@ export default class DialogPopUp extends React.Component {
             backend.change_password(this.state.user, this.state.currPass, this.state.confPass, (data) => {
                 let that = this;
                 if (data.change !== null) {
-                    this.state.closeDialog();
+                    let action = 'password-changed-sucessfully'
+                    this.state.closeDialog(action);
                 } else {
-                    that.setState({ wrongCUrrPass: true })
+                    that.setState({ wrongCurrPass: true })
                 }
             })
         } else (
-            console.log('there is something wrong')
+            this.setState({ error: true })
         )
     }
 
@@ -73,22 +80,23 @@ export default class DialogPopUp extends React.Component {
                 {this.state.changePassword === true &&
                     <Box style={{ width: 600 }}>
                         <Box style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', flexDirection: 'column', marginLeft: 20, marginRight: 20, marginTop: 20, marginBottom: 20 }}>
-                            <Typography fontWeight="bold" variant="h5" mt={2}>Change Password</Typography>
-                            {this.state.wrongCUrrPass === false ?
-                                <TextField style={{ marginTop: 15, marginBottom: 10 }} label="Current Password" variant="outlined" type={this.state.changeType === false ? "password" : "text"} onChange={(e) => this.getCurrentPassword(e)} /> :
-                                <TextField error helperText="Wrong Password" style={{ marginTop: 15, marginBottom: 10 }} label="Current Password" variant="outlined" type={this.state.changeType === false ? "password" : "text"} onChange={(e) => this.getCurrentPassword(e)} />}
+                            <Typography fontWeight="bold" variant="h5" mt={2} mb={2}>Change Password</Typography>
 
-                            <TextField style={{ marginTop: 10, marginBottom: 10 }} label="New Password" variant="outlined" type={this.state.changeType === false ? "password" : "text"} onChange={(e) => this.getNewPassword(e)} />
+                            <TextField error={this.state.wrongCurrPass === true || this.state.error === true} helperText={this.state.wrongCurrPass === true && "Wrong Password"} style={{ marginTop: 15, marginBottom: 10 }}
+                                label="Current Password" variant="outlined" type={this.state.changeType === false ? "password" : "text"} onChange={(e) => this.getCurrentPassword(e)} />
 
-                            <TextField style={{ marginTop: 10, marginBottom: 10 }} label="Confirm New Password" variant="outlined" type={this.state.changeType === false ? "password" : "text"} onChange={(e) => this.getPasswordConfirm(e)} />
+                            <TextField error={this.state.error === true} style={{ marginTop: 10, marginBottom: 10 }} label="New Password" variant="outlined" type={this.state.changeType === false ? "password" : "text"} onChange={(e) => this.getNewPassword(e)} />
+
+                            <TextField error={this.state.error === true} style={{ marginTop: 10, marginBottom: 10 }} label="Confirm New Password" variant="outlined"
+                            onKeyDown={(e) => this.getPasswordConfirm(e)} type={this.state.changeType === false ? "password" : "text"} onChange={(e) => this.getPasswordConfirm(e)} />
 
                             <FormControlLabel control={<Checkbox onChange={() => this.checkBoxClicked()} />} label="Show Password" />
 
                         </Box>
                         <Box style={{ display: 'flex', justifyContent: 'right', alignItems: 'right', marginRight: 12 }}>
                             <DialogActions>
-                                <Button className="LoginBtn" variant="contained" onClick={() => this.cancelChangePass()}>Cancel</Button>
-                                <Button className="LoginBtn" variant="contained" onClick={() => this.submitPassChanges()}>Submit </Button>
+                                <Button className="LoginBtn" variant="contained" onClick={() => this.cancelChangePass()} style={{ marginBottom: 10 }}>Cancel</Button>
+                                <Button className="LoginBtn" variant="contained" onClick={() => this.submitPassChanges()} style={{ marginBottom: 10 }}>Submit </Button>
                             </DialogActions>
                         </Box>
 
