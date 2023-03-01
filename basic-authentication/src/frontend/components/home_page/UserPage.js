b485f9624733ab96a4fc5bb2fa91c221f0b9096c
Tina <tinzzy.cracked@yahoo.com>
2023-03-01 14:36:02 -0500

    added snackbar and alert for when password is changed

--- basic-authentication/src/frontend/components/home_page/UserPage.js
+++ basic-authentication/src/frontend/components/home_page/UserPage.js
@@ -4,6 +4,8 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Dialog from '@mui/material/Dialog';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 
 import BackEndConnection from '../tools/BackEndConnection';
 import DialogPopUp from "./DialogPopUp";
@@ -19,7 +21,9 @@ export default class UserPage extends React.Component {
         this.state = {
             user: props.user,
             openDialog: false,
-            changePassword: false
+            changePassword: false,
+            displaySuccess: false,
+            openSnack: false
         }
         this.closeDialog = this.closeDialog.bind(this);
     }
@@ -39,7 +43,10 @@ export default class UserPage extends React.Component {
         })
     }
 
-    closeDialog() {
+    closeDialog(e) {
+        if (e === 'password-changed-sucessfully') {
+            this.setState({ displaySuccess: true, openSnack: true });
+        }
         this.setState({ openDialog: false });
     }
 
@@ -47,21 +54,32 @@ export default class UserPage extends React.Component {
         this.setState({ openDialog: true, changePassword: true });
     }
 
+    closeAlert() {
+        this.setState({ openSnack: false });
+    }
+
     render() {
         return (
-            <Box className="WholePageBox">
-                <Box style={{ display: 'flex', flexDirection: 'column' }}>
-                    <Typography mb={2} className="LoginHeader" variant="body1">
-                        Hi <span style={{ fontWeight: 'bold' }}>{this.state.user}</span>, welcome back!
-                    </Typography>
-                    <Button style={{ marginBottom: 15 }} className="LoginBtn" variant="contained" onClick={() => this.getSecret()}>SECRET!</Button>
-                    <Button style={{ marginBottom: 15 }} className="LoginBtn" variant="contained" onClick={() => this.changeUserPassword()}>Change Password</Button>
-                    <Button className="LoginBtn" variant="contained" onClick={() => this.logOutUser()}>Logout</Button>
+            <>
+                {this.state.displaySuccess === true &&
+                    <Snackbar open={this.state.openSnack} onClose={() => this.closeAlert()} autoHideDuration={5000} anchorOrigin={{ vertical: "top", horizontal: "center" }}>
+                        <Alert severity="success">Password changed sucessfully</Alert>
+                    </Snackbar>}
+
+                <Box className="WholePageBox">
+                    <Box style={{ display: 'flex', flexDirection: 'column', }}>
+                        <Typography mb={3} className="LoginHeader" variant="body1">
+                            Hi <span style={{ fontWeight: 'bold' }}>{this.state.user}</span>, welcome back!
+                        </Typography>
+                        <Button style={{ marginBottom: 15 }} className="LoginBtn" variant="contained" onClick={() => this.getSecret()}>SECRET!</Button>
+                        <Button style={{ marginBottom: 15 }} className="LoginBtn" variant="contained" onClick={() => this.changeUserPassword()}>Change Password</Button>
+                        <Button className="LoginBtn" variant="contained" onClick={() => this.logOutUser()}>Logout</Button>
+                    </Box>
+                    <Dialog size="md" open={this.state.openDialog} onClose={() => this.closeDialog()}>
+                        <DialogPopUp secret={this.state.secret} changePassword={this.state.changePassword} closeDialog={this.closeDialog} user={this.state.user} />
+                    </Dialog>
                 </Box>
-                <Dialog size="md" open={this.state.openDialog} onClose={() => this.closeDialog()}>
-                    <DialogPopUp secret={this.state.secret} changePassword={this.state.changePassword} closeDialog={this.closeDialog} user={this.state.user} />
-                </Dialog>
-            </Box>
+            </>
         );
     }
 };
