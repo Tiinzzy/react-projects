d2016472360963cad25870e71141c7897a84635c
Tina <tinzzy.cracked@yahoo.com>
2023-03-05 10:55:04 -0500

    component for resetting password if forgotten

---
+++ basic-authentication/src/frontend/components/login/ForgotPassword.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+export default class ForgotPassword extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+
+    render() {
+        return (
+            <>
+                HI THERE IS FORGOT PASSOWRD
+            </>
+            );
+    }
+};
+
+
+/*
+
+USERS_TABLE
+--------------------
+user_name,
+email,
+passowrd_md5,
+reset_password_id,
+asked_for_reset_date
+
+*/
