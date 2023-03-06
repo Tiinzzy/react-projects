1b2aa574e613b241a1b9bdb1fef765293ab97146
Tina <tinzzy.cracked@yahoo.com>
2023-03-06 15:17:08 -0500

    if url is given that is available in database it will be redirected to reset password

--- basic-authentication/src/index.js
+++ basic-authentication/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './frontend/App';
 
+import BackEndConnection from './frontend/components/tools/BackEndConnection';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css';
+
+const backend = BackEndConnection.INSTANCE();
+
+let pathKey = window.location.pathname.replaceAll("/", "");
+
+backend.redirect_if_needed(pathKey, (data) => {
+  console.log(data.result)
+  if (data.msg === 'correct email') {
+    window.location = '/reset-password';
+  } else {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+});
