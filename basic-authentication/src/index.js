08a72ee851858bd3277c7137202f460bafc014b1
Tina <tinzzy.cracked@yahoo.com>
2023-03-06 17:42:16 -0500

    removed redirection to another component

--- basic-authentication/src/index.js
+++ basic-authentication/src/index.js
@@ -2,24 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './frontend/App';
 
-import BackEndConnection from './frontend/components/tools/BackEndConnection';
-
 import './index.css';
 
-const backend = BackEndConnection.INSTANCE();
-
-let pathKey = window.location.pathname.replaceAll("/", "");
-
-backend.redirect_if_needed(pathKey, (data) => {
-  console.log(data.result)
-  if (data.msg === 'correct email') {
-    window.location = '/reset-password';
-  } else {
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
-  }
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
