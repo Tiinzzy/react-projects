0d6baa6915c3b5624336d4320bdf694d6b2d8bd7
Tina <tinzzy.cracked@yahoo.com>
2023-04-17 10:41:48 -0400

    also connects with passing the parameters

--- mongodb-browser/src/back-end/test_mongodb.py
+++ mongodb-browser/src/back-end/test_mongodb.py
@@ -5,7 +5,7 @@ from mongodb import MongoDB
 class TestMongoDB(unittest.TestCase):
     def test_connection(self):
         new_connection = MongoDB()
-        connection = new_connection.connect()
+        connection = new_connection.connect('localhost', 27017)
         new_connection.disconnect(connection)
         result = "MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True)"
         self.assertEqual(str(connection), result)
