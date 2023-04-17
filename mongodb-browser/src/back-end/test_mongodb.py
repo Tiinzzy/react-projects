4f7bfbe69489f80e16603cc0bcd3dfebf44d0419
Tina <tinzzy.cracked@yahoo.com>
2023-04-17 10:32:34 -0400

    passed the test for getting the list of databases

--- mongodb-browser/src/back-end/test_mongodb.py
+++ mongodb-browser/src/back-end/test_mongodb.py
@@ -9,3 +9,11 @@ class TestMongoDB(unittest.TestCase):
         new_connection.disconnect(connection)
         result = "MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True)"
         self.assertEqual(str(connection), result)
+
+    def test_getting_databases(self):
+        new_connection = MongoDB()
+        connection = new_connection.connect()
+        available_databases = new_connection.databases(connection)
+        new_connection.disconnect(connection)
+        self.assertTrue(len(available_databases) >= 3)
+
