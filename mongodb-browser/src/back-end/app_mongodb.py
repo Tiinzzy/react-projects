ca013cf544620cbfbd0a2e52b2db112cf2d72c15
Tina <tinzzy.cracked@yahoo.com>
2023-04-17 13:49:53 -0400

    getting parameters and making a connection and sending all available databases to the front-end

--- mongodb-browser/src/back-end/app_mongodb.py
+++ mongodb-browser/src/back-end/app_mongodb.py
@@ -7,4 +7,16 @@ def connect(parameters):
 
     new_connection = MongoDB()
     connection = new_connection.connect(host_name, port_name)
+    new_connection.disconnect(connection)
     return {'result': str(connection)}
+
+
+def get_databases(parameters):
+    host_name = parameters.get('host_name')
+    port_name = parameters.get('port_name')
+
+    new_connection = MongoDB()
+    connection = new_connection.connect(host_name, port_name)
+    available_databases = new_connection.databases(connection)
+    new_connection.disconnect(connection)
+    return {'databases': available_databases}
