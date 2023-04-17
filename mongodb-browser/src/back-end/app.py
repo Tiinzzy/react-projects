3550ddb08b13f9738004aab98be5b1c97b214184
Tina <tinzzy.cracked@yahoo.com>
2023-04-17 13:43:27 -0400

    api to connect to MongoDB

--- mongodb-browser/src/back-end/app.py
+++ mongodb-browser/src/back-end/app.py
@@ -1,8 +1,17 @@
 from flask import Flask, request, jsonify
 import json
 
+import app_mongodb
+
 app = Flask(__name__)
 
 
 def get_parameters(req):
     return json.loads(req.data.decode('utf8').replace("'", '"'))
+
+
+@app.route('/mongodb/connect', methods=["POST"])
+def connect_mongo_db():
+    parameters = get_parameters(request)
+    result = app_mongodb.connect(parameters)
+    return jsonify(result)
