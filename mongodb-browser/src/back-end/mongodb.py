44e9af3c289ace2a0b00040ef7c645478a641de9
Tina <tinzzy.cracked@yahoo.com>
2023-04-17 09:45:05 -0400

    created a static function to get all the documents in one collection, based on their conditions or none at all

--- mongodb-browser/src/back-end/mongodb.py
+++ mongodb-browser/src/back-end/mongodb.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 
 class MongoDB:
@@ -27,14 +28,32 @@ class MongoDB:
         collections = my_database.list_collection_names()
         return collections
 
+    @staticmethod
+    def search_all_documents(connect, database_name, collection_name, condition=None, return_fields=None):
+        my_database = connect[database_name]
+        collection = my_database.get_collection(collection_name)
+        cursor = collection.find(condition, return_fields)
+        result = dict()
+        list_curser = list(cursor)
+        json_data = dumps(list_curser)
+        result['documents'] = json_data
+        result['length'] = len(list(cursor.clone()))
+        return result
+
 
 if __name__ == '__main__':
-    new_connection = MongoDB()
-    connection = new_connection.connect()
-    print(new_connection.databases(connection))
-    new_connection.disconnect(connection)
+    # new_connection = MongoDB()
+    # connection = new_connection.connect()
+    # print(new_connection.databases(connection))
+    # new_connection.disconnect(connection)
+
+    # new_connection = MongoDB()
+    # connection = new_connection.connect()
+    # print(new_connection.collections(connection, 'tina_db'))
+    # new_connection.disconnect(connection)
 
     new_connection = MongoDB()
     connection = new_connection.connect()
-    print(new_connection.collections(connection, 'tina_db'))
+    documents = new_connection.search_all_documents(connection, 'tina_db', 'movies')
+    print(documents)
     new_connection.disconnect(connection)
