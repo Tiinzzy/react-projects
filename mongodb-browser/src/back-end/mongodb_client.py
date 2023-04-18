7ae6356af483f9f67d31ecc156e5feb0315fe855
Tina <tinzzy.cracked@yahoo.com>
2023-04-18 09:55:51 -0400

    fixed the class and added more methods for various queries within MongoDB

--- mongodb-browser/src/back-end/mongodb_client.py
+++ mongodb-browser/src/back-end/mongodb_client.py
@@ -18,8 +18,39 @@ class MongoDBClient:
             print(e)
         return server_info is not None
 
-    def close(self):
+    def disconnect(self):
         self.mongo_client.close()
 
     def list_databases(self):
-        return self.mongo_client.list_databases()
+        return self.mongo_client.list_database_names()
+
+    def list_collections(self, database_name):
+        my_database = self.mongo_client[database_name]
+        return my_database.list_collection_names()
+
+    def search_all_documents(self, database_name, collection_name, condition=None, return_fields=None):
+        my_database = self.mongo_client[database_name]
+        collection = my_database.get_collection(collection_name)
+        cursor = collection.find(condition, return_fields)
+        result = dict()
+        list_curser = list(cursor)
+        result['documents'] = list_curser
+        result['length'] = len(list(cursor.clone()))
+        return result
+
+    def insert_documents(self, database_name, collection_name, data):
+        my_database = self.mongo_client[database_name]
+        collection = my_database.get_collection(collection_name)
+        cursor = collection.find()
+        result = dict()
+        result['old_length'] = len(list(cursor.clone()))
+        for d in data:
+            collection.insert_one(d)
+        result['current_length'] = len(list(cursor.clone()))
+        return result
+
+    def drop_collection(self, database_name, collection_name):
+        my_database = self.mongo_client[database_name]
+        collection = my_database.get_collection(collection_name)
+        collection.drop()
+        return my_database.list_collection_names()
