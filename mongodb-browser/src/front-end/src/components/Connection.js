f5ef9249c456faf018da76ab0b83119b2715987f
Tina <tinzzy.cracked@yahoo.com>
2023-04-19 08:45:16 -0400

    creating the content of dialog for connecting to the database and also on click of connect button send a callback to close

--- mongodb-browser/src/front-end/src/components/Connection.js
+++ mongodb-browser/src/front-end/src/components/Connection.js
@@ -1,21 +1,39 @@
 import React from "react";
 
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 export default class Connection extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-
+            handleCLoseDialog: props.handleCLoseDialog
         }
+    }
+
+    connectAndClose() {
+        this.state.handleCLoseDialog({ action: 'connect-and-close' });
     }
 
     render() {
         return (
-            <Box>
-                this will be the dialog
-            </Box>
+            <>
+                <DialogTitle>
+                    {"Connect to MongoDB"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => this.connectAndClose()}>Connect</Button>
+                </DialogActions>
+            </>
         );
     }
 }
