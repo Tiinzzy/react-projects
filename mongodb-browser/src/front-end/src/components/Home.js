da59c6da328b2fa122455b578a300388995e60c9
Tina <tinzzy.cracked@yahoo.com>
2023-04-19 08:17:39 -0400

    component as home page to display everything in it

---
+++ mongodb-browser/src/front-end/src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+export default class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <Box>
+                this will be home page
+            </Box>
+        );
+    }
+}
