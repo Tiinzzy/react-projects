39ce086a1626a8f4cff2c06281487b0ff9f22f5d
Tina <tinzzy.cracked@yahoo.com>
2023-04-19 16:47:28 -0400

    fixed menu list indention on mapping data

--- mongodb-browser/src/front-end/src/components/SideBar.js
+++ mongodb-browser/src/front-end/src/components/SideBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import List from '@mui/material/List';
+import Box from '@mui/material/Box';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
@@ -18,7 +19,8 @@ export default class SideBar extends React.Component {
         super(props);
         this.state = {
             connectionInfo: props.connectionInfo,
-            openList: false
+            openList: false,
+            openCollections: ''
         }
     }
 
@@ -34,16 +36,21 @@ export default class SideBar extends React.Component {
     }
 
     getCollections(e) {
-        let query = { 'host_name': this.state.connectionInfo.host, 'port_name': this.state.connectionInfo.port, 'database_name': e };
-        backend.get_collections_mongo_db(query, (data) => {
-            console.log(data.collections);
-        })
+        if (this.state.openCollections === '') {
+            let query = { 'host_name': this.state.connectionInfo.host, 'port_name': this.state.connectionInfo.port, 'database_name': e };
+            backend.get_collections_mongo_db(query, (data) => {
+                let that = this;
+                that.setState({ openCollections: e, collections: data.collections })
+            });
+        } else {
+            this.setState({ openCollections: '' });
+        }
     }
 
     render() {
         return (
             <>
-                <List sx={{ width: '100%', maxWidth: 350, bgcolor: 'background.paper' }}
+                <List sx={{ width: '100%', bgcolor: 'background.paper' }}
                     component="nav">
                     <ListItemButton onClick={() => this.handleOPenList()}>
                         <ListItemText primary="Databases" />
@@ -52,14 +59,25 @@ export default class SideBar extends React.Component {
                     <Collapse in={this.state.openList} timeout="auto" unmountOnExit>
                         <List component="div" disablePadding >
                             {this.state.databases && this.state.databases.map((e, i) => (
-                                <ListItemButton sx={{ pl: 4 }} key={i} onClick={() => this.getCollections(e)}>
-                                    <ListItemText primary={e} />
-                                </ListItemButton>
+                                <Box key={i} >
+                                    <ListItemButton sx={{ pl: 4 }} onClick={() => this.getCollections(e)}>
+                                        <ListItemText primary={e} />
+                                        {this.state.openCollections === e ? <ExpandLess /> : <ExpandMore />}
+                                    </ListItemButton>
+                                    <Collapse in={this.state.openCollections === e} timeout="auto" unmountOnExit>
+                                        <List component="div" disablePadding>
+                                                {this.state.collections && this.state.collections.map((ee, i) => (
+                                                    <ListItemButton key={i} sx={{ pl: 8 }} onClick={() => console.log(e, ee)}>
+                                                        <ListItemText  primary={ee} />
+                                                    </ListItemButton>
+                                                ))}
+                                        </List>
+                                    </Collapse>
+                                </Box>
                             ))}
                         </List>
                     </Collapse>
                 </List>
-
             </>
         );
     }
