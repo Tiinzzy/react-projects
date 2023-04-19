6041036fe8bbafecd54d0dd7ea1823fc8a817863
Tina <tinzzy.cracked@yahoo.com>
2023-04-19 08:16:04 -0400

    component for dialog to open up for connecting to database

---
+++ mongodb-browser/src/front-end/src/components/Connection.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+export default class Connection extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <Box>
+                this will be the dialog
+            </Box>
+        );
+    }
+}
