f0152d9dec23caaaa66e1041858347c5ef893460
Tina <tinzzy.cracked@yahoo.com>
2023-04-19 09:20:00 -0400

    created a class and functional api to send http requests to backend to connect to the database

---
+++ mongodb-browser/src/front-end/src/components/BackEndConnection.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+class BackEndConnectionImpl {
+    async connect_mongo_db(callback) {
+        return axios.post('/mongodb/connect', {}, {})
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                return false;
+            })
+    }
+}
+
+export default class BackEndConnection {
+    static #object = null;
+
+    static INSTANCE() {
+        if (BackEndConnection.#object === null) {
+            BackEndConnection.#object = new BackEndConnectionImpl();
+        }
+        return BackEndConnection.#object;
+    }
+
+}
