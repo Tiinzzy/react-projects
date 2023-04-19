0eafd0e013024129d83cde598d8beb0881ed54c8
Tina <tinzzy.cracked@yahoo.com>
2023-04-19 09:14:31 -0400

    added input fields in dialog to get the host name and port name for connecting to database

--- mongodb-browser/src/front-end/src/components/Connection.js
+++ mongodb-browser/src/front-end/src/components/Connection.js
@@ -5,14 +5,28 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import Input from '@mui/material/Input';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+
 
 export default class Connection extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            handleCLoseDialog: props.handleCLoseDialog
+            handleCLoseDialog: props.handleCLoseDialog,
+            host_name: 'localhost',
+            port_name: 27017
         }
+    }
+
+    getHostName(e) {
+        this.setState({ host_name: e.target.value });
+    }
+
+    getPortName(e) {
+        this.setState({ port_name: e.target.value });
     }
 
     connectAndClose() {
@@ -27,11 +41,20 @@ export default class Connection extends React.Component {
                 </DialogTitle>
                 <DialogContent>
                     <DialogContentText>
-
+                        Connect to default Host and Port or eneter your own values.
                     </DialogContentText>
+                    <FormControl variant="standard" style={{ marginTop: 20 }}>
+                        <InputLabel htmlFor="component-simple">Host</InputLabel>
+                        <Input id="component-simple" defaultValue={this.state.host_name} style={{ width: 400 }} onChange={(e) => this.getHostName(e)} />
+                    </FormControl>
+                    <br />
+                    <FormControl variant="standard" style={{ marginTop: 40, marginBottom: 20 }}>
+                        <InputLabel htmlFor="component-simple">Port</InputLabel>
+                        <Input id="component-simple" defaultValue={this.state.port_name} style={{ width: 400 }} onChange={(e) => this.getPortName(e)} />
+                    </FormControl>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={() => this.connectAndClose()}>Connect</Button>
+                    <Button onClick={() => this.connectAndClose()} variant="contained">Connect</Button>
                 </DialogActions>
             </>
         );
