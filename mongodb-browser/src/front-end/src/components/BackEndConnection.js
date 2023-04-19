f189eeddcb64b0d53b8d5113a3191c02c60e9a79
Tina <tinzzy.cracked@yahoo.com>
2023-04-19 14:12:55 -0400

    function to get databases from backend

--- mongodb-browser/src/front-end/src/components/BackEndConnection.js
+++ mongodb-browser/src/front-end/src/components/BackEndConnection.js
@@ -3,7 +3,22 @@ import axios from 'axios';
 class BackEndConnectionImpl {
     async connect_mongo_db(host_name, port_name, callback) {
         let query = { 'host_name': host_name, 'port_name': port_name };
+
         return axios.post('/mongodb/connect', query, {})
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                return false;
+            })
+    }
+
+    async get_databases_mongo_db(callback) {
+        return axios.post('/mongodb/database', {}, {})
             .then(function (response) {
                 if (callback) {
                     callback(response.data);
