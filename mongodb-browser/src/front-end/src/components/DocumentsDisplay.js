e6f47c5c7cb23081704ea42979bc576a84f6de57
Tina <tinzzy.cracked@yahoo.com>
2023-04-20 09:26:37 -0400

    getting documents on component did mount and component did update if the user clicks on another collection to view the data

--- mongodb-browser/src/front-end/src/components/DocumentsDisplay.js
+++ mongodb-browser/src/front-end/src/components/DocumentsDisplay.js
@@ -2,7 +2,12 @@ import React from "react";
 
 import Box from '@mui/material/Box';
 
+import BackEndConnection from './BackEndConnection';
+
 import './style.css';
+
+const backend = BackEndConnection.INSTANCE();
+
 
 export default class DocumentsDisplay extends React.Component {
 
@@ -10,11 +15,28 @@ export default class DocumentsDisplay extends React.Component {
         super(props);
         this.state = {
             database: props.database,
-            collection: props.collection
+            collection: props.collection,
+            connectionInfo: props.connectionInfo
         }
-        console.log(props.database, props.collection)
     }
 
+    componentDidMount() {
+        let query = {
+            'host_name': this.state.connectionInfo.host, 'port_name': this.state.connectionInfo.port, 'database_name': this.state.database, 'collection_name': this.state.collection
+        };
+        backend.get_documents_mongo_db(query, (data) => {
+            console.log(data);
+        })
+    }
+
+    componentDidUpdate() {
+        let query = {
+            'host_name': this.state.connectionInfo.host, 'port_name': this.state.connectionInfo.port, 'database_name': this.props.database, 'collection_name': this.props.collection
+        };
+        backend.get_documents_mongo_db(query, (data) => {
+            console.log(data);
+        })
+    }
 
     render() {
         return (
