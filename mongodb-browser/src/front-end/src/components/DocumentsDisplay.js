208370308bd8f6b70dac75093cc95965a1fcdb0c
Tina <tinzzy.cracked@yahoo.com>
2023-04-20 14:29:50 -0400

    setting query as global value to set values on component mount and component update

--- mongodb-browser/src/front-end/src/components/DocumentsDisplay.js
+++ mongodb-browser/src/front-end/src/components/DocumentsDisplay.js
@@ -9,7 +9,6 @@ import './style.css';
 
 const backend = BackEndConnection.INSTANCE();
 
-
 export default class DocumentsDisplay extends React.Component {
 
     constructor(props) {
@@ -17,62 +16,63 @@ export default class DocumentsDisplay extends React.Component {
         this.state = {
             database: props.database,
             collection: props.collection,
-            connectionInfo: props.connectionInfo
+            connectionInfo: props.connectionInfo,
+            query: {}
         }
     }
 
     componentDidMount() {
-        let query = {
-            'host_name': this.state.connectionInfo.host, 'port_name': this.state.connectionInfo.port, 'database_name': this.state.database, 'collection_name': this.state.collection
-        };
-        backend.get_documents_mongo_db(query, (data) => {
+        this.state.query['host_name'] = this.state.connectionInfo.host;
+        this.state.query['port_name'] = this.state.connectionInfo.port;
+        this.state.query['database_name'] = this.state.database;
+        this.state.query['collection_name'] = this.state.collection;
+
+        backend.get_documents_mongo_db(this.state.query, (data) => {
             let that = this;
-            let objectLength = [];
-            for (let i in data.documents) {
-                let len = Object.keys(data.documents[i]).length;
-                objectLength.push(len);
-            }
-            data.documents['object_length'] = objectLength;
-            that.setState({ documents: data, length: data.length });
+            that.setState({ documents: data.documents, length: data.length });
         })
     }
 
     componentDidUpdate() {
-        let query = {
-            'host_name': this.state.connectionInfo.host, 'port_name': this.state.connectionInfo.port, 'database_name': this.props.database, 'collection_name': this.props.collection
-        };
-        backend.get_documents_mongo_db(query, (data) => {
-            // let that = this;
-            // Object.keys(data.result).forEach(k => data[k] = data[k] || '');
-            // that.setState({ documents: data.result.documents });
+        this.state.query['database_name'] = this.props.database;
+        this.state.query['collection_name'] = this.props.collection;
+
+        backend.get_documents_mongo_db(this.state.query, (data) => {
+            let that = this;
+            that.setState({ documents: data.documents, length: data.length });
         })
+    }
+
+    displayData(e) {
+        console.log(e)
     }
 
     render() {
         return (
             <>
                 <Box className="display-documents-box-1">
-                    <table width="100%" style={{ fontSize: '80%' }} cellPadding={0} cellSpacing={0}>
-                        <tbody>
-                            <tr>
-                                <th>ObjectId</th>
-                                <th>Values</th>
-                            </tr>
-                            {this.state.documents && this.state.documents.documents.map((e, i) => (
-                                <tr key={i}>
-                                    <td>
-                                        {e._id}
-                                    </td>
-                                    <td>
-                                        {e.object_length}
-                                    </td>
-                                </tr>))}
-                        </tbody>
-                    </table>
+                    <Box className="display-documents-left-box">
+                        <table width="100%" style={{ fontSize: '80%' }} cellPadding={0} cellSpacing={0}>
+                            <tbody>
+                                <tr>
+                                    <th>ObjectId</th>
+                                </tr>
+                                {this.state.documents && this.state.documents.map((e, i) => (
+                                    <tr key={i} onClick={() => this.displayData(e._id)}>
+                                        <td>
+                                            {e._id}
+                                        </td>
+                                    </tr>))}
+                            </tbody>
+                        </table>
+                    </Box>
+                    <Box className="display-documents-right-box">
+                        right box
+                    </Box>
                 </Box>
                 <Box className="display-documents-box-2">
                     <TextField fullWidth id="fullWidth" multiline
-                        rows={6} placeholder="Enter a query"/>
+                        rows={6} placeholder="Enter a query" />
                 </Box >
             </>
         );
