2c3e371e7f4a4d22ed8afd40bb67b1d75ce16002
Tina <tinzzy.cracked@yahoo.com>
2023-04-21 14:13:34 -0400

    dropping collection works properly

--- mongodb-browser/src/front-end/src/components/DocumentsDisplay.js
+++ mongodb-browser/src/front-end/src/components/DocumentsDisplay.js
@@ -110,10 +110,10 @@ export default class DocumentsDisplay extends React.Component {
         } else if (this.state.selected === 2) {
             let info = this.state.command.substring(this.state.command.indexOf("y(") + 1);
             info = info.replace("(", "").replace(")", "");
-            const myObj = JSON.parse('"' + info + '"');
+            // const myObj = JSON.parse('"' + info + '"');
 
-            this.state.query['documents'] = myObj;
-            console.log(typeof myObj)
+            // this.state.query['documents'] = [{ "type": "fishy-fishy-3" }];
+            // console.log(this.state.query)
             // backend.insert_documents_mongo_db((data) => {
             //     console.log(data);
             //     // if (data.length > 0) {
@@ -133,7 +133,11 @@ export default class DocumentsDisplay extends React.Component {
                 };
             })
         } else if (this.state.selected === 4) {
-            console.log('drop')
+            backend.drop_collection_mongo_db(this.state.query, (data) => {
+                if (data.result) {
+                    console.log('collection dropped')
+                };
+            })
         }
     }
 
