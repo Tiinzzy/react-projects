c896e27951d4a3a787aaeb1ef198166c24656579
Tina <tinzzy.cracked@yahoo.com>
2023-04-24 08:50:04 -0400

    new component to handle inserting documents in a new collection

---
+++ mongodb-browser/src/front-end/src/components/InsertDocumentNewCollection.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+import './style.css';
+
+export default class InsertDocumentNewCollection extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+
+    render() {
+        return (
+            <Box>
+                dialog content
+            </Box>
+        );
+    }
+}
