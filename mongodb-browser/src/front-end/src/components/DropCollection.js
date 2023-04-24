748566227cdb16d1db422fc7f1a5fda803b32086
Tina <tinzzy.cracked@yahoo.com>
2023-04-24 10:54:56 -0400

    component to display content of drop collection dialog

---
+++ mongodb-browser/src/front-end/src/components/DropCollection.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+import './style.css';
+
+export default class DropCollection extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+
+    render() {
+        return (
+            <Box>
+                drop collection dialog content
+            </Box>
+        );
+    }
+}
