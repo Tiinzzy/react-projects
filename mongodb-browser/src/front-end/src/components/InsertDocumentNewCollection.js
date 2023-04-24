04f94c962068c2ec29b409998f77c2f883479844
Tina <tinzzy.cracked@yahoo.com>
2023-04-24 09:11:24 -0400

    fixed content inside the dialog for getting the correct data

--- mongodb-browser/src/front-end/src/components/InsertDocumentNewCollection.js
+++ mongodb-browser/src/front-end/src/components/InsertDocumentNewCollection.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
 
 import './style.css';
 
@@ -9,15 +14,45 @@ export default class InsertDocumentNewCollection extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            newData: '',
+            collectionName: ''
         }
     }
 
+    getDocumentData(e) {
+        this.setState({ newData: e.target.value });
+    }
+
+    getNewCollectionName(e) {
+        this.setState({ collectionName: e.target.value });
+    }
 
     render() {
         return (
-            <Box>
-                dialog content
-            </Box>
+            <>
+                <DialogTitle>
+                    {"Insert Document in New Collection"}
+                </DialogTitle>
+                <DialogContent>
+                    <TextField fullWidth label="Collection Name" variant="outlined" sx={{ marginTop: 2, marginBottom: 3 }}
+                        value={this.state.collectionName}
+                        onChange={(e) => this.getNewCollectionName(e)} />
+                    <TextField
+                        fullWidth multiline
+                        rows={20}
+                        label="Data"
+                        variant="outlined"
+                        value={this.state.newData}
+                        onChange={(e) => this.getDocumentData(e)}
+                    />
+                    <Box style={{ width: 1000 }}>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="outlined">Cancel</Button>
+                    <Button variant="outlined">Submit</Button>
+                </DialogActions>
+            </>
         );
     }
 }
