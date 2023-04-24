abc3a1b67ca5dfcb77fb709c7c76d1f827a95f75
Tina <tinzzy.cracked@yahoo.com>
2023-04-24 14:00:54 -0400

    added handle resize for adjusting page and footer

--- mongodb-browser/src/front-end/src/App.js
+++ mongodb-browser/src/front-end/src/App.js
@@ -1,14 +1,35 @@
+import React, { useState, useEffect } from "react";
+
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+const FOOTER_OFFSET = 185;
+
+function getWindowSize() {
+  return { h: window.innerHeight, w: window.innerWidth }
+}
+
 export default function App() {
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    function resizeHandler() {
+      setWindowSize(getWindowSize())
+    }
+    window.addEventListener('resize', resizeHandler);
+    return () => window.removeEventListener('resize', resizeHandler);
+  })
+
   return (
-    < >
+    <>
       <Header />
-      <Home />
+      <div style={{ height: windowSize.h - FOOTER_OFFSET }}>
+        <Home />
+      </div>
       <Footer />
     </>
+
   );
 }
 
