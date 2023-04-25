3942e129ef235f2b47925c695b74e7d5b9876ac5
Tina <tinzzy.cracked@yahoo.com>
2023-04-25 17:07:30 -0400

    changed depth display to breadcrumb

--- mongodb-browser/src/front-end/src/components/DepthTitle.js
+++ mongodb-browser/src/front-end/src/components/DepthTitle.js
@@ -1,98 +1,27 @@
 import * as React from 'react';
 
-import PropTypes from 'prop-types';
-import { styled } from '@mui/material/styles';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import CircleRoundedIcon from '@mui/icons-material/CircleRounded';
-import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
-const QontoConnector = styled(StepConnector)(({ theme }) => ({
-    [`&.${stepConnectorClasses.alternativeLabel}`]: {
-        top: 10,
-        left: 'calc(-50% + 16px)',
-        right: 'calc(50% + 16px)',
-    },
-    [`&.${stepConnectorClasses.active}`]: {
-        [`& .${stepConnectorClasses.line}`]: {
-            borderColor: '#784af4',
-        },
-    },
-    [`&.${stepConnectorClasses.completed}`]: {
-        [`& .${stepConnectorClasses.line}`]: {
-            borderColor: '#784af4',
-        },
-    },
-    [`& .${stepConnectorClasses.line}`]: {
-        borderColor: theme.palette.mode === 'dark' ? '#65cce4' : '#65cce4',
-        borderTopWidth: 2,
-        borderRadius: 1,
-    },
-}));
 
-const QontoStepIconRoot = styled('div')(({ theme, ownerState }) => ({
-    color: theme.palette.mode === 'dark' ? '#65cce4' : '#65cce4',
-    display: 'flex',
-    height: 22,
-    alignItems: 'left',
-    ...(ownerState.active && {
-        color: '#65cce4',
-    }),
-    '& .QontoStepIcon-completedIcon': {
-        color: '#65cce4',
-        zIndex: 1,
-        fontSize: 18,
-    },
-    '& .QontoStepIcon-circle': {
-        width: 4,
-        height: 4,
-        borderRadius: '50%',
-        backgroundColor: 'currentColor',
-    },
-}));
-
-function QontoStepIcon(props) {
-    const { active, completed, className } = props;
+export default function CustomSeparator(props) {
+    const breadcrumbs = [
+        <Link underline="none" key="1" color="inherit" >
+            {props.database}
+        </Link>,
+        <Link underline="none" key="1" color="text.primary" >
+            {props.collection}
+        </Link>
+    ];
 
     return (
-        <QontoStepIconRoot ownerState={{ active }} className={className}>
-            {completed ? (
-                <CircleRoundedIcon className="QontoStepIcon-completedIcon" />
-            ) : (
-                <div className="QontoStepIcon-circle" />
-            )}
-        </QontoStepIconRoot>
-    );
-}
-
-QontoStepIcon.propTypes = {
-    /**
-     * Whether this step is active.
-     * @default false
-     */
-    active: PropTypes.bool,
-    className: PropTypes.string,
-    /**
-     * Mark the step as completed. Is passed to child components.
-     * @default false
-     */
-    completed: PropTypes.bool,
-};
-
-export default function DepthTitle(props) {
-    const steps = [props.database, props.collection];
-
-    return (
-        <div>
-            <Stepper alternativeLabel activeStep={2} connector={<QontoConnector />}>
-                {steps.map((label) => (
-                    <Step key={label}>
-                        <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
-                    </Step>
-                ))}
-            </Stepper>
-
-        </div>
+        <>
+            <Breadcrumbs
+                separator={<NavigateNextIcon fontSize="small" />}
+                aria-label="breadcrumb">
+                {breadcrumbs}
+            </Breadcrumbs>
+        </>
     );
 }
