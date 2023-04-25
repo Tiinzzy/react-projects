0c18de3cb2fd84ea081b52827d3c6307595e9cc8
Tina <tinzzy.cracked@yahoo.com>
2023-04-25 10:03:04 -0400

    changed depth title and need to fix further

--- mongodb-browser/src/front-end/src/components/DepthTitle.js
+++ mongodb-browser/src/front-end/src/components/DepthTitle.js
@@ -1,45 +1,98 @@
 import * as React from 'react';
 
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import Typography from '@mui/material/Typography';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import CircleRoundedIcon from '@mui/icons-material/CircleRounded';
+import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
 
-import './style.css';
+const QontoConnector = styled(StepConnector)(({ theme }) => ({
+    [`&.${stepConnectorClasses.alternativeLabel}`]: {
+        top: 10,
+        left: 'calc(-50% + 16px)',
+        right: 'calc(50% + 16px)',
+    },
+    [`&.${stepConnectorClasses.active}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+            borderColor: '#784af4',
+        },
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+            borderColor: '#784af4',
+        },
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+        borderColor: theme.palette.mode === 'dark' ? '#65cce4' : '#65cce4',
+        borderTopWidth: 2,
+        borderRadius: 1,
+    },
+}));
 
-const test = [1, 2, 3, 4, 5, 6];
+const QontoStepIconRoot = styled('div')(({ theme, ownerState }) => ({
+    color: theme.palette.mode === 'dark' ? '#65cce4' : '#65cce4',
+    display: 'flex',
+    height: 22,
+    alignItems: 'left',
+    ...(ownerState.active && {
+        color: '#65cce4',
+    }),
+    '& .QontoStepIcon-completedIcon': {
+        color: '#65cce4',
+        zIndex: 1,
+        fontSize: 18,
+    },
+    '& .QontoStepIcon-circle': {
+        width: 4,
+        height: 4,
+        borderRadius: '50%',
+        backgroundColor: 'currentColor',
+    },
+}));
 
-export default class DepthTitle extends React.Component {
+function QontoStepIcon(props) {
+    const { active, completed, className } = props;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
+    return (
+        <QontoStepIconRoot ownerState={{ active }} className={className}>
+            {completed ? (
+                <CircleRoundedIcon className="QontoStepIcon-completedIcon" />
+            ) : (
+                <div className="QontoStepIcon-circle" />
+            )}
+        </QontoStepIconRoot>
+    );
+}
 
+QontoStepIcon.propTypes = {
+    /**
+     * Whether this step is active.
+     * @default false
+     */
+    active: PropTypes.bool,
+    className: PropTypes.string,
+    /**
+     * Mark the step as completed. Is passed to child components.
+     * @default false
+     */
+    completed: PropTypes.bool,
+};
 
-    render() {
-        return (
-            <div>
-                <Timeline className='timeline' position="alternate">
-                    {test.map((e, i) => (
-                        <TimelineItem key={i}>
-                            <TimelineSeparator>
-                                <TimelineDot className='timelineIcon' />
-                                <TimelineConnector />
-                            </TimelineSeparator>
-                            <TimelineContent className="timelineContentContainer">
-                                <Typography className='timelineContent'>
-                                    {e}
-                                </Typography>
-                            </TimelineContent>
-                        </TimelineItem>))}
+export default function DepthTitle(props) {
+    const steps = [props.database, props.collection];
 
-                </Timeline>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Stepper alternativeLabel activeStep={2} connector={<QontoConnector />}>
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+
+        </div>
+    );
 }
