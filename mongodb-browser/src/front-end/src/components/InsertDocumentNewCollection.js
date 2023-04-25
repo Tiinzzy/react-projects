eb3ebd42607ba97c2588d0013d355945eceee421
Tina <tinzzy.cracked@yahoo.com>
2023-04-25 11:30:24 -0400

    added try catch for checking if json is valid or not

--- mongodb-browser/src/front-end/src/components/InsertDocumentNewCollection.js
+++ mongodb-browser/src/front-end/src/components/InsertDocumentNewCollection.js
@@ -28,7 +28,8 @@ export default class InsertDocumentNewCollection extends React.Component {
             newData: '',
             collectionName: '',
             databaseName: '',
-            query: {}
+            query: {},
+            errorMessage: ''
         }
     }
 
@@ -42,17 +43,17 @@ export default class InsertDocumentNewCollection extends React.Component {
     }
 
     getDatabaseName(e) {
-        this.setState({ databaseName: e.target.value }, () => {
+        this.setState({ databaseName: e.target.value, errorMessage: '' }, () => {
             this.state.query['database_name'] = this.state.databaseName;
         });
     }
 
     getDocumentData(e) {
-        this.setState({ newData: e.target.value });
+        this.setState({ newData: e.target.value, errorMessage: '' });
     }
 
     getNewCollectionName(e) {
-        this.setState({ collectionName: e.target.value }, () => {
+        this.setState({ collectionName: e.target.value, errorMessage: '' }, () => {
             this.state.query['collection_name'] = this.state.collectionName;
         });
     }
@@ -62,14 +63,19 @@ export default class InsertDocumentNewCollection extends React.Component {
     }
 
     submitAndClose() {
-        let parsedDocument = JSON.parse(this.state.newData);
-        this.state.query['documents'] = parsedDocument;
-        backend.insert_documents_mongo_db(this.state.query, (data) => {
-            if (data.inserted_count > 0) {
-                this.state.handleCloseDialog({ action: 'close-dialog' });
-                console.log('successful');
-            };
-        })
+        try {
+            let parsedDocument = JSON.parse(this.state.newData);
+            this.state.query['documents'] = parsedDocument;
+            backend.insert_documents_mongo_db(this.state.query, (data) => {
+                if (data.inserted_count > 0) {
+                    this.state.handleCloseDialog({ action: 'close-dialog' });
+                };
+            })
+        }
+        catch (err) {
+            console.log(err)
+            this.setState({ errorMessage: err.toString() });
+        }
     }
 
     render() {
@@ -99,6 +105,7 @@ export default class InsertDocumentNewCollection extends React.Component {
                         value={this.state.collectionName}
                         onChange={(e) => this.getNewCollectionName(e)} />
                     <TextField
+                        sx={{ marginBottom: 3 }}
                         fullWidth multiline
                         rows={20}
                         label="Data"
@@ -106,7 +113,8 @@ export default class InsertDocumentNewCollection extends React.Component {
                         value={this.state.newData}
                         onChange={(e) => this.getDocumentData(e)}
                     />
-                    <Box style={{ width: 1000 }}>
+                    <Box style={{ width: 1000, border: 'solid 0px red', height: 10 }}>
+                        {this.state.errorMessage !== '' && this.state.errorMessage}
                     </Box>
                 </DialogContent>
                 <DialogActions>
