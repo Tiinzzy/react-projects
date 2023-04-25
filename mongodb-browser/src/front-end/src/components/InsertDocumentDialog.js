45886b29fdda95c5d23235682047d01a8d9c8589
Tina <tinzzy.cracked@yahoo.com>
2023-04-25 10:23:01 -0400

    dialog component for inserting document in a preexisting collection

---
+++ mongodb-browser/src/front-end/src/components/InsertDocumentDialog.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+import './style.css';
+
+export default class InsertDocumentDialog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+
+    render() {
+        return (
+            <Box>
+                insert into collection a document componentDidMount
+            </Box>
+        );
+    }
+}
