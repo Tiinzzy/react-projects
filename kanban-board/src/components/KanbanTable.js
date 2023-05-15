cb8365ef4bb6d667bbaf04b2b920fc78e2be2c53
Tina <tinzzy.cracked@yahoo.com>
2023-05-15 13:29:34 -0400

    changed format of the display

--- kanban-board/src/components/KanbanTable.js
+++ kanban-board/src/components/KanbanTable.js
@@ -6,7 +6,7 @@ const KANBAN_HEADERS = ['Backlog', 'To Do', 'In Progress', 'Completed'];
 export default function KanbanTable() {
     const dragItem = useRef();
     const dragOverItem = useRef();
-    const [list, setList] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4']);
+    const [list, setList] = useState([['Item 1', 123], ['Item 2', 434], ['Item 3', 5453], ['Item 4', 675]]);
 
     const dragStart = (e, position) => {
         dragItem.current = position;
@@ -32,7 +32,7 @@ export default function KanbanTable() {
     };
 
     return (
-        <div style={{ width: 1000, height: 1000 }}>
+        <div style={{ width: 1400 }}>
             <table width="100%" style={{ fontSize: '80%', backgroundColor: 'white', maring: 5, border: 'solid 1px #eaeaea' }} cellPadding={0} cellSpacing={1}>
                 <tbody>
                     <tr>
@@ -43,14 +43,15 @@ export default function KanbanTable() {
                     <tr>
                         {list && list.map((item, index) => (
                             <td key={index}
-                                draggable
-                                onDragStart={(e) => dragStart(e, index)}
-                                onDragEnter={(e) => dragEnter(e, index)}
-                                onDragEnd={drop}
-                                width='25%'>{item}
-                                <div >
-                                    123
-                                </div>
+                                width='25%'>{item.map((e, i) => (
+                                    <div draggable
+                                        onDragStart={(e) => dragStart(e, index)}
+                                        onDragEnter={(e) => dragEnter(e, index)}
+                                        onDragEnd={drop}
+                                        key={i}>
+                                        {e}
+                                    </div>
+                                ))}
                             </td>
                         ))}
                     </tr>
