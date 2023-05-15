e9df1850d655ac3f64b4081848b039c77acda73d
Tina <tinzzy.cracked@yahoo.com>
2023-05-15 12:02:16 -0400

    made some changes

--- kanban-board/src/components/KanbanTable.js
+++ kanban-board/src/components/KanbanTable.js
@@ -11,23 +11,20 @@ export default function KanbanTable() {
     const dragStart = (e, position) => {
         dragItem.current = position;
         console.log(position, 1)
-        // console.log(e.target.innerHTML);
+        console.log(e.target.innerHTML, '<<');
     };
 
     const dragEnter = (e, position) => {
         dragOverItem.current = position;
         console.log(position, 2)
-        // console.log(e.target.innerHTML);
+        console.log(e.target.innerHTML, '<<');
     };
 
     const drop = (e) => {
         const copyListItems = [...list];
         const dragItemContent = copyListItems[dragItem.current];
-        console.log(copyListItems, '1')
         copyListItems.splice(dragItem.current, 1);
-        console.log(copyListItems, '2')
         copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-        console.log(copyListItems, '3')
         dragItem.current = null;
         dragOverItem.current = null;
         setList(copyListItems);
@@ -35,8 +32,8 @@ export default function KanbanTable() {
     };
 
     return (
-        <>
-            <table width="100%" style={{ fontSize: '80%', backgroundColor: 'white', maring: 5 }} cellPadding={0} cellSpacing={1}>
+        <div style={{ width: 1000, height: 1000 }}>
+            <table width="100%" style={{ fontSize: '80%', backgroundColor: 'white', maring: 5, border: 'solid 1px #eaeaea' }} cellPadding={0} cellSpacing={1}>
                 <tbody>
                     <tr>
                         {KANBAN_HEADERS.map((j, k) => (
@@ -50,12 +47,16 @@ export default function KanbanTable() {
                                 onDragStart={(e) => dragStart(e, index)}
                                 onDragEnter={(e) => dragEnter(e, index)}
                                 onDragEnd={drop}
-                                width='25%'>{item}</td>
+                                width='25%'>{item}
+                                <div >
+                                    123
+                                </div>
+                            </td>
                         ))}
                     </tr>
 
                 </tbody>
             </table>
-        </>
+        </div>
     );
 };
