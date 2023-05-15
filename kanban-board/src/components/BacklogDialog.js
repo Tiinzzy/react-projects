7a4dd2ee1ec359afdf084f58853274a6efb16f97
Tina <tinzzy.cracked@yahoo.com>
2023-05-15 14:38:49 -0400

    fixed priority and status drop down menu

--- kanban-board/src/components/BacklogDialog.js
+++ kanban-board/src/components/BacklogDialog.js
@@ -4,15 +4,32 @@ import Button from '@mui/material/Button';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+
+const STATUS = ['Backlog', 'To Do', 'In Progress', 'Completed'];
+const PRIORITY = ['High', 'Low', 'Medium'];
 
 export default class BacklogDialog extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            status: STATUS[0],
+            priority: PRIORITY[1]
         }
+    }
+
+    handleChangeStatus(e) {
+        this.setState({ status: e.target.value });
+    }
+
+    handleChangePriority(e) {
+        this.setState({ priority: e.target.value });
     }
 
     render() {
@@ -25,8 +42,28 @@ export default class BacklogDialog extends React.Component {
                     <Box style={{ marginTop: 20, display: 'flex', flexDirection: 'column', width: 500 }}>
                         <TextField label="Title" variant="outlined" style={{ marginBottom: 18 }} />
                         <TextField fullWidth multiline rows={5} label="Description" variant="outlined" style={{ marginBottom: 18 }} />
-                        <TextField label="Status" variant="outlined" style={{ marginBottom: 18 }} />
-                        <TextField label="Priority" variant="outlined" style={{ marginBottom: 18 }} />
+                        <FormControl fullWidth style={{ marginBottom: 18 }}>
+                            <InputLabel >Status</InputLabel>
+                            <Select
+                                value={this.state.status}
+                                label="Status"
+                                onChange={(e) => this.handleChangeStatus(e)}>
+                                {STATUS.map((e, i) => (
+                                    <MenuItem key={i} value={e}>{e}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <FormControl fullWidth style={{ marginBottom: 18 }}>
+                            <InputLabel >Priority</InputLabel>
+                            <Select
+                                value={this.state.priority}
+                                label="Priority"
+                                onChange={(e) => this.handleChangePriority(e)}>
+                                {PRIORITY.map((e, i) => (
+                                    <MenuItem key={i} value={e}>{e}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
                     </Box>
                 </DialogContent>
                 <DialogActions>
