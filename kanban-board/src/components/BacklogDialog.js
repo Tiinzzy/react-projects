4d4f742beea9d36d37a6cb1b3c17a995f927509f
Tina <tinzzy.cracked@yahoo.com>
2023-05-15 14:21:14 -0400

    added bases of dialog have to make list for status and priority and also fix buttons

--- kanban-board/src/components/BacklogDialog.js
+++ kanban-board/src/components/BacklogDialog.js
@@ -3,8 +3,9 @@ import React from "react";
 import Button from '@mui/material/Button';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 export default class BacklogDialog extends React.Component {
 
@@ -17,20 +18,20 @@ export default class BacklogDialog extends React.Component {
     render() {
         return (
             <>
-                <DialogTitle id="alert-dialog-title">
-                    {"Use Google's location service?"}
+                <DialogTitle>
+                    {"Add New Task"}
                 </DialogTitle>
                 <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.
-                    </DialogContentText>
+                    <Box style={{ marginTop: 20, display: 'flex', flexDirection: 'column', width: 500 }}>
+                        <TextField label="Title" variant="outlined" style={{ marginBottom: 18 }} />
+                        <TextField fullWidth multiline rows={5} label="Description" variant="outlined" style={{ marginBottom: 18 }} />
+                        <TextField label="Status" variant="outlined" style={{ marginBottom: 18 }} />
+                        <TextField label="Priority" variant="outlined" style={{ marginBottom: 18 }} />
+                    </Box>
                 </DialogContent>
                 <DialogActions>
-                    <Button >Disagree</Button>
-                    <Button >
-                        Agree
-                    </Button>
+                    <Button variant="outlined">Cancel</Button>
+                    <Button variant="outlined">Submit</Button>
                 </DialogActions>
             </>
         );
