0f3900303ea23002481d04b49a50d0caa528cd35
Tina <tinzzy.cracked@yahoo.com>
2023-05-15 11:45:49 -0400

    fixing kanban table

--- kanban-board/src/components/KanbanTable.js
+++ kanban-board/src/components/KanbanTable.js
@@ -1,71 +1,61 @@
-import React from "react";
-
+import React, { useState, useRef } from 'react';
 import './style.css';
 
 const KANBAN_HEADERS = ['Backlog', 'To Do', 'In Progress', 'Completed'];
-const TEST = ['Item 1', 'Item 2', 'Item 3', 'Item 4']
 
-export default class KanbanTable extends React.Component {
+export default function KanbanTable() {
+    const dragItem = useRef();
+    const dragOverItem = useRef();
+    const [list, setList] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4']);
 
-    constructor(props) {
-        super(props);
-        this.dragItem = React.createRef();
-        this.dragOverItem = React.createRef();
-        this.state = {
-            list: TEST,
-            dragItem: null,
-            dragOverItem: null
-        }
-    }
+    const dragStart = (e, position) => {
+        dragItem.current = position;
+        console.log(position, 1)
+        // console.log(e.target.innerHTML);
+    };
 
-    handleDragStart(e, position) {
-        console.log(position, 'start postion');
-        this.dragItem.current = position;
-        this.setState({ dragItem: this.dragItem.current });
-        console.log(e.target.innerHTML);
-        console.log("Dragging STARTED")
-    }
+    const dragEnter = (e, position) => {
+        dragOverItem.current = position;
+        console.log(position, 2)
+        // console.log(e.target.innerHTML);
+    };
 
-    handleDrag(e, position) {
-        console.log(position, 'moving position');
-        this.dragOverItem.current = position;
-        this.setState({ dragOverItem: this.dragOverItem.current });
-        console.log(e.target.innerHTML);
-        console.log("Dragging CURRENT...")
-    }
+    const drop = (e) => {
+        const copyListItems = [...list];
+        const dragItemContent = copyListItems[dragItem.current];
+        console.log(copyListItems, '1')
+        copyListItems.splice(dragItem.current, 1);
+        console.log(copyListItems, '2')
+        copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+        console.log(copyListItems, '3')
+        dragItem.current = null;
+        dragOverItem.current = null;
+        setList(copyListItems);
+        e.preventDefault();
+    };
 
-    handleDragEnd() {
-        const copy_test = [...TEST];
-        const dragItemContent = copy_test[this.dragItem.current];
-        copy_test.splice(this.state.dragItem, 1);
-        copy_test.splice(this.state.dragOverItem, 0, dragItemContent);
-        this.setState({ list: copy_test })
+    return (
+        <>
+            <table width="100%" style={{ fontSize: '80%', backgroundColor: 'white', maring: 5 }} cellPadding={0} cellSpacing={1}>
+                <tbody>
+                    <tr>
+                        {KANBAN_HEADERS.map((j, k) => (
+                            <th key={k} width='25%'>{j}</th>
+                        ))}
+                    </tr>
+                    <tr>
+                        {list && list.map((item, index) => (
+                            <td key={index}
+                                draggable
+                                onDragStart={(e) => dragStart(e, index)}
+                                onDragEnter={(e) => dragEnter(e, index)}
+                                onDragEnd={drop}
+                                width='25%'>{item}</td>
+                        ))}
+                    </tr>
 
-        console.log("Ended")
-    }
-
-    render() {
-        return (
-            <>
-                <table width="100%" style={{ fontSize: '80%', backgroundColor: 'white', maring: 5 }} cellPadding={0} cellSpacing={1}>
-                    <tbody>
-                        <tr>
-                            {KANBAN_HEADERS.map((e, i) => (
-                                <th key={i} width='25%'>{e}</th>
-                            ))}
-                        </tr>
-                        <tr>
-                            {this.state.list && this.state.list.map((e, i) => (
-                                <td key={i}
-                                    draggable
-                                    onDragStart={(e) => this.handleDragStart(e, i)}
-                                    onDragEnter={(e) => this.handleDrag(e, i)}
-                                    onDragEnd={this.handleDragEnd} width='25%'>{e}</td>
-                            ))}
-                        </tr>
-                    </tbody>
-                </table>
-            </>
-        );
-    }
-}
+                </tbody>
+            </table>
+        </>
+    );
+};
