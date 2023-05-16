345249e15ed39efd192547c75ff1944a5292390e
Tina <tinzzy.cracked@yahoo.com>
2023-05-16 09:36:19 -0400

    fixed naming

--- kanban-board/src/components/MainHome.js
+++ kanban-board/src/components/MainHome.js
@@ -17,7 +17,7 @@ class MainHome extends React.Component {
         return (
             <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: 20 }}>
                 <Box style={{ width: 1400, marginBottom: 35 }}>
-                    <Typography fontWeight="bold" fontFamily="helvetica" fontSize="18px"> Kanbana Board</Typography>
+                    <Typography fontWeight="bold" fontFamily="helvetica" fontSize="18px"> Kanban Board</Typography>
                 </Box>
                 <KanbanTable />
             </Box>
