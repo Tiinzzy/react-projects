06c4f5fbb7a539fd18fee6df50283d327e327fd9
Tina <tinzzy.cracked@yahoo.com>
2023-05-16 10:35:16 -0400

    running the calculation and getting data from the class before sending to app

---
+++ kanban-board/src/back-end/app_mongodb.py
@@ -0,0 +1,35 @@
+from mongo_db_client import MongoDBClient
+
+
+def insert_documents(parameters):
+    host_name = 'localhost'
+    port_name = 27017
+    database_name = 'kanban'
+    collection_name = 'kanban_board'
+    documents = parameters.get('documents')
+    print(documents, '<<<')
+
+    client = MongoDBClient(host_name, port_name)
+    connection = client.connect()
+    if connection:
+        result = client.insert_documents(database_name, collection_name, documents)
+        client.disconnect()
+        return result
+    else:
+        return {'result': False}
+
+
+def get_documents():
+    host_name = 'localhost'
+    port_name = 27017
+    database_name = 'kanban'
+    collection_name = 'kanban_board'
+
+    client = MongoDBClient(host_name, port_name)
+    connection = client.connect()
+    if connection:
+        documents = client.get_all_documents(database_name, collection_name)
+        client.disconnect()
+        return documents
+    else:
+        return {'result': False}
