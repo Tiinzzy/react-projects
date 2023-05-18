4fe81bf569b9d1086ea710b48a2141ba9b88bf99
Tina <tinzzy.cracked@yahoo.com>
2023-05-18 09:35:35 -0400

    api functional rendering of data to insert in mongodb

--- kanban-board/src/back-end/app_mongodb.py
+++ kanban-board/src/back-end/app_mongodb.py
@@ -7,7 +7,6 @@ def insert_documents(parameters):
     database_name = 'kanban'
     collection_name = 'kanban_board'
     documents = parameters.get('documents')
-    print(documents, '<<<')
 
     client = MongoDBClient(host_name, port_name)
     connection = client.connect()
@@ -31,5 +30,22 @@ def get_documents():
         documents = client.get_all_documents(database_name, collection_name)
         client.disconnect()
         return documents
+    else:
+        return {'result': False}
+
+
+def insert_comment(parameters):
+    host_name = 'localhost'
+    port_name = 27017
+    database_name = 'kanban'
+    collection_name = 'kanban_comment'
+    documents = parameters.get('documents')
+
+    client = MongoDBClient(host_name, port_name)
+    connection = client.connect()
+    if connection:
+        result = client.insert_documents(database_name, collection_name, documents)
+        client.disconnect()
+        return result
     else:
         return {'result': False}
