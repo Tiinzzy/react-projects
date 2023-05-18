20fd7753088d8c62ede3b52204b7d739ea29244f
Tina <tinzzy.cracked@yahoo.com>
2023-05-18 09:02:40 -0400

    created the base of the dialog have to fix getting data and submitting them

--- kanban-board/src/front-end/src/components/CommenDialog.js
+++ kanban-board/src/front-end/src/components/CommenDialog.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+import Button from '@mui/material/Button';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
 
 export default class CommenDialog extends React.Component {
 
@@ -12,7 +18,19 @@ export default class CommenDialog extends React.Component {
     render() {
         return (
             <>
-                {'this is a test'}
+                <DialogTitle>
+                    {"Make Comment"}
+                </DialogTitle>
+                <DialogContent>
+                    <Box style={{ marginTop: 20, display: 'flex', flexDirection: 'column', width: 500 }}>
+                        <TextField fullWidth multiline rows={6} label="Comment"
+                            variant="outlined" style={{ marginBottom: 12 }} />
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="outlined" >Cancel</Button>
+                    <Button variant="outlined" >Submit</Button>
+                </DialogActions>
             </>
         );
     }
