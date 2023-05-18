456b7bda792404db793381ddd02f825627cfd524
Tina <tinzzy.cracked@yahoo.com>
2023-05-18 09:35:54 -0400

    post api to save comments

--- kanban-board/src/back-end/app.py
+++ kanban-board/src/back-end/app.py
@@ -12,7 +12,6 @@ def get_parameters(req):
 @app.route('/mongodb/insert_documents', methods=["POST"])
 def insert_documents_mongo_db():
     parameters = get_parameters(request)
-    print(parameters)
     result = app_mongodb.insert_documents(parameters)
     return jsonify(result)
 
@@ -20,4 +19,11 @@ def insert_documents_mongo_db():
 @app.route('/mongodb/get_documents', methods=["POST"])
 def get_documents_mongo_db():
     result = app_mongodb.get_documents()
+    return jsonify(result)
+
+
+@app.route('/mongodb/insert_comment', methods=["POST"])
+def insert_comments_mongo_db():
+    parameters = get_parameters(request)
+    result = app_mongodb.insert_comment(parameters)
     return jsonify(result)
