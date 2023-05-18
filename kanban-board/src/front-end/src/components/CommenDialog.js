07e18e0d23e34555cec6060c2ec59bf3099924a5
Tina <tinzzy.cracked@yahoo.com>
2023-05-18 08:55:06 -0400

    component to open up as dialog and allows user to leave comment for task

---
+++ kanban-board/src/front-end/src/components/CommenDialog.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default class CommenDialog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <>
+                {'this is a test'}
+            </>
+        );
+    }
+};
