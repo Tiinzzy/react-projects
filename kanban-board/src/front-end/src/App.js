80bd2ea5be0c072846f6de30c6e4175dde008f09
Tina <tinzzy.cracked@yahoo.com>
2023-05-21 19:09:37 -0400

    testing and trying NEXT.js

---
+++ kanban-board/src/front-end/src/App.js
@@ -0,0 +1,15 @@
+import MainHome from "./components/MainHome";
+
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <>
+      <div id="test"></div>
+      <MainHome /> 
+      {ReactDOM.createPortal(<h1>CHILD</h1>, document.body)}
+    </>
+  );
+}
+
+export default App;
