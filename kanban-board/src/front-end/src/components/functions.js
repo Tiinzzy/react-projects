7ecad09d1f20db0a6f4db9ef08ab00f9d6e78e90
Tina <tinzzy.cracked@yahoo.com>
2023-05-23 10:42:37 -0400

    code cleanup

--- kanban-board/src/front-end/src/components/functions.js
+++ kanban-board/src/front-end/src/components/functions.js
@@ -8,16 +8,7 @@ export function getLogList(serialLogs, HEADER_TO_INDEX) {
     return list;
 }
 
-// TODO: fix variable names here and everywhere
-export function valueExist(array, key, value) {
-    // TODO: try to use more fuinctional things ...
-    let result = array.filter(d => d[key] === value);
+export function valueExist(kanbanBoard, key, value) {
+    let result = kanbanBoard.filter(d => d[key] === value);
     return result.length > 0 ? result[0] : null;
-
-    // for (let i = 0; i < array.length; i++) {
-    //     if (array[i][key] === value) {
-    //         return array[i];
-    //     }
-    // }
-    // return null;
 }
