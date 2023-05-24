e2019c1fa88b676a28da96f429536da9391ec8e6
Tina <tinzzy.cracked@yahoo.com>
2023-05-24 17:26:23 -0400

    updated types

--- next-js/next-test-2/src/app/components/kanban-types.tsx
+++ next-js/next-test-2/src/app/components/kanban-types.tsx
@@ -1,7 +1,7 @@
 export type KanbanBoardProps = {
     title: string,
     paragraph: string,
-    boardData: Array<{ [key: string]: string }>
+    boardData: BoardType
 }
 
 export type StateInfoType = {
@@ -16,4 +16,8 @@ export type TaskType = {
     title: string
 }
 
-export type ArrayOfObjects = [Array<{ [key: string]: string }>, Array<{ [key: string]: string }>, Array<{ [key: string]: string }>, Array<{ [key: string]: string }>];
+export type TaskColumnType = Array<{ [key: string]: string }>;
+export let emptyTaskColumnType: TaskColumnType = [];
+
+export type BoardType = [TaskColumnType, TaskColumnType, TaskColumnType, TaskColumnType];
+export let emptyBoard: BoardType = [emptyTaskColumnType, emptyTaskColumnType, emptyTaskColumnType, emptyTaskColumnType];
