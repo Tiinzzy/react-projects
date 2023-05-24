5883b3b9100aeff0a120acff1644294034aac714
Tina <tinzzy.cracked@yahoo.com>
2023-05-24 13:18:37 -0400

    fixed updating the backend as moving around

--- next-js/next-test-2/src/app/components/kanban-board.tsx
+++ next-js/next-test-2/src/app/components/kanban-board.tsx
@@ -1,7 +1,10 @@
 import { TaskType, ArrayOfObjects } from './kanban-types';
+import BackEndConnection from './BackEndConnection';
 
-const NAME_2_INDEX: { [key: string]: number; } = { 'back-log': 0, 'to-do': 1, 'in-progress': 2, 'completed': 3 };
-const INDEX_2_NAME: { [key: number]: string; } = { 0: 'back-log', 1: 'to-do', 2: 'in-progress', 3: 'completed' };
+const backend = BackEndConnection.INSTANCE();
+
+const NAME_2_INDEX: { [key: string]: number; } = { 'Backlog': 0, 'To Do': 1, 'In Progress': 2, 'Completed': 3 };
+const INDEX_2_NAME: { [key: number]: string; } = { 0: 'Backlog', 1: 'To Do', 2: 'In Progress', 3: 'Completed' };
 const HEADER_TO_INDEX: { [key: string]: number; } = { 'Backlog': 0, 'To Do': 1, 'In Progress': 2, 'Completed': 3 };
 
 
@@ -9,22 +12,22 @@ export const getData = (): TaskType[] => {
     return [
         {
             id: 10,
-            status: 'to-do',
+            status: 'Backlog',
             title: 'Fixing the css issue in order page'
         },
         {
             id: 12,
-            status: 'back-log',
+            status: 'To Do',
             title: 'Moving the project to next js'
         },
         {
             id: 15,
-            status: 'in-progress',
+            status: 'Completed',
             title: 'Fixing the drag and drop issue'
         },
         {
             id: 16,
-            status: 'in-progress',
+            status: 'In Progress',
             title: 'New user interface documentation'
         }
     ]
@@ -42,18 +45,17 @@ export const getBoardData = (data: TaskType[]): TaskType[][] => {
 }
 
 export const updateBoard = (data: ArrayOfObjects, taskPosition: { colIndex: number, rowIndex: number }, toColumnIndex: number): ArrayOfObjects => {
-    console.log(data)
     data = [...data];
     let task = data[taskPosition.colIndex][taskPosition.rowIndex];
     task.status = INDEX_2_NAME[toColumnIndex];
     data[toColumnIndex].push(task);
     data[taskPosition.colIndex].splice(taskPosition.rowIndex, 1);
+    backend.update_document_mongo_db({ documents: data });
     return data;
 }
 
 export function getLogList(serialLogs: Array<{ [key: string]: string }>): object {
     let list: ArrayOfObjects = [[], [], [], []];
-
     serialLogs.forEach(e => {
         list[HEADER_TO_INDEX[e.status]].push(e);
     });
