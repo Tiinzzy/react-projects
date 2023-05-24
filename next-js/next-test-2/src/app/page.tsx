3ffcf03c4bf948975268a8d6a30f265a6a2112a9
Tina <tinzzy.cracked@yahoo.com>
2023-05-24 17:25:50 -0400

    fixed the data display and rendering

--- next-js/next-test-2/src/app/page.tsx
+++ next-js/next-test-2/src/app/page.tsx
@@ -4,25 +4,31 @@ import React, { useEffect, useState } from 'react';
 
 import { KanbanBoard } from "./components/KanbanBoard";
 import { getLogList } from './components/kanban-board';
-
+import { emptyBoard, BoardType } from './components/kanban-types'
 
 import BackEndConnection from './components/BackEndConnection';
 
 const backend = BackEndConnection.INSTANCE();
 
+
 export default function Home() {
-  const [boardData, setBoardData] = useState(Array<{ [key: string]: string }>);
+  const [count, setCount] = useState(0);
+  const [boardData, setBoardData] = useState(emptyBoard);
+
 
   useEffect(() => {
-    backend.get_documents_from_mongo_db((data: any) => {
-      let newBoard: any = getLogList(data.documents);
-      setBoardData(newBoard);
-    })
+    if (count === 0) {
+      backend.get_documents_from_mongo_db((data: any) => {
+        let board: BoardType = getLogList(data.documents);
+        setBoardData(board);
+        setCount(1)
+      });
+    }
+  }, [boardData, count]);
 
-  }, []);
   return (
     <div style={{ width: '100%' }}>
-      {boardData !== null &&
+      {count > 0 &&
         <KanbanBoard
           boardData={boardData}
           title="Kanban Board"
