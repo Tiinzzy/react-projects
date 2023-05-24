fb3debe9f8cdfc85ad4378a26125818b04a0e9d3
Tina <tinzzy.cracked@yahoo.com>
2023-05-24 09:40:07 -0400

    processing the board and passing to the child

--- next-js/next-test-2/src/app/page.tsx
+++ next-js/next-test-2/src/app/page.tsx
@@ -1,11 +1,32 @@
-import { KanbanBoard } from "./components/KanbanBoard"
+"use client"
+
+import React, { useEffect, useState } from 'react';
+
+import { KanbanBoard } from "./components/KanbanBoard";
+import { getLogList } from './components/kanban-board';
+
+
+import BackEndConnection from './components/BackEndConnection';
+
+const backend = BackEndConnection.INSTANCE();
 
 export default function Home() {
+  const [boardData, setBoardData] = useState(Array<object>);
+
+  useEffect(() => {
+    backend.get_documents_from_mongo_db((data: any) => {
+      let newBoard: any = getLogList(data.documents);
+      setBoardData(newBoard);
+    })
+
+  }, []);
   return (
     <div style={{ width: '100%' }}>
-      <KanbanBoard
-        title="Kanban Board"
-        paragraph="This is my personal tasks board which i track my projects in it!" />
+      {boardData !== null &&
+        <KanbanBoard
+          boardData={boardData}
+          title="Kanban Board"
+          paragraph="This is my personal tasks board which i track my projects in it!" />}
 
     </div>
   )
