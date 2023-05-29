cb7a6bd3a5ab7b76ccf76ef51e3945e473b8186a
Tina <tinzzy.cracked@yahoo.com>
2023-05-29 10:56:52 -0400

    made audio play by my own manipulation without audio tag

--- sound-fade-in-out/src/components/LeftAudio.js
+++ sound-fade-in-out/src/components/LeftAudio.js
@@ -3,19 +3,36 @@ import Box from "@mui/material/Box";
 import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
 
+var a;
+
 class LeftAudio extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             audioSound: null,
-            fileName: 'No File Chosen'
+            fileName: 'No File Chosen',
+            buttonName: 'Play'
         }
     }
 
     getAudio(e) {
         if (e) {
             let audio = URL.createObjectURL(e.target.files[0]);
-            this.setState({ audioSound: audio, fileName: e.target.files[0].name });
+            this.setState({ audioSound: audio, fileName: e.target.files[0].name }, () => {
+                a = new Audio(this.state.audioSound);
+                this.setState({audioSound: a});
+            });
+        }
+    }
+
+    handleClick() {
+        console.log(this.state.buttonName)
+        if (this.state.buttonName === "Play") {
+            this.state.audioSound.play();
+            this.setState({ buttonName: 'Pause' });
+        } else {
+            this.state.audioSound.pause();
+            this.setState({ buttonName: 'Play' });
         }
     }
 
@@ -32,7 +49,8 @@ class LeftAudio extends React.Component {
                     <Typography variant="body1" ml={1}>{this.state.fileName}</Typography>
                 </Box>
                 <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 20 }}>
-                    <audio id="sound" controls src={this.state.audioSound}></audio>
+                    {/* <audio id="sound" controls src={this.state.audioSound}></audio> */}
+                    <button onClick={() => this.handleClick()}>{this.state.buttonName}</button>
                 </Box>
             </Box>
         );
