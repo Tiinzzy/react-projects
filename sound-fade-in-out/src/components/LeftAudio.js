8a745c7affc9b80f6da767d27d25859ad586bcd6
Tina <tinzzy.cracked@yahoo.com>
2023-05-29 09:51:03 -0400

    fixed left side audio upload

--- sound-fade-in-out/src/components/LeftAudio.js
+++ sound-fade-in-out/src/components/LeftAudio.js
@@ -1,17 +1,40 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
 
 class LeftAudio extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            audioSound: null,
+            fileName: 'No File Chosen'
+        }
+    }
+
+    getAudio(e) {
+        if (e) {
+            let audio = URL.createObjectURL(e.target.files[0]);
+            this.setState({ audioSound: audio, fileName: e.target.files[0].name });
         }
     }
 
     render() {
         return (
-            <div>
-                This is Left Audio
-            </div>
+            <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: 10, flexDirection: 'column', marginTop: 20, marginBottom: 20 }}>
+                <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                    <Button variant="contained" component="label">
+                        Choose File
+                        <input hidden type="file"
+                            accept="audio/mp3,audio/*;capture=microphone"
+                            onChange={(e) => this.getAudio(e)} />
+                    </Button>
+                    <Typography variant="body1" ml={1}>{this.state.fileName}</Typography>
+                </Box>
+                <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 20 }}>
+                    <audio id="sound" controls src={this.state.audioSound}></audio>
+                </Box>
+            </Box>
         );
     }
 }
