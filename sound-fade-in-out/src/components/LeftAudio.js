4a1d413856265a0621ff5f0986c4a6de346a5376
Tina <tinzzy.cracked@yahoo.com>
2023-05-29 09:23:07 -0400

    left audio box and upload

---
+++ sound-fade-in-out/src/components/LeftAudio.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class LeftAudio extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                This is Left Audio
+            </div>
+        );
+    }
+}
+export default LeftAudio;
