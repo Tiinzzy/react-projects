192fa570d7e9f48e65e6efd75886bdc89bc4c0d9
Tina <tinzzy.cracked@yahoo.com>
2023-05-29 09:23:18 -0400

    right audio box and upload

---
+++ sound-fade-in-out/src/components/RightAudio.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class RightAudio extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                This is Right Audio
+            </div>
+        );
+    }
+}
+export default RightAudio;
