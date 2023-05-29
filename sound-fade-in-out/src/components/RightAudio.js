38bea0f2daa2682a10b9cd97dcefd8757790553b
Tina <tinzzy.cracked@yahoo.com>
2023-05-29 16:18:57 -0400

    made right side exactly like left side as asked

--- sound-fade-in-out/src/components/RightAudio.js
+++ sound-fade-in-out/src/components/RightAudio.js
@@ -2,6 +2,15 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
+import Slider from '@mui/material/Slider';
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
+import PauseCircleOutlinedIcon from '@mui/icons-material/PauseCircleOutlined';
+
+var a;
 
 class RightAudio extends React.Component {
     constructor(props) {
@@ -10,13 +19,37 @@ class RightAudio extends React.Component {
             audioSound: null,
             fileName: '',
             selectFile: 'No File Chosen',
+            buttonName: 'Play',
+            volumeValue: 1,
+            buttonDisabled: true
         }
     }
 
     getAudio(e) {
         if (e) {
             let audio = URL.createObjectURL(e.target.files[0]);
-            this.setState({ audioSound: audio, fileName: e.target.files[0].name, selectFile: '' });
+            this.setState({ audioSound: audio, fileName: e.target.files[0].name, selectFile: '' }, () => {
+                a = new Audio(this.state.audioSound);
+                this.setState({ buttonDisabled: false, audioSound: a });
+            });
+        }
+    }
+
+    handlePlayAudio() {
+        if (this.state.buttonName === "Play") {
+            this.state.audioSound.play();
+            this.setState({ buttonName: 'Pause' });
+        } else {
+            this.state.audioSound.pause();
+            this.setState({ buttonName: 'Play' });
+        }
+    }
+
+    handleChangeVolume(e, newVolume) {
+        if (this.state.audioSound !== null) {
+            this.setState({ volumeValue: newVolume }, () => {
+                a.volume = this.state.volumeValue;
+            });
         }
     }
 
@@ -35,7 +68,19 @@ class RightAudio extends React.Component {
                 <Typography variant="body1" mt={2} mb={2}>{this.state.fileName}</Typography>
 
                 <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <audio id="sound" controls src={this.state.audioSound}></audio>
+                    <IconButton aria-label="delete" onClick={() => this.handlePlayAudio()} color="primary" disabled={this.state.buttonDisabled}>
+                        {this.state.buttonName === "Play" ? <PlayCircleFilledWhiteOutlinedIcon fontSize="large" /> : <PauseCircleOutlinedIcon fontSize="large" />}
+                    </IconButton>
+                    <Stack spacing={2} direction="row" sx={{ ml: 1 }} alignItems="center" style={{ width: 250 }}>
+                        <VolumeDown />
+                        <Slider step={0.1}
+                            max={1.0}
+                            min={0.0}
+                            aria-label="Volume"
+                            value={this.state.volumeValue} onChange={(e, i) => this.handleChangeVolume(e, i)} />
+                        <VolumeUp />
+                    </Stack>
+
                 </Box>
             </Box>
         );
