af6316566e0263b721a2c11b5c6f8536dd12c8db
Tina <tinzzy.cracked@yahoo.com>
2023-05-29 14:07:33 -0400

    fixed right side

--- sound-fade-in-out/src/components/RightAudio.js
+++ sound-fade-in-out/src/components/RightAudio.js
@@ -8,30 +8,33 @@ class RightAudio extends React.Component {
         super(props);
         this.state = {
             audioSound: null,
-            fileName: 'No File Chosen'
+            fileName: '',
+            selectFile: 'No File Chosen',
         }
     }
 
     getAudio(e) {
         if (e) {
             let audio = URL.createObjectURL(e.target.files[0]);
-            this.setState({ audioSound: audio, fileName: e.target.files[0].name });
+            this.setState({ audioSound: audio, fileName: e.target.files[0].name, selectFile: '' });
         }
     }
 
     render() {
         return (
             <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: 10, flexDirection: 'column', marginTop: 20, marginBottom: 20 }}>
-                <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: 300 }}>
                     <Button variant="contained" component="label">
                         Choose File
                         <input hidden type="file"
                             accept="audio/mp3,audio/*;capture=microphone"
                             onChange={(e) => this.getAudio(e)} />
                     </Button>
-                    <Typography variant="body1" ml={1}>{this.state.fileName}</Typography>
+                    <Typography variant="body1" ml={2}>{this.state.selectFile}</Typography>
                 </Box>
-                <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 20 }}>
+                <Typography variant="body1" mt={2} mb={2}>{this.state.fileName}</Typography>
+
+                <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
                     <audio id="sound" controls src={this.state.audioSound}></audio>
                 </Box>
             </Box>
