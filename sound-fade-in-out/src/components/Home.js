625a5e2e333b4511322f5eeacb8e3d3587d65391
Tina <tinzzy.cracked@yahoo.com>
2023-05-30 09:14:18 -0400

    added button to play music on one of the child component through event emitter

--- sound-fade-in-out/src/components/Home.js
+++ sound-fade-in-out/src/components/Home.js
@@ -2,6 +2,11 @@ import React from "react";
 
 import LeftAudio from "./LeftAudio";
 import RightAudio from "./RightAudio";
+import Button from "@mui/material/Button";
+
+import EventEmitter from 'eventemitter3';
+
+export const eventEmitter = new EventEmitter();
 
 class Home extends React.Component {
     constructor(props) {
@@ -10,19 +15,33 @@ class Home extends React.Component {
         }
     }
 
+    playBothAudio() {
+        const data = {
+            message: 'play audio', callBack: function (data) {
+                if (data) {
+                    console.log(data);
+                }
+            }
+        };
+        eventEmitter.emit('customEvent', data);
+    }
+
     render() {
         return (
-            <div style={{ width: '100%', display: 'flex', flexDirection: 'row', height: '500px' }}>
-                <div style={{ width: '50%', border: 'dotted 5px #145490', alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
-                    <LeftAudio />
+            <>
+                <div style={{ width: '100%', display: 'flex', flexDirection: 'row', height: '500px' }}>
+                    <div style={{ width: '50%', border: 'dotted 5px #145490', alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                        <LeftAudio />
+                    </div>
+                    <div style={{
+                        width: '50%', borderTop: 'dotted 5px #145490', borderBottom: 'dotted 5px #145490', borderRight: 'dotted 5px #145490',
+                        alignItems: 'center', justifyContent: 'center', display: 'flex'
+                    }}>
+                        <RightAudio />
+                    </div>
                 </div>
-                <div style={{
-                    width: '50%', borderTop: 'dotted 5px #145490', borderBottom: 'dotted 5px #145490', borderRight: 'dotted 5px #145490',
-                    alignItems: 'center', justifyContent: 'center', display: 'flex'
-                }}>
-                    <RightAudio />
-                </div>
-            </div>
+                <Button variant="contained" onClick={() => this.playBothAudio()}>PLay</Button>
+            </>
         );
     }
 }
