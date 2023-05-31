16e52898b9991e145d39525ac8e04d4ed25198e1
Tina <tinzzy.cracked@yahoo.com>
2023-05-31 11:55:54 -0400

    fixing elements of header component

--- sound-fade-in-out/src/components/Header.js
+++ sound-fade-in-out/src/components/Header.js
@@ -1,5 +1,8 @@
 import React from "react";
-
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 export default class Header extends React.Component {
     constructor(props) {
@@ -11,7 +14,15 @@ export default class Header extends React.Component {
     render() {
         return (
             <>
-               
+                <Box sx={{ flexGrow: 1 }}>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} color="white" fontWeight='bold'>
+                                Sound Mix
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
             </>
         );
     }
