d1b45b70cb8d7cc74e10b0a17d5fa21d7cbb66b2
Tina <tinzzy.cracked@yahoo.com>
2023-05-31 13:38:35 -0400

    fixed the sizing of boxes

--- sound-fade-in-out/src/components/Home.js
+++ sound-fade-in-out/src/components/Home.js
@@ -41,23 +41,20 @@ class Home extends React.Component {
     render() {
         return (
             <>
-                <div style={{ width: '100%' }}>
+                <div style={{ width: '100%', marginTop: 50 }}>
                     <div style={{ minWidth: 800, width: '100%', display: 'flex', flexDirection: 'row', height: '400px' }}>
-                        <div style={{ width: '50%', border: 'dotted 5px #145490', alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                        <div style={{ width: '50%', border: 'dotted 5px #145490', alignItems: 'center', justifyContent: 'center', display: 'flex', marginRight: 50 }}>
                             <LeftAudio />
                         </div>
-                        <div style={{
-                            width: '50%', borderTop: 'dotted 5px #145490', borderBottom: 'dotted 5px #145490', borderRight: 'dotted 5px #145490',
-                            alignItems: 'center', justifyContent: 'center', display: 'flex'
-                        }}>
+                        <div style={{ width: '50%', border: 'dotted 5px #145490', alignItems: 'center', justifyContent: 'center', display: 'flex', marginLeft: 50 }}>
                             <RightAudio />
                         </div>
                     </div>
                     <div style={{ width: '100%', display: 'flex', flexDirection: 'column', height: '200px', alignItems: 'center', justifyContent: 'center' }}>
-                        <Typography variant="body1">Select two separate audios and click play button</Typography>
                         <IconButton aria-label="delete" onClick={() => this.playBothAudio()} color="primary" >
                             {this.state.buttonName === "Play" ? <PlayCircleFilledWhiteOutlinedIcon fontSize="large" /> : <PauseCircleOutlinedIcon fontSize="large" />}
                         </IconButton>
+                        <Typography variant="body1">Fade in-n-out Button</Typography>
                     </div>
                 </div>
             </>
