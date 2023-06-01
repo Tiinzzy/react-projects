c11ca687df5ce5f70778a50c2942fb66d9f5cf9c
Tina <tinzzy.cracked@yahoo.com>
2023-06-01 09:09:50 -0400

    setting styling to control the theme on being dark and light

--- sound-fade-in-out/src/App.js
+++ sound-fade-in-out/src/App.js
@@ -1,11 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
 import Home from "./components/Home";
 import Header from "./components/Header";
 
+import { styleEventEmitter } from './components/Header';
+
+import './components/style.css';
+
 function App() {
+  const [theme, setTheme] = useState('');
+
+  useEffect(() => {
+    styleEventEmitter.on('settingStyle', (data) => {
+      setTheme(data.className);
+      console.log(data, 'app')
+    })
+  }, []);
   return (
     <>
       <Header />
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div className={theme} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <div style={{ width: '65%' }}>
           <Home />
         </div>
