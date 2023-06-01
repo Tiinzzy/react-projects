d96aa009ba8b49360cd036014c816c21d4cf5270
Tina <tinzzy.cracked@yahoo.com>
2023-06-01 16:57:02 -0400

    fixated for emitting steps

--- sound-fade-in-out/src/components/LeftAudio.js
+++ sound-fade-in-out/src/components/LeftAudio.js
@@ -38,7 +38,7 @@ class LeftAudio extends React.Component {
                     data.callBack('Pause');
                 });
                 let timerHandle = setInterval(() => {
-                    let volumeValue = this.state.volumeValue - 0.1;
+                    let volumeValue = this.state.volumeValue - data.steps;
                     volumeValue = volumeValue <= 0 ? 0 : volumeValue;
                     this.setState({ volumeValue }, function () {
                         a.volume = this.state.volumeValue;
@@ -90,8 +90,8 @@ class LeftAudio extends React.Component {
     }
 
     componentWillUnmount() {
-        eventEmitter.off('customEvent', (data) => { });
-        styleEventEmitter.off('settingStyle', (data) => { });
+        eventEmitter.off('customEvent');
+        styleEventEmitter.off('settingStyle');
     }
 
     render() {
