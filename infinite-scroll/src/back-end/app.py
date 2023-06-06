2253462a58a243f7e68c8520bd8fd4d7524b28a7
Tina <tinzzy.cracked@yahoo.com>
2023-06-06 09:17:02 -0400

    fixed api for getting data

--- infinite-scroll/src/back-end/app.py
+++ infinite-scroll/src/back-end/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
-import json
+import app_mysql
 
 app = Flask(__name__)
 
 
-def get_parameters(req):
-    return json.loads(req.data.decode('utf8').replace("'", '"'))
+@app.route("/movies/all", methods=["POST"])
+def all_movies():
+    result = app_mysql.get_all_movies()
+    return jsonify(result)
