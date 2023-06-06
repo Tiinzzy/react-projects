a63e8419ae61d4bbd0ae762ba6fe28ce8efa00e3
Tina <tinzzy.cracked@yahoo.com>
2023-06-06 17:19:49 -0400

    fixed the sql command and also added row page number

--- infinite-scroll/src/back-end/app_mysql.py
+++ infinite-scroll/src/back-end/app_mysql.py
@@ -8,16 +8,18 @@ def get_all_movies(parameters):
     con, cur = db.open_database()
     sql_command = f"""
     SELECT m.title, m.vote_average, m.overview, m.vote_count, m.imdb_id, mal.genre_count, m.id FROM tests.imbd_movies m
-    join tests.movies_all_genres mal on mal.title = m.title
+    join tests.movies_all_genres mal on mal.id = m.id
     where m.title <> '0' and m.vote_average <> '0' and m.overview <> '0'
-    order by m.id
-    limit {page_num} ,10
+    limit {page_num} ,20
     """
     cur.execute(sql_command)
     rows = cur.fetchall()
     result = []
+    row_number = page_num
     for row in rows:
-        result.append({'title': row[0], 'vote': row[1], 'overview': row[2], 'vote_count': row[3], 'imdb': row[4],
-                       'genres': row[5], 'movie_id': row[6]})
+        result.append(
+            {'row_number': row_number, 'title': row[0], 'vote': row[1], 'overview': row[2],
+             'vote_count': row[3], 'imdb': row[4], 'genres': row[5], 'movie_id': row[6]})
+        row_number += 1
     db.close_database()
     return result
