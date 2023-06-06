cea949fb5d24fe921642e82fc85276cf1b723be7
Tina <tinzzy.cracked@yahoo.com>
2023-06-06 13:34:06 -0400

    changing the mysql command and getting page number to get new data on scroll hitting the end of the div

--- infinite-scroll/src/back-end/app_mysql.py
+++ infinite-scroll/src/back-end/app_mysql.py
@@ -1,18 +1,23 @@
 from database import Database
 
-sql_command = """
-SELECT m.title, m.vote_average, m.overview, m.vote_count, m.imdb_id, mal.genre_count, m.id FROM tests.imbd_movies m
-join tests.movies_all_genres mal on mal.title = m.title
-where m.title <> '0' and m.vote_average <> '0' and m.overview <> '0'
-"""
 
-def get_all_movies():
+def get_all_movies(parameters):
+    page_num = parameters.get('page_number')
+
     db = Database()
     con, cur = db.open_database()
+    sql_command = f"""
+    SELECT m.title, m.vote_average, m.overview, m.vote_count, m.imdb_id, mal.genre_count, m.id FROM tests.imbd_movies m
+    join tests.movies_all_genres mal on mal.title = m.title
+    where m.title <> '0' and m.vote_average <> '0' and m.overview <> '0'
+    order by m.id
+    limit {page_num} ,10
+    """
     cur.execute(sql_command)
     rows = cur.fetchall()
     result = []
     for row in rows:
-        result.append({'title': row[0], 'vote': row[1], 'overview': row[2], 'vote_count': row[3], 'imdb': row[4], 'genres': row[5], 'movie_id': row[6]})
+        result.append({'title': row[0], 'vote': row[1], 'overview': row[2], 'vote_count': row[3], 'imdb': row[4],
+                       'genres': row[5], 'movie_id': row[6]})
     db.close_database()
     return result
