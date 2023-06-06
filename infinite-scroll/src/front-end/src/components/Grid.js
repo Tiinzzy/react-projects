32e922ff134d6a04428b77390ebe1276e39b6587
Tina <tinzzy.cracked@yahoo.com>
2023-06-06 09:38:09 -0400

    added headers to the top of table

--- infinite-scroll/src/front-end/src/components/Grid.js
+++ infinite-scroll/src/front-end/src/components/Grid.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import BackEndConnection from './BackEndConnection';
 
+import '../App.css';
+
 const backend = BackEndConnection.INSTANCE();
 
 export default class Grid extends React.Component {
@@ -9,20 +11,43 @@ export default class Grid extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            headers: null
         }
     }
 
     componentDidMount() {
         backend.get_all_movies((data) => {
-            console.log(data);
+            this.setState({ headers: Object.keys(data[0]) });
+            console.log(data[0])
         })
     }
 
     render() {
         return (
-            <>
-                this is the grid
-            </>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <div style={{ overflowY: 'scroll', height: 100, border: 'solid 1px black', width: 400 }}>
+                    <table width="100%" style={{ fontSize: '80%', backgroundColor: 'white', maring: 5 }} cellPadding={0} cellSpacing={1}>
+                        <tbody >
+                            <tr>
+                                {this.state.headers !== null && this.state.headers.map((e, i) => (
+                                    <th key={i}>{e}</th>
+                                ))}
+                            </tr>
+                            <tr>
+                                {/* <td>
+                                    gf
+                                </td>
+                                <td>
+                                    gf
+                                </td>
+                                <td>
+                                    gh
+                                </td> */}
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
 }
