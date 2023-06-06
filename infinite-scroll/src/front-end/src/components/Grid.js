3de0628d31bc2443c10db6cdc4bc96dbcacefb65
Tina <tinzzy.cracked@yahoo.com>
2023-06-06 13:34:38 -0400

    changed the format of displaying data and reloading and updating the grid

--- infinite-scroll/src/front-end/src/components/Grid.js
+++ infinite-scroll/src/front-end/src/components/Grid.js
@@ -11,13 +11,17 @@ export default class Grid extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            headers: null
+            headers: null,
+            fullData: [],
+            pageNum: 0
         }
     }
 
     componentDidMount() {
-        backend.get_all_movies((data) => {
-            this.setState({ headers: Object.keys(data[0]), fullData: data.slice(0, 10) });
+        console.log(3)
+        let query = { page_number: this.state.pageNum };
+        backend.get_all_movies(query, (data) => {
+            this.setState({ headers: Object.keys(data[0]), fullData: data, dataDisplay: data.slice(0, 10) });
         })
 
         document.getElementById('scorll-element').addEventListener('scroll', (e) => this.handelScroll(e));
@@ -25,11 +29,19 @@ export default class Grid extends React.Component {
 
     handelScroll(e) {
         const { scrollHeight, scrollTop, clientHeight } = e.target;
+        console.log(0)
         if (Math.abs(scrollHeight - clientHeight - scrollTop) < 1) {
+            console.log(1)
             let len = this.state.fullData.length;
-            len = len < this.state.fullData.length ? len + 5 : len;
-            let updatedData = this.state.fullData.slice(0, len)
-            this.setState({ fullData: updatedData })
+            len = len < this.state.fullData.length ? len + 10 : len;
+            let updatedData = this.state.dataDisplay.slice(0, len)
+            let updatedPageNumber = this.state.pageNum++
+            this.setState({ dataDisplay: updatedData, pageNum: updatedPageNumber }, () => {
+                let query = { page_number: this.state.pageNum };
+                backend.get_all_movies(query, (data) => {
+                    this.setState({ dataDisplay: data.slice(0, 10) });
+                })
+            })
         }
     }
 
@@ -40,15 +52,15 @@ export default class Grid extends React.Component {
     render() {
         return (
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <div id='scorll-element' style={{ overflowY: 'scroll', height: 100, border: 'solid 1px black', width: '100%' }}>
+                <div id='scorll-element' style={{ overflowY: 'scroll', height: 400, border: 'solid 1px black', width: '100%' }}>
                     <table width="100%" style={{ fontSize: '80%', backgroundColor: 'white', maring: 5 }} cellPadding={0} cellSpacing={1}>
                         <tbody >
                             <tr>
                                 {this.state.headers !== null && this.state.headers.map((e, i) => (
-                                    <th key={i} width='0.07%'>{e}</th>
+                                    <th key={i}>{e}</th>
                                 ))}
                             </tr>
-                            {this.state.fullData && this.state.fullData.map((e, i) => (
+                            {this.state.dataDisplay && this.state.dataDisplay.map((e, i) => (
                                 <tr key={i}>
                                     <td >
                                         {e.genres}
