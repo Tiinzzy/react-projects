2acb8b6e702daebec0a1f503ddf32244eaca979c
Tina <tinzzy.cracked@yahoo.com>
2023-06-06 13:33:50 -0400

    setting up getting parameters

--- infinite-scroll/src/back-end/app.py
+++ infinite-scroll/src/back-end/app.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, jsonify
+import json
 import app_mysql
 
 app = Flask(__name__)
 
 
+def get_parameters(req):
+    return json.loads(req.data.decode('utf8').replace("'", '"'))
+
+
 @app.route("/movies/all", methods=["POST"])
 def all_movies():
-    result = app_mysql.get_all_movies()
+    parameters = get_parameters(request)
+    result = app_mysql.get_all_movies(parameters)
     return jsonify(result)
