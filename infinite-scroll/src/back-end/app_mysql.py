121672cf2fd8eb4d112a025d76e3498d5a897377
Tina <tinzzy.cracked@yahoo.com>
2023-06-06 10:14:18 -0400

    removed random selection from mysql query

--- infinite-scroll/src/back-end/app_mysql.py
+++ infinite-scroll/src/back-end/app_mysql.py
@@ -4,7 +4,6 @@ sql_command = """
 SELECT m.title, m.vote_average, m.overview, m.vote_count, m.imdb_id, mal.genre_count, m.id FROM tests.imbd_movies m
 join tests.movies_all_genres mal on mal.title = m.title
 where m.title <> '0' and m.vote_average <> '0' and m.overview <> '0'
-order by rand()
 """
 
 def get_all_movies():
