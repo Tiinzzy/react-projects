d3e76c54b37b4dc0e28378748964cd58ea6298c8
Tina <tinzzy.cracked@yahoo.com>
2023-06-07 09:34:33 -0400

    component to make react table

---
+++ infinite-scroll/src/front-end/src/components/ReactTable.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class ReactTable extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+               this is react table
+            </div>
+        );
+    }
+}
