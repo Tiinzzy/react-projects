07cc7ca51e66e0bb11fb2f36ce662858ef7f4068
Tina <tinzzy.cracked@yahoo.com>
2023-06-07 11:20:33 -0400

    imported home component for display of react virtualized grid

--- infinite-scroll/src/front-end/src/App.js
+++ infinite-scroll/src/front-end/src/App.js
@@ -1,10 +1,13 @@
 import Grid from './components/Grid';
 import ReactTable from './components/ReactTable';
+import Home from './components/Home';
+
 
 function App() {
+
   return (
     <>
-      <ReactTable />
+        <ReactTable />
     </>
   );
 }
