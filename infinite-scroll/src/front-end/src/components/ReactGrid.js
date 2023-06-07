fa10a38ac7007c9f77e6c5441efb6c0da00e399f
Tina <tinzzy.cracked@yahoo.com>
2023-06-07 17:03:48 -0400

    tried to change the grid

--- infinite-scroll/src/front-end/src/components/ReactGrid.js
+++ infinite-scroll/src/front-end/src/components/ReactGrid.js
@@ -6,21 +6,26 @@ import './style.css'
 
 const ArrayGrid = ({ arrayOfArrays }) => {
     const cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
-        const value = arrayOfArrays[rowIndex][columnIndex];
+        const row = arrayOfArrays[rowIndex];
         return (
             <div key={key} style={style} className='row-styling'>
-                {value}
+                <span style={{ display: 'inline-block', width: 350, borderBottom: 'solid 1px gray', marginBottom: 20 }}>{row[0]}</span>
+                <span style={{ marginLeft: 20, display: 'inline-block', width: 90, borderBottom: 'solid 1px gray', marginBottom: 20 }}>{row[1]}</span>
+                <span style={{ marginLeft: 20, display: 'inline-block', width: 90, borderBottom: 'solid 1px gray', marginBottom: 20 }}>{row[2]}</span>
+                <span style={{ marginLeft: 20, display: 'inline-block', width: 500, borderBottom: 'solid 1px gray', marginBottom: 20 }}>{row[3].substr(0, 60) + ' ...'}</span>
+                <span style={{ marginLeft: 20, display: 'inline-block', width: 30, borderBottom: 'solid 1px gray', marginBottom: 20 }}>{row[4]}</span>
             </div>
         );
     };
 
     return (
         <Grid
+            style={{ borderBottom: 'solid 1px #eaeaea' }}
             cellRenderer={cellRenderer}
-            columnCount={arrayOfArrays[0].length}
-            columnWidth={900}
+            columnCount={1}
+            columnWidth={1300}
             rowCount={arrayOfArrays.length}
-            rowHeight={120}
+            rowHeight={30}
             height={950}
             width={1350}
         />
