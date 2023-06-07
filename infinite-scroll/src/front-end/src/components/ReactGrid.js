f18cb2da6953750e50443313839c434eb3903d24
Tina <tinzzy.cracked@yahoo.com>
2023-06-07 13:56:16 -0400

    fixed styling a bit

--- infinite-scroll/src/front-end/src/components/ReactGrid.js
+++ infinite-scroll/src/front-end/src/components/ReactGrid.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import { Grid } from 'react-virtualized';
 
-import 'react-virtualized/styles.css'; 
+import 'react-virtualized/styles.css';
+import './style.css'
 
 const ArrayGrid = ({ arrayOfArrays }) => {
-  const cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
-    const value = arrayOfArrays[rowIndex][columnIndex];
-    return (
-      <div key={key} style={style}>
-        {value}
-      </div>
-    );
-  };
+    const cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
+        const value = arrayOfArrays[rowIndex][columnIndex];
+        return (
+            <div key={key} style={style} className='row-styling'>
+                {value}
+            </div>
+        );
+    };
 
-  return (
-    <Grid
-      cellRenderer={cellRenderer}
-      columnCount={arrayOfArrays[0].length}
-      columnWidth={900} // Width of each column
-      rowCount={arrayOfArrays.length}
-      rowHeight={60} 
-      height={1000} 
-      width={1000} 
-    />
-  );
+    return (
+        <Grid
+            cellRenderer={cellRenderer}
+            columnCount={arrayOfArrays[0].length}
+            columnWidth={900}
+            rowCount={arrayOfArrays.length}
+            rowHeight={120}
+            height={950}
+            width={1350}
+        />
+    );
 };
 
 export default ArrayGrid;
