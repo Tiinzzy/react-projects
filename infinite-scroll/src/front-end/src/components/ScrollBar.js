d1382c94c2ab181dad2542db44c63828e50d964c
Tina <tinzzy.cracked@yahoo.com>
2023-06-09 09:54:12 -0400

    trying to fix the scroll bar

--- infinite-scroll/src/front-end/src/components/ScrollBar.js
+++ infinite-scroll/src/front-end/src/components/ScrollBar.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './style.css';
 
 export default function ScrollBar({ height, totalPages, currentPage }) {
+    const [isDragging, setIsDragging] = useState(false);
+    const [dragOffsetY, setDragOffsetY] = useState(0);
+
     const marginTop = height / totalPages * currentPage;
+
+    const handleMouseDown = (e) => {
+        console.log(1)
+        setIsDragging(true);
+        setDragOffsetY(e.clientY - e.target.getBoundingClientRect().top);
+    };
+
+    const handleMouseMove = (e) => {
+        console.log(2)
+        if (isDragging) {
+            console.log(3)
+            e.preventDefault();
+            const scrollContainer = e.target.parentElement;
+            const scrollContent = scrollContainer.firstChild;
+            const scrollBar = e.target;
+            const newY = e.clientY - scrollContainer.getBoundingClientRect().top - dragOffsetY;
+
+            const maxScroll = scrollContainer.clientHeight - scrollBar.clientHeight;
+            const clampedY = Math.max(0, Math.min(maxScroll, newY));
+
+            const scrollPosition = clampedY / maxScroll;
+            const newScrollTop = scrollPosition * (scrollContent.clientHeight - scrollContainer.clientHeight);
+
+            scrollContent.style.marginTop = -newScrollTop + 'px';
+            scrollBar.style.top = clampedY + 'px';
+            handleMouseUp();
+        }
+    };
+
+    const handleMouseUp = () => {
+        console.log(4)
+        setIsDragging(false);
+    };
+
     return (
-        <div style={{ height, width: 20, background: '#eaeaea' }}>
-            <div draggable={true}
-                style={{ marginTop, height: 5, background: 'dodgerblue' }}></div>
+        <div className="scroll-container">
+            <div style={{ marginTop }} className="scroll-bar" onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}></div>
         </div>
     )
 
