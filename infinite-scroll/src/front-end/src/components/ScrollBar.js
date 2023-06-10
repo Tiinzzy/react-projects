764b6429777690e8e3853dc8831e8e111dbf2733
Tina <tinzzy.cracked@yahoo.com>
2023-06-10 11:09:57 -0400

    cleaned up the component

--- infinite-scroll/src/front-end/src/components/ScrollBar.js
+++ infinite-scroll/src/front-end/src/components/ScrollBar.js
@@ -1,37 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import './style.css';
 
-function getWindowSize() {
-    return { h: window.innerHeight, w: window.innerWidth }
-}
-
 export default function ScrollBar({ height, totalPages, currentPage, callParent }) {
-    const [windowSize, setWindowSize] = useState(getWindowSize());
-
     var marginTop = height / totalPages * currentPage;
 
-    useEffect(() => {
-        function resizeHandler() {
-            setWindowSize(getWindowSize())
-        }
-        window.addEventListener('resize', resizeHandler);
-        return () => window.removeEventListener('resize', resizeHandler);
-    })
-
-    const jumpToPage = (pageNo) => {
+    const jumpToPage = (e) => {
+        let pageNo = Math.floor(totalPages * (e.clientY - 10) / height);
         pageNo = (pageNo > totalPages ? totalPages : pageNo);
         pageNo = (pageNo < 0 ? 0 : pageNo);
-        console.log(pageNo, totalPages);
         callParent(pageNo);
     }
 
     return (
-        <div className="scroll-container" style={{ height: height + 10 }}
-            onClick={(e) => {
-                jumpToPage(Math.floor(totalPages * (e.clientY - 10) / height), e.clientY);
-            }}>
-            <div className="scroll-bar" style={{ marginTop }}>{currentPage}</div>
+        <div id='page-scroll' className="scroll-container" style={{ height: height + 10 }}
+            // onMouseMove={(e) => console.log('PAGE:' + Math.floor(totalPages * (e.clientY - 10) / height))}
+            onClick={(e) => jumpToPage(e)}>
+            <div id='page-scroll' className="scroll-bar" style={{ marginTop }}>{currentPage}</div>
         </div >
     )
 };
