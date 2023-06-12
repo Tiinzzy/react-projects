6aa0b6e3a85b2ea92206620627939871375e9e2d
Tina <tinzzy.cracked@yahoo.com>
2023-06-12 10:04:24 -0400

    trying to add what page to move to on hover

--- infinite-scroll/src/front-end/src/components/ScrollBar.js
+++ infinite-scroll/src/front-end/src/components/ScrollBar.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from 'react';
 
 import Tooltip from '@mui/material/Tooltip';
 
 import './style.css';
 
 export default function ScrollBar({ height, totalPages, currentPage, callParent }) {
+    const [pageHover, setPageHover] = useState(0);
+
     var marginTop = height / totalPages * currentPage;
-    
+
     const jumpToPage = (e) => {
         let pageNo = Math.floor(totalPages * (e.clientY - 10) / height);
         pageNo = (pageNo > totalPages ? totalPages : pageNo);
@@ -15,12 +17,14 @@ export default function ScrollBar({ height, totalPages, currentPage, callParent 
     }
 
     return (
-        <div id='page-scroll' className="scroll-container" style={{ height: height + 10 }}
-            // onMouseMove={(e) => console.log('PAGE:' + Math.floor(totalPages * (e.clientY - 10) / height))}
-            onClick={(e) => jumpToPage(e)}>
-            <Tooltip title={'Page #' + currentPage}>
-                <div id='page-scroll' className="scroll-bar" style={{ marginTop }}>{currentPage}</div>
-            </Tooltip >
-        </div >
+        <Tooltip title={'Jump to Page #' + pageHover} arrow>
+            <div id='page-scroll' className="scroll-container" style={{ height: height + 10 }}
+                onMouseMove={(e) => { setPageHover(Math.floor(totalPages * (e.clientY - 10) / height)) }}
+                onClick={(e) => jumpToPage(e)}>
+                <Tooltip title={'Page #' + currentPage}>
+                    <div id='page-scroll' className="scroll-bar" style={{ marginTop }}>{currentPage}</div>
+                </Tooltip >
+            </div >
+        </Tooltip >
     )
 };
