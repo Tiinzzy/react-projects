90a8b06b44831dcf10eb6f0a9c853a0f261a1689
Tina <tinzzy.cracked@yahoo.com>
2023-06-12 10:18:22 -0400

    trying to make tooltip hover around

--- infinite-scroll/src/front-end/src/components/ScrollBar.js
+++ infinite-scroll/src/front-end/src/components/ScrollBar.js
@@ -16,11 +16,32 @@ export default function ScrollBar({ height, totalPages, currentPage, callParent 
         callParent(pageNo);
     }
 
+    const positionRef = React.useRef({
+        x: 0,
+        y: 0,
+    });
+    const popperRef = React.useRef(null);
+    const areaRef = React.useRef(null);
+
+    const handlePageSuggestions = (e) => {
+        setPageHover(Math.floor(totalPages * (e.clientY - 10) / height))
+        positionRef.current = { x: e.clientX, y: e.clientY };
+
+        if (popperRef.current != null) {
+            popperRef.current.update();
+        }
+    };
+
     return (
-        <Tooltip title={'Jump to Page #' + pageHover} arrow>
+        <Tooltip title={'Jump to Page #' + pageHover} arrow
+            PopperProps={{
+                popperRef,
+                anchorEl: { getBoundingClientRect: () => { return new DOMRect(positionRef.current.x, areaRef.current.getBoundingClientRect().y, 0, 0,); }, },
+            }}>
             <div id='page-scroll' className="scroll-container" style={{ height: height + 10 }}
-                onMouseMove={(e) => { setPageHover(Math.floor(totalPages * (e.clientY - 10) / height)) }}
-                onClick={(e) => jumpToPage(e)}>
+                onMouseMove={handlePageSuggestions}
+                onClick={(e) => jumpToPage(e)}
+                ref={areaRef}>
                 <Tooltip title={'Page #' + currentPage}>
                     <div id='page-scroll' className="scroll-bar" style={{ marginTop }}>{currentPage}</div>
                 </Tooltip >
