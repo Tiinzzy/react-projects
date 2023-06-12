d2028c3518347da0850bb2316aae2afaffebe2b9
Tina <tinzzy.cracked@yahoo.com>
2023-06-12 13:25:26 -0400

    code cleanup

--- infinite-scroll/src/front-end/src/components/ScrollBar.js
+++ infinite-scroll/src/front-end/src/components/ScrollBar.js
@@ -6,7 +6,6 @@ import './style.css';
 
 export default function ScrollBar({ height, totalPages, currentPage, callParent }) {
     const [pageHover, setPageHover] = useState(0);
-    const [tooltipPosition, setTooltipPosition] = React.useState({ x: 0, y: 0 });
 
     var marginTop = height / totalPages * currentPage;
 
@@ -17,25 +16,16 @@ export default function ScrollBar({ height, totalPages, currentPage, callParent 
         callParent(pageNo);
     }
 
-
-    const handlePageSuggestions = (e) => {
-        setPageHover(Math.floor(totalPages * (e.clientY - 10) / height));
-        setTooltipPosition({ x: e.clientX, y: e.clientY });
-    };
-
     return (
         <Tooltip arrow placement="left-start"
-            open={tooltipPosition.x !== 0 && tooltipPosition.y !== 0}
-            PopperProps={{ style: { transform: `translate(${tooltipPosition.x}px, ${tooltipPosition.y}px)`, }, }}
             title={'Jump to Page #' + pageHover}>
-
             <div id='page-scroll' className="scroll-container" style={{ height: height + 10 }}
-                onMouseMove={handlePageSuggestions}
-                onMouseLeave={() => { setTooltipPosition(0) }}
+                onMouseMove={(e) => {
+                    setPageHover(Math.floor(totalPages * (e.clientY - 10) / height));}}
                 onClick={(e) => jumpToPage(e)}>
-                <Tooltip title={'Page #' + currentPage}>
-                    <div id='page-scroll' className="scroll-bar" style={{ marginTop }}>{currentPage}</div>
-                </Tooltip >
+                {/* <Tooltip title={'Page #' + currentPage}> */}
+                <div id='page-scroll' className="scroll-bar" style={{ marginTop }}>{currentPage}</div>
+                {/* </Tooltip > */}
             </div >
         </ Tooltip >
     )
