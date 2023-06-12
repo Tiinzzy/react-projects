c671ab4c1537f578aa61abdbc06de6aee809d76e
Tina <tinzzy.cracked@yahoo.com>
2023-06-12 18:12:59 -0400

    fixed hover display of jumping page

--- infinite-scroll/src/front-end/src/components/ScrollBar.js
+++ infinite-scroll/src/front-end/src/components/ScrollBar.js
@@ -1,32 +1,43 @@
 import React, { useState } from 'react';
 
-import Tooltip from '@mui/material/Tooltip';
-
 import './style.css';
 
-export default function ScrollBar({ height, totalPages, currentPage, callParent }) {
-    const [pageHover, setPageHover] = useState(0);
+const SCROLL_BAR_INDICATOR_HEIGHT = 10;
+const TOOL_TIP_Y_OFFSET = 15;
+const TOOL_TIP_X_OFFSET = 215;
 
-    var marginTop = height / totalPages * currentPage;
-
+export default function ScrollBar({ buttonHeight, height, totalPages, currentPage, callParent }) {
     const jumpToPage = (e) => {
-        let pageNo = Math.floor(totalPages * (e.clientY - 10) / height);
+        let pageNo = Math.floor(totalPages * (e.clientY - buttonHeight - SCROLL_BAR_INDICATOR_HEIGHT) / (height - SCROLL_BAR_INDICATOR_HEIGHT));
         pageNo = (pageNo > totalPages ? totalPages : pageNo);
         pageNo = (pageNo < 0 ? 0 : pageNo);
         callParent(pageNo);
     }
 
     return (
-        <Tooltip arrow placement="left-start"
-            title={'Jump to Page #' + pageHover}>
-            <div id='page-scroll' className="scroll-container" style={{ height: height + 10 }}
+        <div>
+            <div id='page-scroll' className="scroll-container" style={{ height: height }}
+                onMouseLeave={(e) => {
+                    let tooTipDiv = document.getElementById("tool-tip");
+                    tooTipDiv.style.display = "none";
+                    tooTipDiv.style.top = 0;
+                    tooTipDiv.style.left = 0;
+                }}
                 onMouseMove={(e) => {
-                    setPageHover(Math.floor(totalPages * (e.clientY - 10) / height));}}
+                    let pageNo = Math.floor(totalPages * (e.clientY - buttonHeight - SCROLL_BAR_INDICATOR_HEIGHT) / (height - SCROLL_BAR_INDICATOR_HEIGHT));
+                    pageNo = (pageNo > totalPages ? totalPages : pageNo);
+                    pageNo = (pageNo < 0 ? 0 : pageNo);
+                    let tooTipDiv = document.getElementById("tool-tip");
+                    tooTipDiv.style.top = (e.clientY - TOOL_TIP_Y_OFFSET) + 'px';
+                    tooTipDiv.style.left = (window.innerWidth - TOOL_TIP_X_OFFSET) + 'px';
+                    tooTipDiv.textContent = 'Jump to page # ' + pageNo;
+                    tooTipDiv.style.display = "block";
+                }}
                 onClick={(e) => jumpToPage(e)}>
-                {/* <Tooltip title={'Page #' + currentPage}> */}
-                <div id='page-scroll' className="scroll-bar" style={{ marginTop }}>{currentPage}</div>
-                {/* </Tooltip > */}
+                <div id='page-scroll' className="scroll-bar" style={{ marginTop: (height - SCROLL_BAR_INDICATOR_HEIGHT) / totalPages * currentPage }}>{currentPage}</div>
             </div >
-        </ Tooltip >
+            <div id="tool-tip" className='tool-tip'>
+            </div>
+        </div>
     )
 };
