e471ccc374ef041f2f8521191b2965e85fb26522
Tina <tina.vatanabadi@gmail.com>
2023-06-24 11:19:53 -0400

    class component for testing drop zone package

---
+++ photo-gallery/src/tests/TestDropZone.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class TestDropZone extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <>
+                this is dropzone
+            </>
+        );
+    }
+};
