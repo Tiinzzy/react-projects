a070f9294ea3e21309c2cccfa9ecdfabc803374b
Tina <tina.vatanabadi@gmail.com>
2023-06-24 14:26:29 -0400

    fixed drop zone but will vanilla javascript and not the package!

--- photo-gallery/src/tests/TestDropZone.js
+++ photo-gallery/src/tests/TestDropZone.js
@@ -1,50 +1,87 @@
 import React, { Component } from 'react';
-import { Dropzone } from 'dropzone';
 
-class TestDropZone extends Component {
-  componentDidMount() {
-    // Check if Dropzone is already attached to the element
-    if (!document.querySelector(".dropzone")) {
-      // Initialize Dropzone when the component mounts
-      this.initializeDropzone();
+import './test-style.css';
+
+const BOX_STYLE = function (width) {
+  return {
+    display: "inline-block",
+    height: width / 2.5,
+    width: width,
+    marginRight: 10,
+    marginBottom: 10,
+    border: "solid 1px darkred",
+    textAlign: "center",
+  };
+};
+
+export default class TestDropZone extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      droppedImage: null,
+      imageInformation: {},
+      width: 300
     }
   }
 
-  initializeDropzone() {
-    const dropzone = new Dropzone("div#myId", {
-      url: "/file/post",
-      acceptedFiles: "image/*", // Limit the accepted file types to images
-      maxFiles: 1, // Limit the number of files to 1
-      autoProcessQueue: false, // Disable automatic file processing
+  componentDidMount() {
+    const dropZone = document.getElementById('dropzone');
+
+    window.addEventListener("resize", this.resizeWindow());
+
+    dropZone.addEventListener('dragover', (e) => {
+      e.preventDefault();
     });
 
-    // Event handler for when a file is added
-    dropzone.on("addedfile", function (file) {
-      const reader = new FileReader();
+    dropZone.addEventListener('drop', (e) => {
+      e.preventDefault();
 
-      reader.onload = function (event) {
-        const image = new Image();
-        image.src = event.target.result;
+      const files = e.dataTransfer.files;
+      const imageInformation = {};
 
-        // Display the dropped image in a div with the id "preview"
-        document.getElementById("preview").appendChild(image);
-      };
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        imageInformation['name'] = file.name;
 
-      reader.readAsDataURL(file);
+        if (file.type.startsWith('image/')) {
+          const reader = new FileReader();
+
+          reader.onload = (e) => {
+            this.setState({
+              droppedImage: e.target.result
+            });
+            imageInformation['path'] = e.target.result;
+          };
+          reader.readAsDataURL(file);
+        }
+      }
+      this.setState({ imageInformation });
     });
+  }
+
+  resizeWindow() {
+    let screenWidth = window.innerWidth * 0.95;
+    this.setState({ width: screenWidth - 100 });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.resizeWindow());
   }
 
   render() {
     return (
-      <div>
-        <div id="myId" className="dropzone" style={{ border: 'dotted 1px gray', borderRadius: 10, padding: 20, margin: 10, textAlign: 'center' }}>
-          <span>Drop an image here to upload.</span>
+      <>
+        <div id="dropzone" className="dropzone"
+          style={{ height: 200, border: 'dotted 1px gray', borderRadius: 10, padding: 20, margin: 10, textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <span>Drag and drop an image here.</span>
         </div>
-        <hr/> 
-        <div id="preview"></div>
-      </div>
+        <hr />
+        <div id="preview" className="preview-container"
+          // style={BOX_STYLE(this.state.width)}
+        >
+          {this.state.droppedImage && <img src={this.state.droppedImage} alt="Dropped" className="preview-image" width="500" height="500" />}
+        </div>
+      </>
     );
   }
 }
-
-export default TestDropZone;
