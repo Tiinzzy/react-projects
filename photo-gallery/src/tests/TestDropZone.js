4dc31587b4fc4af7294030fe1dc509433f9bd2c8
Tina <tina.vatanabadi@gmail.com>
2023-06-24 12:03:58 -0400

    changed drop zone component to see if it works

--- photo-gallery/src/tests/TestDropZone.js
+++ photo-gallery/src/tests/TestDropZone.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Dropzone } from 'dropzone';
 
-export default class TestDropZone extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
+class TestDropZone extends Component {
+  componentDidMount() {
+    // Check if Dropzone is already attached to the element
+    if (!document.querySelector(".dropzone")) {
+      // Initialize Dropzone when the component mounts
+      this.initializeDropzone();
     }
+  }
 
-    render() {
-        return (
-            <>
-                this is dropzone
-            </>
-        );
-    }
-};
+  initializeDropzone() {
+    const dropzone = new Dropzone("div#myId", {
+      url: "/file/post",
+      acceptedFiles: "image/*", // Limit the accepted file types to images
+      maxFiles: 1, // Limit the number of files to 1
+      autoProcessQueue: false, // Disable automatic file processing
+    });
+
+    // Event handler for when a file is added
+    dropzone.on("addedfile", function (file) {
+      const reader = new FileReader();
+
+      reader.onload = function (event) {
+        const image = new Image();
+        image.src = event.target.result;
+
+        // Display the dropped image in a div with the id "preview"
+        document.getElementById("preview").appendChild(image);
+      };
+
+      reader.readAsDataURL(file);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="myId" className="dropzone" style={{ border: 'dotted 1px gray', borderRadius: 10, padding: 20, margin: 10, textAlign: 'center' }}>
+          <span>Drop an image here to upload.</span>
+        </div>
+        <hr/> 
+        <div id="preview"></div>
+      </div>
+    );
+  }
+}
+
+export default TestDropZone;
