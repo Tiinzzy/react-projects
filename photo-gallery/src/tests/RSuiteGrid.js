94eee06eb4579696fe7104e42d2c8093e66d56d7
Tina <tina.vatanabadi@gmail.com>
2023-06-24 15:17:28 -0400

    fixed to work while mapping data

--- photo-gallery/src/tests/RSuiteGrid.js
+++ photo-gallery/src/tests/RSuiteGrid.js
@@ -2,85 +2,37 @@ import { Grid, Row, Col } from "rsuite";
 
 import "rsuite/dist/rsuite.min.css";
 
+const NAMES = [
+    "Steven James",
+    "Julia Olsen",
+    "Ana Mcbride",
+    "Caylee Preston",
+    "Kaiya Mccall",
+    "Xiomara Thompson",
+    "Parker Willis",
+    "Nathanial Quinn",
+    "Madden Boyer",
+    "Kali Roy",
+    "Aydan Blevins",
+    "Chaz Perry",
+];
+
 export default function RSuiteGrid() {
     return (
-        <div>
+        <div style={{ margin: 'auto' }}>
             <Grid>
                 <Row>
-                    <Col
-                        xs={24}
-                        sm={8}
-                        md={6}
-                        lg={4}
-                        style={{ backgroundColor: "#A0F962", textAlign: 'center' }}
-                    >
-                        1
-                    </Col>
-                    <Col
-                        xs={24}
-                        sm={8}
-                        md={6}
-                        lg={4}
-                        style={{ backgroundColor: "#D97BFC", textAlign: 'center' }}
-                    >
-                        2
-                    </Col>
-                    <Col
-                        xs={24}
-                        sm={8}
-                        md={6}
-                        lg={4}
-                        style={{ backgroundColor: "#96FEF2", textAlign: 'center' }}
-                    >
-                        3
-                    </Col>
-                    <Col
-                        xs={24}
-                        sm={8}
-                        md={6}
-                        lg={4}
-                        style={{ backgroundColor: "#FC7B96", textAlign: 'center' }}
-                    >
-                        4
-                    </Col>
-                </Row>
-                <Row>
-                    <Col
-                        xs={24}
-                        sm={8}
-                        md={6}
-                        lg={4}
-                        style={{ backgroundColor: "#FEAF96", textAlign: 'center' }}
-                    >
-                        1
-                    </Col>
-                    <Col
-                        xs={24}
-                        sm={8}
-                        md={6}
-                        lg={4}
-                        style={{ backgroundColor: "#CAFF83", textAlign: 'center' }}
-                    >
-                        2
-                    </Col>
-                    <Col
-                        xs={24}
-                        sm={8}
-                        md={6}
-                        lg={4}
-                        style={{ backgroundColor: "#FEED96 ", textAlign: 'center' }}
-                    >
-                        3
-                    </Col>
-                    <Col
-                        xs={24}
-                        sm={8}
-                        md={6}
-                        lg={4}
-                        style={{ backgroundColor: "#D6D8FF", textAlign: 'center' }}
-                    >
-                        4
-                    </Col>
+                    {NAMES.map((n, i) => (
+                        <Col
+                            key={i}
+                            xs={24}
+                            sm={8}
+                            md={6}
+                            lg={4}
+                            style={{ backgroundColor: "wheat", border: 'solid 1px #FAD7A1', textAlign: 'center', marginRight: 5, marginBottom: 5 }}>
+                            {n}
+                        </Col>
+                    ))}
                 </Row>
             </Grid>
         </div>
