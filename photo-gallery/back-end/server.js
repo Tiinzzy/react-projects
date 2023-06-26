366b0b78df29f85c7c5a171efd735df2dd1214b3
Tina <tina.vatanabadi@gmail.com>
2023-06-26 10:28:49 -0400

    changed the format and name saving of the image so even repetitive ones can be saved

--- photo-gallery/back-end/server.js
+++ photo-gallery/back-end/server.js
@@ -27,9 +27,11 @@ function addImageData(newData) {
 app.listen(PORT, () => {
 
     app.post("/image/upload", upload.single('file'), (req, res) => {
+        const newImgName = 'IMG-' + randomNumAssignment() + '.JPG';
+
         if (req.file.mimetype.startsWith('image/')) {
             const sourcePath = req.file.path;
-            const destinationPath = '/Users/tina/Documents/react-projects/photo-gallery/back-end/dropzone-save-photo/' + req.file.originalname;
+            const destinationPath = '/Users/tina/Documents/react-projects/photo-gallery/back-end/dropzone-save-photo/' + newImgName;
             fs.rename(sourcePath, destinationPath, (error) => {
                 if (error) {
                     console.error('error moving file:', error);
@@ -40,7 +42,7 @@ app.listen(PORT, () => {
                 }
             });
 
-            addImageData({ file_real_name: req.file.originalname, alternative_name: randomNumAssignment() })
+            addImageData({ file_real_name: req.file.originalname, alternative_name: newImgName })
 
             const jsonToString = JSON.stringify(imageJsonData, null, 1);
 
