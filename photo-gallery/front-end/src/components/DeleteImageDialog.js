bb8d18d8f81b235ba74acdeb0b1513612656409f
Tina <tina.vatanabadi@gmail.com>
2023-06-28 13:20:57 -0400

    starting to build the inside data of dialog

--- photo-gallery/front-end/src/components/DeleteImageDialog.js
+++ photo-gallery/front-end/src/components/DeleteImageDialog.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 
+import Button from '@mui/material/Button';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 export default class DeleteImageDialog extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            arrayOfImages: [],
-            count: 0,
-            width: 300,
-            openBackdrop: false,
-            clickedImage: '',
-            openDialog: false
+            handleCloseDialog: props.handleCloseDialog
         }
+    }
+
+    cancelAndClose() {
+        this.state.handleCloseDialog();
+    }
+
+    deleteAndClose(){
+        this.state.handleCloseDialog();
     }
 
     render() {
         return (
             <>
-              this will be delete dialog 
+                <DialogTitle>
+                    {"Would you like to delete the following Image?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Let Google help apps determine location. This means sending anonymous
+                        location data to Google, even when no apps are running.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => this.cancelAndClose()}>Cancel</Button>
+                    <Button onClick={() => this.deleteAndClose()}> Agree </Button>
+                </DialogActions>
             </>
         );
     }
