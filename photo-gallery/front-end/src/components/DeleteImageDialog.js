0a2df87efd82f7ba464e243d1503a5d53fe1130e
Tina <tina.vatanabadi@gmail.com>
2023-06-30 17:22:51 -0400

    changed dialog to only display image instead of deleting

--- photo-gallery/front-end/src/components/DeleteImageDialog.js
+++ photo-gallery/front-end/src/components/DeleteImageDialog.js
@@ -7,41 +7,19 @@ import Box from '@mui/material/Box';
 
 import EventEmitter from 'eventemitter3';
 
-import BackEndConnection from './BackEndConnection';
-import { findTheObject } from './functions';
-
 export const deleteEmitter = new EventEmitter();
-
-const backend = BackEndConnection.INSTANCE();
 
 export default class DeleteImageDialog extends Component {
     constructor(props) {
         super(props);
         this.state = {
             handleCloseDialog: props.handleCloseDialog,
-            selectedImage: props.selectedImage,
-            allImagesInfo: props.allImagesInfo,
-            imageName: ''
+            selectedImage: props.selectedImage
         }
-    }
-
-    componentDidMount() {
-        let deleteImage = findTheObject(this.state.allImagesInfo, this.state.selectedImage);
-        let imageName = deleteImage.file_real_name;
-        this.setState({ imageName });
     }
 
     cancelAndClose() {
         this.state.handleCloseDialog();
-    }
-
-    deleteAndClose() {
-        backend.delete_image(this.state.imageName, (data) => {
-            if (data.success) {
-                deleteEmitter.emit('check_updated', { message: 'image_deleted' });
-                this.state.handleCloseDialog();
-            };
-        })
     }
 
     render() {
