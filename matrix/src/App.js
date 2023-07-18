5a76c7f3d2b89a6e11d4b463f9e761405675cd3e
Tina <tina.vatanabadi@gmail.com>
2023-07-18 11:10:56 -0400

    added grid to the app

--- matrix/src/App.js
+++ matrix/src/App.js
@@ -1,9 +1,11 @@
 import Matrix from './components/Matrix';
+import Grid from './components/Grid';
 
 function App() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 25 }}>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 25 }}>
       <Matrix />
+      <Grid />
     </div>
   );
 }
