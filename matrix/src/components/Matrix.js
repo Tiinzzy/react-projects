db5b15f60048e6797c36c6a0e9256715a4a79411
Tina <tina.vatanabadi@gmail.com>
2023-07-18 10:14:05 -0400

    created an app for matrix box

---
+++ matrix/src/components/Matrix.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class Matrix extends React.Component { 
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+  
+
+    render() {
+        return (
+            <>
+                this is matrix
+            </>            
+        );
+    }
+};
