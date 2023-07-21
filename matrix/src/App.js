ba08466618167241546289243ef201692df1baae
Tina <tina.vatanabadi@gmail.com>
2023-07-21 10:40:54 -0400

    changed the padding from top

--- matrix/src/App.js
+++ matrix/src/App.js
@@ -3,7 +3,7 @@ import Grid from './components/Grid';
 
 function App() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 15 }}>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '25px 15px 15px 15px' }}>
       <Matrix />
       <Grid />
     </div>
