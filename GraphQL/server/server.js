89c8e6b1f90a6b3f74c60e08c838971fcb729ed9
Tina <tinzzy.cracked@yahoo.com>
2023-08-02 13:59:02 -0400

    created a GraphQL server to handle APIs

---
+++ GraphQL/server/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLNonNull, GraphQLInt } = require('graphql');
+
+const app = express();
+
+const tasks = [{ id: 0, content: "Watching TV" }, { id: 1, content: "Washing Dishes" }];
+
+const TaskType = new GraphQLObjectType({
+    name: 'Task',
+    fields: () => ({
+        id: { type: GraphQLInt },
+        content: { type: GraphQLString },
+    }),
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        tasks: {
+            type: GraphQLList(TaskType),
+            resolve: () => tasks,
+        },
+    },
+});
+
+const RootMutation = new GraphQLObjectType({
+    name: 'RootMutationType',
+    fields: {
+        addTask: {
+            type: TaskType,
+            args: {
+                content: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve: (_, { content }) => {
+                const newTask = { id: tasks.length + 1, content };
+                tasks.push(newTask);
+                return newTask;
+            },
+        },
+    },
+});
+
+const schema = new GraphQLSchema({
+    query: RootQuery,
+    mutation: RootMutation,
+});
+
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
