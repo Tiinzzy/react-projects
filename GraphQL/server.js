9ea9279ded701f3e08a057185f1c71520905ffa4
Tina <tinzzy.cracked@yahoo.com>
2023-08-02 09:12:01 -0400

    adding books to graphql server and also sending query with schema to get specific data

--- GraphQL/server.js
+++ GraphQL/server.js
@@ -1,22 +1,61 @@
 const express = require('express');
 const app = express();
 const { graphqlHTTP } = require('express-graphql');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
+const { GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLNonNull } = require('graphql');
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields: () => ({
-            message: {
-                type: GraphQLString,
-                resolve: () => 'Hello World'
-            }
-        })
+const authors = [
+    { id: 1, name: 'J. K. Rowling' },
+    { id: 2, name: 'J. R. R. Tolkien' },
+    { id: 3, name: 'Brent Weeks' }
+];
+
+const books = [
+    { id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+    { id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+    { id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+    { id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+    { id: 5, name: 'The Two Towers', authorId: 2 },
+    { id: 6, name: 'The Return of the King', authorId: 2 },
+    { id: 7, name: 'The Way of Shadows', authorId: 3 },
+    { id: 8, name: 'Beyond the Shadows', authorId: 3 }
+];
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'THis represents a book writeen by an authpr',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt) }
     })
 });
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List of ALl Books',
+            resolve: () => books
+        }
+    })
+});
+
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
+});
+
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true
 }));
+
 app.listen(8888., () => { console.log('Server is Running!') });
