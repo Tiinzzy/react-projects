2ba56d047681fb13a5e12be73a631fccf1b9e14b
Tina <tinzzy.cracked@yahoo.com>
2023-10-10 11:24:07 -0400

    created home component and placed in app.js

---
+++ chat-bot/src/front-end/src/components/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { Box } from "@mui/material";
+
+import ChatBox from "./ChatBox";
+
+const homeStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 40,
+    padding: 20,
+    height: '90%'
+}
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    componentDidMount() {
+        console.log(1);
+    }
+
+    render() {
+        return (
+            <>
+                <Box style={homeStyle}>
+                    <ChatBox />
+                </Box>
+            </>
+        );
+    }
+};
