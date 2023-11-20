c9faebdc52d2a71eabfa1903e3a8a76bbbf1eec9
Tina <tinzzy.cracked@yahoo.com>
2023-11-20 11:01:05 -0500

    backend method to get all tv series

--- netflix-model/src/components/BackEndConnection.js
+++ netflix-model/src/components/BackEndConnection.js
@@ -68,6 +68,23 @@ class BackEndConnectionImpl {
                 return { result: false };
             })
     }
+
+    async get_all_tvseries(callback) {
+        return axios.get('/tvseries/all', {}, {})
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                if (callback) {
+                    callback({ result: false })
+                }
+                return { result: false };
+            })
+    }
 }
 
 export default class BackEndConnection {
