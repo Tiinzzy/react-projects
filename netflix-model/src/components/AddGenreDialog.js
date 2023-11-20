ae8bd639d43edbc79f9b7038a35f34535ed28d44
Tina <tinzzy.cracked@yahoo.com>
2023-11-20 12:32:01 -0500

    renamed to dialog for genre only

--- netflix-model/src/components/AddGenreDialog.js
+++ netflix-model/src/components/AddGenreDialog.js
@@ -4,6 +4,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 import BackEndConnection from './BackEndConnection';
@@ -19,35 +20,39 @@ class AddDialog extends React.Component {
         }
     }
 
-    componentDidMount() {
-        if (this.state.addClick === "Genre") {
-            this.agreeAndClose();
-        };
+    getGenre(e) {
+        this.setState({ genreDescription: e.target.value });
     }
 
     agreeAndClose() {
-        let query = { 'description': 'test' };
-        backend.add_genre('query', (data) => {
-            console.log(data);
+        if (this.props.addClick === "Genre") {
+            this.processAddGenre();
+        }
+
+    }
+
+    processAddGenre() {
+        let query = { 'description': this.state.genreDescription };
+        backend.add_genre(query, (data) => {
+            if (data === true) {
+                this.state.handleClose();
+            };
         })
-        this.state.handleClose();
     }
 
 
     render() {
         return (
             <>
-                <DialogTitle id="alert-dialog-title">
+                <DialogTitle id="alert-dialog-title" mb={2}>
                     {"Add " + this.state.addClick}
                 </DialogTitle>
                 <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        HAVE TO LIST THE ADDING DETAIL HERE!
-                    </DialogContentText>
+                    <TextField label="Genre Description" variant="outlined" style={{ margin: 10 }} onChange={(e) => this.getGenre(e)} />
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={this.state.handleClose}>Disagree</Button>
-                    <Button autoFocus onClick={() => this.agreeAndClose()}>Agree</Button>
+                    <Button onClick={this.state.handleClose} variant="outlined">Disagree</Button>
+                    <Button autoFocus onClick={() => this.agreeAndClose()} variant="outlined">Agree</Button>
                 </DialogActions>
             </>
         );
