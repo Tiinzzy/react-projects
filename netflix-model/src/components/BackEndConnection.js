7a6f9329de54cf3526a607d862031798486be631
Tina <tinzzy.cracked@yahoo.com>
2023-11-20 10:06:42 -0500

    added method to load all customers via get method

--- netflix-model/src/components/BackEndConnection.js
+++ netflix-model/src/components/BackEndConnection.js
@@ -17,6 +17,23 @@ class BackEndConnectionImpl {
                 return { result: false };
             })
     }
+
+    async get_all_customers(callback) {
+        return axios.get('/customer/all', {}, {})
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                if (callback) {
+                    callback({ result: false })
+                }
+                return { result: false };
+            })
+    }
 }
 
 export default class BackEndConnection {
