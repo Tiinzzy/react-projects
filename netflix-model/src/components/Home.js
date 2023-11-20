0ad2c39f2cab8f353caf574fd327264ebe9147cc
Tina <tinzzy.cracked@yahoo.com>
2023-11-20 08:35:10 -0500

    adjusting styling and added the new display component

--- netflix-model/src/components/Home.js
+++ netflix-model/src/components/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 import DisplayList from "./DisplayList";
+import DisplayEdit from "./DisplayEdit";
+
+import Box from '@mui/material/Box';
 
 class Home extends React.Component {
     constructor(props) {
@@ -11,9 +14,10 @@ class Home extends React.Component {
 
     render() {
         return (
-            <>
+            <Box style={{ border: 'solid 1px red', paddingTop: 50, paddingRight: 10, paddingLeft: 10, paddingBottom: 50, display: 'flex', flexDirection: 'row' }}>
                 <DisplayList />
-            </>
+                <DisplayEdit />
+            </Box>
         );
     }
 }
