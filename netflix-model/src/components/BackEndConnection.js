3ae131c9d08943bfdfc618e91c5436d8eb32085c
Tina <tinzzy.cracked@yahoo.com>
2023-11-20 09:36:48 -0500

    component to connect to backend via axios

---
+++ netflix-model/src/components/BackEndConnection.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+class BackEndConnectionImpl {
+    async get_all_genre(callback) {
+        return axios.get('/genre/all', {}, {})
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                if (callback) {
+                    callback({ result: false })
+                }
+                return { result: false };
+            })
+    }
+}
+
+export default class BackEndConnection {
+    static #object = null;
+
+    static INSTANCE() {
+        if (BackEndConnection.#object === null) {
+            BackEndConnection.#object = new BackEndConnectionImpl();
+        }
+        return BackEndConnection.#object;
+    }
+}
