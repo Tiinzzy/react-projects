54e060dd63481c22ac75fb16fbfe4dc3537263a7
Tina <tinzzy.cracked@yahoo.com>
2023-11-20 08:24:51 -0500

    created a list with all the items of the model for modifying

---
+++ netflix-model/src/components/DisplayList.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+const NETFLIX_MODEL = ["Customer", "Genre", "Movies", "Subscription", "TV Series"];
+
+class DisplayList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', border: 'solid 1px #eaeaea' }}>
+                <nav aria-label="main mailbox folders">
+                    <List >
+                        {NETFLIX_MODEL.map((e, i) => (
+                            <ListItem disablePadding key={i}>
+                                <ListItemButton>
+                                    <ListItemText primary={e} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </nav>
+            </Box>
+        );
+    }
+}
+export default DisplayList;
