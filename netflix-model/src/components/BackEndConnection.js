20442632fd614628866bbcbf2a30d11f9d7615bc
Tina <tinzzy.cracked@yahoo.com>
2023-11-21 10:16:39 -0500

    axios post method for adding a new subscription

--- netflix-model/src/components/BackEndConnection.js
+++ netflix-model/src/components/BackEndConnection.js
@@ -188,6 +188,24 @@ class BackEndConnectionImpl {
             })
     }
 
+    async add_subscription(query, callback) {
+        return axios.post('/subscription/add', query, { headers: { 'Content-Type': 'application/json' } })
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                if (callback) {
+                    callback({ result: false })
+                }
+                return { result: false };
+            })
+    }
+
+
 }
 
 export default class BackEndConnection {
