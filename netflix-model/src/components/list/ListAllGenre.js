1cc2bc07171482114eef532702a5cd4f65c8b1fa
Tina <tinzzy.cracked@yahoo.com>
2023-11-21 14:34:43 -0500

    open dialog to display genre editing on double click of the row

--- netflix-model/src/components/list/ListAllGenre.js
+++ netflix-model/src/components/list/ListAllGenre.js
@@ -6,6 +6,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import Dialog from '@mui/material/Dialog';
 
 import GenreDelete from "../delete/GenreDelete";
+import UpdateGenre from "../edit/UpdateGenre";
 
 import EventEmitter from 'eventemitter3';
 
@@ -22,7 +23,7 @@ class ListAllGenre extends React.Component {
     }
 
     dialogToDelete(e) {
-        this.setState({ openDialog: true, toBeDeleted: e })
+        this.setState({ openDialog: true, toBeDeleted: e, selection: 'delete' })
     }
 
     handleCloseDialog(e) {
@@ -32,7 +33,13 @@ class ListAllGenre extends React.Component {
                     genreUpdate.emit('update', { task: 'update' });
                 }
             })
+        } else {
+            this.setState({ openDialog: false });
         }
+    }
+
+    openEdit(e) {
+        this.setState({ openDialog: true, toBeUpdated: e, selection: 'update' })
     }
 
     render() {
@@ -50,6 +57,7 @@ class ListAllGenre extends React.Component {
                         <TableBody>
                             {this.props.genreData.map((e, i) => (
                                 <TableRow
+                                    onDoubleClick={() => this.openEdit(e)}
                                     hover
                                     key={i}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
@@ -61,7 +69,8 @@ class ListAllGenre extends React.Component {
                     </Table>
                 </TableContainer>
                 <Dialog open={this.state.openDialog} onClose={() => this.handleCloseDialog()}>
-                    <GenreDelete closeDialog={this.handleCloseDialog} toBeDeleted={this.state.toBeDeleted} />
+                    {this.state.selection === 'delete' && <GenreDelete closeDialog={this.handleCloseDialog} toBeDeleted={this.state.toBeDeleted} />}
+                    {this.state.selection === 'update' && <UpdateGenre toBeUpdated={this.state.toBeUpdated} handleClose={this.handleCloseDialog} />}
                 </Dialog>
             </Box>
         );
