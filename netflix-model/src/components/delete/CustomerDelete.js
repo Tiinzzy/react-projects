f9284e9d27558b76d2310498f07e00087a20fdbf
Tina <tinzzy.cracked@yahoo.com>
2023-11-21 08:19:53 -0500

    fixed the internal content of the dialog for delete

--- netflix-model/src/components/delete/CustomerDelete.js
+++ netflix-model/src/components/delete/CustomerDelete.js
@@ -1,21 +1,61 @@
 import React from "react";
 
-import Box from '@mui/material/Box';
+import DialogActions from '@mui/material/DialogActions';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormHelperText from '@mui/material/FormHelperText';
+import Checkbox from '@mui/material/Checkbox';
+import DialogContent from '@mui/material/DialogContent';
+import Typography from '@mui/material/Typography';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+
 
 class CustomerDelete extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             closeDialog: props.closeDialog,
-            toBeDeleted: props.toBeDeleted
+            toBeDeleted: props.toBeDeleted,
+            checkBox: false
         }
+    }
+
+    componentDidMount() {
+        console.log(this.state.toBeDeleted)
+    }
+
+    agreeAndClose() {
+
+    }
+
+    handleCheckBox() {
+        this.setState({ checkBox: !this.state.checkBox });
     }
 
     render() {
         return (
-            <Box>
-
-            </Box>
+            <>
+                <DialogTitle id="alert-dialog-title" mb={2}>
+                    {"Delete Customer"}
+                </DialogTitle>
+                <DialogContent style={{ display: 'flex', flexDirection: 'column', width: '500PX' }}>
+                    <Typography>OID: <span>{this.state.toBeDeleted.oid}</span></Typography>
+                    <Typography>Name: <span>{this.state.toBeDeleted.name}</span></Typography>
+                    <Typography>Email: <span>{this.state.toBeDeleted.email}</span></Typography>
+                    <Typography>Phone Number: <span>{this.state.toBeDeleted.phoneNo}</span></Typography>
+                </DialogContent>
+                <DialogActions>
+                    <FormControlLabel
+                        control={
+                            <Checkbox checked={this.state.checkBox} onChange={() => this.handleCheckBox()} />}
+                        label="Are you sure you want to delete the dollowing?" />
+                    <Button onClick={this.state.closeDialog} variant="outlined">No</Button>
+                    <Button autoFocus onClick={() => this.agreeAndClose()} variant="outlined" disabled={this.state.checkBox === false}>Yes</Button>
+                </DialogActions>
+            </>
         );
     }
 }
