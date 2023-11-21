bbada272b85c4f35becfa917ed3520c8e14275b2
Tina <tinzzy.cracked@yahoo.com>
2023-11-21 10:15:57 -0500

    class component for type of subscription

---
+++ netflix-model/src/components/add/TypeMenu.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+
+const TYPE_OPTIONS = ['Monthly', 'Annualy', 'Not Defined'];
+
+class TypeMenu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            anchorEl: null,
+            selectedOption: 'Not Defined'
+        };
+    }
+
+    handleClick(e) {
+        this.setState({ anchorEl: e.currentTarget });
+    };
+
+    handleClose() {
+        this.setState({ anchorEl: null });
+    };
+
+    handleSelect(option) {
+        this.setState({ selectedOption: option, anchorEl: null });
+    };
+
+    render() {
+        return (
+            <>
+                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={(e) => this.handleClick(e)} variant="outlined">
+                    {this.state.selectedOption}
+                </Button>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={this.state.anchorEl}
+                    keepMounted
+                    open={Boolean(this.state.anchorEl)}
+                    onClose={() => this.handleClose()}>
+                    {TYPE_OPTIONS.map((e, i) => (
+                        <MenuItem key={i} selected={e === this.state.selectedOption} onClick={() => this.handleSelect(e)}>
+                            {e}
+                        </MenuItem>
+                    ))}
+                </Menu>
+            </>
+        );
+    }
+} export default TypeMenu;
