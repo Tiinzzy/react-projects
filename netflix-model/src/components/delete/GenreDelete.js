2f9a897a9acbef19de66eb7ab18b6e6de363d52f
Tina <tinzzy.cracked@yahoo.com>
2023-11-21 09:04:41 -0500

    dialog to delete genre and send query to backend for deleting

---
+++ netflix-model/src/components/delete/GenreDelete.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import DialogActions from '@mui/material/DialogActions';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import DialogContent from '@mui/material/DialogContent';
+import Typography from '@mui/material/Typography';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+
+import BackEndConnection from '../BackEndConnection';
+
+const backend = BackEndConnection.INSTANCE();
+
+class GenreDelete extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            closeDialog: props.closeDialog,
+            toBeDeleted: props.toBeDeleted,
+            checkBox: false
+        }
+    }
+
+    agreeAndClose() {
+        let query = { 'oid': this.props.toBeDeleted.oid };
+        backend.delete_genre(query, (data) => {
+            if(data === true){
+                this.state.closeDialog();
+            };
+        })
+    }
+
+    handleCheckBox() {
+        this.setState({ checkBox: !this.state.checkBox });
+    }
+
+    render() {
+        return (
+            <>
+                <DialogTitle id="alert-dialog-title" mb={2}>
+                    {"Delete Genre"}
+                </DialogTitle>
+                <DialogContent style={{ display: 'flex', flexDirection: 'column', width: '500PX' }}>
+                    <Typography>OID: <span>{this.state.toBeDeleted.oid}</span></Typography>
+                    <Typography>Description: <span>{this.state.toBeDeleted.description}</span></Typography>
+                </DialogContent>
+                <DialogActions>
+                    <FormControlLabel
+                        control={
+                            <Checkbox checked={this.state.checkBox} onChange={() => this.handleCheckBox()} />}
+                        label="Are you sure you want to delete the dollowing?" />
+                    <Button onClick={this.state.closeDialog} variant="outlined">No</Button>
+                    <Button autoFocus onClick={() => this.agreeAndClose()} variant="outlined" disabled={this.state.checkBox === false}>Yes</Button>
+                </DialogActions>
+            </>
+        );
+    }
+}
+export default GenreDelete;
