d1d2439c6e31e5495f6fcaa43f8690a4c4f34dd0
Tina <tinzzy.cracked@yahoo.com>
2023-11-22 10:38:29 -0500

    fixing styling and adding detail for the dialog to add episode

--- netflix-model/src/components/add/AddTvSeriesDialog.js
+++ netflix-model/src/components/add/AddTvSeriesDialog.js
@@ -5,6 +5,8 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
 
 import BackEndConnection from '../BackEndConnection';
 
@@ -36,33 +38,40 @@ class AddTvSeriesDialog extends React.Component {
     }
 
     agreeAndClose() {
-        if (this.props.addClick === "Subscription") {
-            let query = { 'title': this.state.title, 'summary': this.state.summary, 'startDate': this.state.startDate, 'endDate': this.state.endDate };
-            backend.add_tvseries(query, (data) => {
-                if (data === true) {
-                    this.state.handleClose('reload tvseries');
-                };
-            })
-        }
+        let query = { 'title': this.state.title, 'summary': this.state.summary, 'startDate': this.state.startDate, 'endDate': this.state.endDate };
+        backend.add_tvseries(query, (data) => {
+            if (data === true) {
+                this.state.handleClose('reload tvseries');
+            };
+        })
+    }
+
+    openEpisodeDetail(){
+        console.log(11)
     }
 
     render() {
         return (
-            <>
+            <Box style={{ width: '500px' }}>
                 <DialogTitle id="alert-dialog-title" mb={2}>
                     {"Add " + this.state.addClick}
                 </DialogTitle>
-                <DialogContent style={{ display: 'flex', flexDirection: 'column', width: '450px' }}>
-                    <TextField label="Title" variant="outlined" style={{ margin: 10 }} onChange={(e) => this.getTitle(e)} />
-                    <TextField label="Summary" variant="outlined" style={{ margin: 10 }} onChange={(e) => this.getSummary(e)} />
-                    <TextField label="Start Date" variant="outlined" style={{ margin: 10 }} onChange={(e) => this.getStartDate(e)} />
-                    <TextField label="End Date" variant="outlined" style={{ margin: 10 }} onChange={(e) => this.getEndDate(e)} />
+                <DialogContent style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Box style={{ paddingRight: 100 }}>
+                        <TextField label="Title" variant="outlined" style={{ margin: 10 }} onChange={(e) => this.getTitle(e)} size='small' />
+                        <TextField label="Summary" variant="outlined" style={{ margin: 10 }} onChange={(e) => this.getSummary(e)} size='small' />
+                        <TextField label="Start Date" variant="outlined" style={{ margin: 10 }} onChange={(e) => this.getStartDate(e)} size='small' />
+                        <TextField label="End Date" variant="outlined" style={{ margin: 10 }} onChange={(e) => this.getEndDate(e)} size='small' />
+                    </Box>
+                    <Button variant="contained" size="small" style={{ marginLeft: 'auto', marginBottom: 15 }} onClick={() => this.openEpisodeDetail()}>Add Episode</Button>
+
+                    <Divider />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={this.state.handleClose} variant="outlined">Disagree</Button>
                     <Button autoFocus onClick={() => this.agreeAndClose()} variant="outlined">Agree</Button>
                 </DialogActions>
-            </>
+            </Box>
         );
     }
 }
