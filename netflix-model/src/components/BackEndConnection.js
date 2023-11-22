55902e7cc0fd84cdeba4272b5e455a42583d89e0
Tina <tinzzy.cracked@yahoo.com>
2023-11-22 13:58:51 -0500

    added post api for updating tv series

--- netflix-model/src/components/BackEndConnection.js
+++ netflix-model/src/components/BackEndConnection.js
@@ -324,6 +324,23 @@ class BackEndConnectionImpl {
             })
     }
 
+    async update_tvseries(query, callback) {
+        return axios.post('/tvseries/update', query, { headers: { 'Content-Type': 'application/json' } })
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                if (callback) {
+                    callback({ result: false })
+                }
+                return { result: false };
+            })
+    }
+
 
 }
 
