ae90998fc1a2ab16dc6aee6ba8a661fa829b4654
Tina <tinzzy.cracked@yahoo.com>
2023-11-22 18:28:43 -0500

    fixed the size of dialog for tv series

--- netflix-model/src/components/list/ListAllTvSeries.js
+++ netflix-model/src/components/list/ListAllTvSeries.js
@@ -76,7 +76,7 @@ class ListAllTvSeries extends React.Component {
                         </TableBody>
                     </Table>
                 </TableContainer>
-                <Dialog open={this.state.openDialog} onClose={() => this.handleCloseDialog()}>
+                <Dialog open={this.state.openDialog} onClose={() => this.handleCloseDialog()} maxWidth="md" fullWidth={true}>
                     {this.state.selection === 'delete' && <TvSeriesDelete closeDialog={this.handleCloseDialog} toBeDeleted={this.state.toBeDeleted} />}
                     {this.state.selection === 'update' && <UpdateTvSeries toBeUpdated={this.state.toBeUpdated} handleClose={this.handleCloseDialog} />}
                 </Dialog>
