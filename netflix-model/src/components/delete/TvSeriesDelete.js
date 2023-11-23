b32dfa4b5d8205072510589a666d33fd0968e013
Tina <tinzzy.cracked@yahoo.com>
2023-11-23 13:43:27 -0500

    fixed loginc for deleting tv series that have no seasons added to them

--- netflix-model/src/components/delete/TvSeriesDelete.js
+++ netflix-model/src/components/delete/TvSeriesDelete.js
@@ -8,6 +8,7 @@ import Button from '@mui/material/Button';
 import Box from "@mui/material/Box";
 
 import BackEndConnection from '../BackEndConnection';
+import { Typography } from "@mui/material";
 
 const backend = BackEndConnection.INSTANCE();
 
@@ -16,28 +17,24 @@ class TvSeriesDelete extends React.Component {
         super(props);
         this.state = {
             toBeDeleted: props.toBeDeleted,
-            handleClose: props.handleClose,
+            closeDialog: props.closeDialog,
             title: props.toBeDeleted.title,
             summary: props.toBeDeleted.summary,
             startDate: props.toBeDeleted.startDate,
-            endDate: props.toBeDeleted.endDate
+            endDate: props.toBeDeleted.endDate,
+            messageDisplay: false
         }
     }
 
     componentDidMount() {
         let query = { 'oid': this.state.toBeDeleted.oid };
         backend.load_tv_seasons(query, (data) => {
-            console.log(data)
-            if(data.length >0 ){
-                this.setState({ seasons: data });
-            }else{
-                this.setState({ seasons: 0 });
-            }
+            this.setState({ seasons: data.length });
         })
     }
 
     agreeAndClose() {
-        if (this.state.seasons.length > 0) {
+        if (this.state.seasons === 0) {
             console.log('delete here')
             let query = { 'oid': this.props.toBeDeleted.oid };
             backend.delete_tvseries(query, (data) => {
@@ -46,9 +43,7 @@ class TvSeriesDelete extends React.Component {
                 };
             });
         } else {
-            this.setState({ message: 'You need to delete seasons first!' }, () => {
-                console.log(this.state.message)
-            });
+            this.setState({ message: 'You need to delete seasons first!', messageDisplay: true });
         }
 
     }
@@ -65,9 +60,10 @@ class TvSeriesDelete extends React.Component {
                     <TextField label="End Date" variant="outlined" style={{ margin: 10 }} size='small' value={this.state.endDate} disabled={true} />
                     <TextField label="Summary" variant="outlined" style={{ margin: 10 }} size='small' value={this.state.summary}
                         multiline={true} maxRows={10} minRows={4} disabled={true} />
-                    <TextField label="Number of Seasons" variant="outlined" style={{ margin: 10 }} size='small' value={this.state.seasons} />
+                    <TextField label="Number of Seasons" variant="outlined" style={{ margin: 10 }} size='small' value={this.state.seasons} disabled={true} />
                 </DialogContent>
                 <DialogActions>
+                    {this.state.messageDisplay && <Typography style={{ color: 'crimson', marginRight: 'auto', marginLeft: 25 }}>{this.state.message}</Typography>}
                     <Button onClick={this.state.handleClose} variant="outlined">Disagree</Button>
                     <Button autoFocus onClick={() => this.agreeAndClose()} variant="outlined">Agree</Button>
                 </DialogActions>
