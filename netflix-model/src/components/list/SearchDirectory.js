5945aac0ea354a8e4b32e509701afd2edc260980
Tina <tinzzy.cracked@yahoo.com>
2023-11-28 09:05:03 -0500

    fixed internal display of getting user input need to send data to backend

--- netflix-model/src/components/list/SearchDirectory.js
+++ netflix-model/src/components/list/SearchDirectory.js
@@ -1,18 +1,48 @@
 import React from "react";
 
+import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
 
 class SearchDirectory extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            path: '',
+            depth: '',
+            erroMsg: ''
+        }
+    }
+
+    getPath(e) {
+        this.setState({ path: e.target.value, erroMsg: '' });
+    }
+
+    getDepth(e) {
+        this.setState({ depth: e.target.value * 1, erroMsg: '' });
+    }
+
+    searchGetTree() {
+        if (this.state.depth.length !== 0 || this.state.path.length !== 0) {
+
+        } else {
+            this.setState({ erroMsg: 'PLease fill in all fields!' })
         }
     }
 
     render() {
         return (
-            <Box style={{ paddingTop: 50, paddingRight: 10, paddingLeft: 10, paddingBottom: 50, display: 'flex', flexDirection: 'row' }}>
-                test
+            <Box style={{ display: 'flex', flexDirection: 'column', margin: 5, padding: 20 }}>
+                <TextField label="Directory Path" variant="outlined" onChange={(e) => this.getPath(e)} />
+                <TextField label="Depth Level" variant="outlined" sx={{ mt: 3 }} onChange={(e) => this.getDepth(e)} />
+                <Box style={{ display: 'flex', marginTop: 15 }}>
+                    {this.state.erroMsg.length > 0 && <Typography color="crimson">{this.state.erroMsg}</Typography>}
+                    <Box flexGrow={1} />
+                    <Button variant="outlined" onClick={() => this.searchGetTree()}>Search</Button>
+                </Box>
+                <Divider sx={{ mt: 1 }} />
             </Box>
         );
     }
