8f8334a07086b857290ab698f849a55233cc7736
Tina <tinzzy.cracked@yahoo.com>
2023-11-28 08:50:04 -0500

    component to get user input for searching directory depth

---
+++ netflix-model/src/components/list/SearchDirectory.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+class SearchDirectory extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Box style={{ paddingTop: 50, paddingRight: 10, paddingLeft: 10, paddingBottom: 50, display: 'flex', flexDirection: 'row' }}>
+                test
+            </Box>
+        );
+    }
+}
+export default SearchDirectory;
