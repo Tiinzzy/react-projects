3bd6b53c09ed98cb6e998c12b167a26422195b8f
Tina <tinzzy.cracked@yahoo.com>
2023-12-04 08:56:22 -0500

    created a class for displaying game of life

---
+++ netflix-model/src/components/list/GameOfLife.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+class GameOfLife extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Box>
+                this is GameOfLife
+            </Box>
+        );
+    }
+}
+export default GameOfLife;
