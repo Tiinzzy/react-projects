119a7c05dc0d1bd0f94b9ffa79aa0427e7cc7d9c
Tina <tinzzy.cracked@yahoo.com>
2023-12-18 10:52:45 -0500

    component to display langtons ant simulation

---
+++ netflix-model/src/components/list/LangtonsAnt.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+class LangtonsAnt extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Box >
+              ant
+            </Box>
+        );
+    }
+}
+export default LangtonsAnt;
