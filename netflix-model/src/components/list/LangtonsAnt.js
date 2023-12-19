0354ca31a9e10aabe1ee9860b80ed25bc0fa52d6
Tina <tinzzy.cracked@yahoo.com>
2023-12-19 09:18:42 -0500

    added textfield to get the number of board size and steps per tick and also display the steps as the program goes

--- netflix-model/src/components/list/LangtonsAnt.js
+++ netflix-model/src/components/list/LangtonsAnt.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
 
 import BackEndConnection from '../BackEndConnection';
 
@@ -14,19 +16,29 @@ class LangtonsAnt extends React.Component {
         this.state = {
             delay: 50,
             grid: null,
-            counter: 0
+            counter: 0,
+            boardSize: 100,
+            steps: 100
         }
+    }
+
+    getBoardSize(e) {
+        this.setState({ boardSize: e.target.value * 1 });
+    }
+
+    getStepsNumber(e) {
+        this.setState({ steps: e.target.value * 1 });
     }
 
     initializeSimulation() {
         let updateBoard = () => {
             this.setState({ counter: this.state.counter + 1 })
-            backend.get_langtons_ant((data) => {
+            backend.get_langtons_ant(this.state.boardSize, this.state.steps, (data) => {
                 if (this.state.counter % 10 === 0) {
                     console.log(this.state.counter);
                 }
                 this.setState({ grid: null }, () => {
-                    this.setState({ grid: data });
+                    this.setState({ grid: data.data, stepNum: data.steps });
                 });
             });
         }
@@ -69,9 +81,16 @@ class LangtonsAnt extends React.Component {
 
     render() {
         return (
-            <Box style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: 15 }}>
+            <Box style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: 20 }}>
+                <Box style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', marginBottom: 20 }}>
+                    <TextField label="Board Size" type="number" value={this.state.boardSize}
+                        onChange={(e) => this.getBoardSize(e)} sx={{ width: 150, mr: 2 }} />
+                    <TextField label="Number of Steps" type="number" name='width' value={this.state.steps}
+                        onChange={(e) => this.getStepsNumber(e)} sx={{ width: 150 }} />
+                </Box>
                 <Button variant="outlined" onClick={() => this.initializeSimulation()}>Initialize Langtons Ant Simulation</Button>
                 <Divider sx={{ mt: 2, mb: 2 }} />
+                {this.state.stepNum && <Typography>Step Number: {this.state.stepNum}</Typography>}
                 <Box >
                     {this.state.grid && this.renderGrid()}
                 </Box>
