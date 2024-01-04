21af861f580091f45538626e80e69fb6dc009726
Tina <tinzzy.cracked@yahoo.com>
2024-01-04 11:34:14 -0500

    now it moves forward solo and also the orbit of two object is not circle

--- netflix-model/src/components/gravity/physics.js
+++ netflix-model/src/components/gravity/physics.js
@@ -1,18 +1,28 @@
 const G = 1;
 
 export function updateUniverse(universe) {
-    console.log(1)
     return universe.map(body => nextState(body, universe));
 }
 
 function nextState(body, universe) {
     let newState = { ...body, vx: body.vx || 0, vy: body.vy || 0 };
 
+    // Handling for single objects
+    if (newState.s === 0 && newState.a === 0) {
+        applyDefaultMotion(newState);
+    }
+
     universe.forEach(otherBody => {
-        if (otherBody.id !== body.id) {
+        if (otherBody.id !== body.id && !isPairedWith(body, otherBody)) {
             applyGravitationalForce(newState, otherBody);
         }
     });
+
+    // Prioritize maintaining orbit with paired object
+    const pairedObject = findPairedObject(body, universe);
+    if (pairedObject) {
+        lockIntoOrbit(newState, pairedObject);
+    }
 
     newState.x += newState.vx;
     newState.y += newState.vy;
@@ -43,7 +53,8 @@ function isWithinOrbitRange(body, externalBody, distance) {
     return distance < orbitThreshold;
 }
 
-function lockIntoOrbit(body, externalBody, distance) {
+function lockIntoOrbit(body, externalBody) {
+    let distance = Math.sqrt((externalBody.x - body.x) ** 2 + (externalBody.y - body.y) ** 2);
     let orbitalVelocity = Math.sqrt(G * getMass(externalBody.r) / distance);
     let directionToExternalBody = Math.atan2(externalBody.y - body.y, externalBody.x - body.x);
     let orbitalDirection = directionToExternalBody + Math.PI / 2;
@@ -55,4 +66,19 @@ function lockIntoOrbit(body, externalBody, distance) {
 function getMass(r) {
     const weightPerVolume = 1;
     return 4 / 3 * Math.PI * r * r * r * weightPerVolume;
+}
+
+function isPairedWith(body, otherBody) {
+    return Math.abs(body.id - otherBody.id) === 1;
+}
+
+function findPairedObject(body, universe) {
+    return universe.find(otherBody => isPairedWith(body, otherBody));
+}
+
+function applyDefaultMotion(body) {
+    // Logic to apply default motion for single objects
+    // For example, a slow circular motion
+    body.vx += 0.1;
+    body.vy += 0.1;
 }
