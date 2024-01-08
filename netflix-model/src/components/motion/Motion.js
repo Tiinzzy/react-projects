c6b6c5e6a746825c879aca96785a0304fa9a555b
Tina <tinzzy.cracked@yahoo.com>
2024-01-08 10:08:34 -0500

    created a motion component

---
+++ netflix-model/src/components/motion/Motion.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+class Motion extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                motion
+            </div>
+        );
+    }
+}
+export default Motion;
